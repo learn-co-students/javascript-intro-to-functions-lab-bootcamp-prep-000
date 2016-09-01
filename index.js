a3fcba8a92f091a9ce43853981f732371b69c55a
TENZIN  TSAGONG <tsagong@gmail.com>
2016-09-01 17:23:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase('HELLO')
+}
+
+function logshout(string) {
+  console.log.toUpperCase('hello')
+} ('string') + ('spy')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sayHiToGrandma() {
+  if (hello) = .toLowerCase
+  return "I can't hear you!."
+  if (HELLO) =.toUpperCase
+  return "YES INDEED!"
+  if () = ('I love you, Grandma')
+  return "I love you, too"
+}
