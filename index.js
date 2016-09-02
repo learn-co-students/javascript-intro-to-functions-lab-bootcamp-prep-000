246cd07d59016991d9e6fc62ed301ce047843c27
Anna Bowers <anna.bowers@gmail.com>
2016-09-02 11:25:01 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ('lowercase')
+  if (string === string.toLowerCase())
   {
   return "I can't hear you!"
-} else if ('uppercase')
+} else if (string === string.toUpperCase())
 {
   return "YES INDEED!"
 } else if ("I love you, Grandma.")
