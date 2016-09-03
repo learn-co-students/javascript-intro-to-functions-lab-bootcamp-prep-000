4ffb267299b32b677ec0ccc463829fa416780068
Jesse Vineyard <jessevineyard@gmail.com>
2016-09-03 23:32:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(){
+  console.log("hello".toUpperCase()
+)
+}
+
+function logWhisper(){
+  console.log("hello".toLowerCase()
+)
+}
+
+function sayHiToGrandma(string){
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (string === " I love you, Grandma.") {
+    return " I love you, too."
+  }
+}
