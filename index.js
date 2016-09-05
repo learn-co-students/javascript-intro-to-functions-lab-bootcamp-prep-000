dd5d40681fe1230bef6eba7da6337f78f730f150
Joseph Long <joseph@looker.com>
2016-09-04 18:37:21 -0700

    add closing paren
    Closing paren needed for function to work

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-
+}
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
