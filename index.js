e7bb1c2c50f241ffec1b0d9333f75c9cea34b9fa
Landon Tarver <landontarver@gmail.com>
2016-09-06 20:43:53 +0000

    Done.

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase){
-   return 'I can\'t hear you!'
+  if (string === string.toLowerCase()){
+   return 'I can\'t hear you!'}
 
-  else if (string === string.toUpperCase) {
-    return 'YES INDEED!'
+  else if (string === string.toUpperCase()) {
+    return 'YES INDEED!'}
+
+  else return 'I love you, too.'
 
   }
-  }
-}
