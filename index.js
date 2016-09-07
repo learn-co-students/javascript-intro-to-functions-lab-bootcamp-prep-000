266af7b0aaaa26cb96665f0ae55b902e616fa838
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-07 13:13:00 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else {
+    if (string === string.toLowerCase()) {
+      return "I can\'t hear you!"
+    } else {
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+    }
+    }
+    }
+  }
