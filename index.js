e6a0c275ccd402383e1702c7e12baa5a2b2613bc
James Appleton <jappleton101@gmail.com>
2016-09-08 03:35:15 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase() === string {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
-  } else if string.toUpperCase() === string {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-  } else if string === "I love you, Grandma." {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
