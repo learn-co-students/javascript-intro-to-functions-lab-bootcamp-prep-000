4c0371ef951126f2c47c670db26f257625919037
Alexandra Cooper <allie.g.cooper@gmail.com>
2016-09-08 18:02:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+return(age >= 13 && age <= 19) ? ("You are a teenager") : ("You are not a teenager");
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return("I can't hear you!")
+  }
+  else if (string.toUpperCase() === string) {
+    return("YES INDEED!")
+  }
+  else if (string === "I love you, Grandma.") {
+    return("I love you, too.")
+  }
+}
