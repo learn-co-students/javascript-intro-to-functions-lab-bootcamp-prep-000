2bed8ae89ae65c65f87635f6c60fba3162be2f89
Clifton Jadoo <cliffjadoo@gmail.com>
2016-09-08 03:22:12 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ function shout(string){
   return string.toUpperCase()
 }
 
-function whipser(string){
-  return whisper.toLowerCase()
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string){
+  if (shout(string) === string) {
+    return "YES INDEED!"
+  } else if (whisper(string) === string){
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
