c4125155d9f549e794cc24229e5a9960f4f51018
Joe Niehaus <joeniehaus10@gmail.com>
2016-09-08 23:26:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if string === uppercase {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if string === "I love you, Grandma." {
+    return "I love you, too."
+  }
+}
