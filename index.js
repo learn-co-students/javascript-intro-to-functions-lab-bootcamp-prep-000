b066fc2c5e0927c99b4c0c614234d1e5e8ce6fad
Joe Niehaus <joeniehaus10@gmail.com>
2016-09-09 00:21:36 +0000

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,11 @@ function logShout(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
-    return "I can't hear you!"
-  }
-}
-
-function sayHiToGrandma(string) {
-  if string === uppercase {
+  if (string === string.toLowerCase()) {
+    return "I can\'t hear you!"
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  }
-}
-
-function sayHiToGrandma(string) {
-  if string === "I love you, Grandma." {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
