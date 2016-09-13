412f6c0ac2ecbd3aef46b123eb12730e426fa7f0
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-13 22:11:39 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function whisper(string) {
 }
 
 function logShout(string) {
-console.log(string. toUpperCase)();
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase()===string) {
+  return "I can\'t hear you!"}
+  if(string.toUpperCase()===string) {
+    return "YES INDEED!"}
+  if(string==="I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
