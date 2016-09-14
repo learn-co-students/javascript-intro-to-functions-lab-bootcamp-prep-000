596019fb69a21e61bbd7b74cd6673ef815f42e4a
Eduardo Moura <eduardomourabiz@gmail.com>
2016-09-14 02:59:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return "Hello".toUpperCase()
+}
+
+function whisper(string) {
+
+  return "HELLO".toLowerCase()
+}
+
+function logShout(string) {
+
+console.log("HELLO")
+
+}
+function logWhisper(string) {
+
+  console.log ("hello")
+}
+
+function sayHiToGrandma(string) {
+
+
+if (string === "hello")
+return "I can't hear you!"
+
+else if(string === "HELLO")
+return "YES INDEED!"
+
+  else if(string === "I love you, Grandma.")
+  return "I love you, too."
+
+
+}
