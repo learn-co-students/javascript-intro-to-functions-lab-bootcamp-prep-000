56e47b4013d2147b0dcd90fbc70375d9965f7537
Carlo Francisco <cfrancisco726@gmail.com>
2016-09-15 19:08:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+
+function sayHiToGrandma(string) {
+switch(string) {
+  case ("hello"):
+  return("I can\'t hear you!");
+  break;
+  case ("HELLO"):
+  return("YES INDEED!");
+  break;
+  case "I love you, Grandma.":
+  return("I love you, too.");
+  break;
+  }
+}
