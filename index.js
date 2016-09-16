d29e54332b5311a5e04fa5c01316e8db03d5ae45
Daniel Puch <danielpuch1969@gmail.com>
2016-09-16 14:15:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var lowercase = 'hello!' {
+
+if function sayHiToGrandma(lowercase) {
+
+      if lowercase.toLowerCase() === lowercase
+  }
+  {
+    return "I can't hear you!"
+}
+}
