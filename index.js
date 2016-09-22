edb942bba6c5fbcabd1f14196799175c110c9b6f
Alexander Rosenthal <alexrosenthalster@gmail.com>
2016-09-22 15:25:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+logShout("hello")
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+logWhisper("HELLO")
+
+var uppercase = 'HELLO'
+var lowercase = 'hello'
+function sayHiToGrandma(string) {
+if (string = lowercase && lowercase.toLowerCase() === lowercase) {
+  return "I can't hear you!"
+} else if (uppercase.toUppercase() === uppercase) {
+  return "YES INDEED!"
+}
+}
