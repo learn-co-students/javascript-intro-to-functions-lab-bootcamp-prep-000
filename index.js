ff1ea2cc7b65048e17f472976127c7cf8ebf47c0
Herbert Tejada <herberthtejada@gmail.com>
2016-10-05 04:32:27 +0000

    Done.

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ logWhisper();
 
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
-    return "I can't hear you!";
-  }
-  else if(string === string.toUpperCase()){
+    return "I can\'t hear you!";
+  }else if(string === string.toUpperCase()){
     return "YES INDEED!";
   }else if(string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
+
 sayHiToGrandma();
