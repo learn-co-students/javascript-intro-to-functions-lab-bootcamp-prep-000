7eeb0b739c466e6a0a2751c3bd11cdd445b9642a
Murray Elder <elder.murray84@gmail.com>
2016-10-11 03:27:37 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,15 +1,12 @@
-
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
+function whisper(string){
+  return string.toLowerCase()
+}
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
