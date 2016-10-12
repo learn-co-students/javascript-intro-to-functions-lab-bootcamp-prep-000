05dd13adf36b6b476a15360c097518d99ef120d0
Daniel Lempesis <danlrcn@hotmail.com>
2016-10-12 15:38:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase
+}
+
+fuction whisper(string){
+  return string.toLowerCase
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase)
+}
