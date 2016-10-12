8f7e92a627087521de75a8a8bc1b9d30b3ee7513
Daniel Lempesis <danlrcn@hotmail.com>
2016-10-12 16:14:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 function shout(string) {
-  return string.toUpperCase
+  return string.toUpperCase()
 }
 
 function whisper(string){
-  return string.toLowerCase
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  var lowercase = "hello"
+  var mixedcase = "I love you, Grandma"
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+}
+  else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  }
