406c52032587d4671d82eba890be714fed01e76c
Emily Croft <emilyhcroft@gmail.com>
2016-10-16 16:45:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === string) {
+    return "I can't hear you!"
+    }
+
+  if (string.toUpperCase === string) {
+    return "YES INDEED!"
+  }
+
+  if !(("I love you, Grandma.".toLowerCase === string) || ("I love you, Grandma.".toUpperCase === string) {
+    return "I love you, too."
+  }
+  else if "I love you, Grandma.".toLowerCase === string {
+    return "I love you, too."
+  }
+  else if "I love you, Grandma.".toUpperCase === string {
+    return "I love you, too."
+  }
+}
