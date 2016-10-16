26a5c0d4037d166d8ebfc58c26dcee79812d419d
Jonathan Crook <jonathanrcrook@gmail.com>
2016-10-16 18:45:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  console.log('hello');
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase()
+  var uppercase = string.toUpperCase()
+
+if (string === lowercase) {
+  return "I can't hear you!"
+} else if (string === uppercase) {
+  return "YES INDEED!"
+} else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+  }
+}
