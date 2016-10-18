3b4b9fb4a699027580cf555896d48e32a46ef61f
Laziza Kodirova <kodirova.laziza@gmail.com>
2016-10-18 11:34:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function shout(string){
+function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string){
+return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase)
+}
+
+fucntion logWhisper(string){
+  console.log(string.toLowerCase)
+}
+
+//sayHiToGrandma(string){
+  //if (string.toLowerCase===)
+//}
