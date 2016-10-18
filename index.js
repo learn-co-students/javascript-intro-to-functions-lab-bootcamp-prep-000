7544e142c1807daf7e802a40c2aeaf4fedca839b
Reinier Blanco-Diaz <reinierblanco10@gmail.com>
2016-10-17 23:15:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,22 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase() )
+  console.log(string.toUpperCase() );
 }
+
+function logWhisper(string) {
+   console.log(string.toLowerCase() );
+}
+
+function sayHiToGrandma(string) {
+   if(string.toLowerCase() === string) {
+       return "I can't hear you!"
+   } else if(string.toUpperCase() === srting) {
+     return "YES INDEED!"
+   } else if( string === "I love you, Grandma.") {
+      return "I love you, too."
+   } else {
+     undefined
+   }
+
+   }
