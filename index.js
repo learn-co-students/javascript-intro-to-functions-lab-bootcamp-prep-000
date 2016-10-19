fda58e7fc35c152278963893c5f1c8a668f13be7
Katerina  Papacostas <katpapacostas@gmail.com>
2016-10-19 20:51:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+switch(string) {
+  case (string) = lowercase : {return "I can't hear you!"
+} case (string) = uppercase : {return "YES INDEED"
+} case (string) = "I love you, Grandma" : {return "I love you, too."}
+}
