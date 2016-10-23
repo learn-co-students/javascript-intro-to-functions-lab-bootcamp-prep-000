9f9508c25ab7f221eb9e47ace37ea90535a04815
Byron Staton <b.dewaynestaton@gmail.com>
2016-10-23 05:05:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string 'Hello'.toUpperCase()
+}
+function whisper(string) {
+  return string 'HELLO'.toLowerCase()
+}
+function logout(string) {
+  console.log('logout').toUpperCase()
+}
+function logWhisper(string) {
+  console.log('Hello').toLowerCase()
+}
+var sayHiToGrandma = function(string) {
+  if(string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
