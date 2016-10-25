fd0169415d9c2a56bebfc3fa4e9fbf480b26c817
Azekah Griffiths <azekah.griffiths@icloud.com>
2016-10-25 13:04:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = string
-  var uppercase = string
-  if (lowercase.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
-  else if (uppercase.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-  else if ("I love you, Grandma." === string) {
+  var lowercase = string.toLowerCase();
+  var uppercase = string.toUpperCase();
+
+  if (string === lowercase) {
+    return "I can't hear you!";
+  } else if (string === uppercase) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
