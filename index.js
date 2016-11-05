64ff7ea4d9f49562810fe1572641bbb23e21fcf9
Dante Zell <dantezell78@gmail.com>
2016-11-05 18:48:37 +0000

    Done.

--- index.js
+++ index.js
@@ -2,32 +2,26 @@ function shout(string) {
   return string.toUpperCase();
 }
 
-
-
 function whisper(string) {
-  return string.toLocaleLowerCase();
+  return string.toLowerCase();
 }
 
-
 function logShout(string) {
-  console.log(string.toUpperCase());
-
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLocaleLowerCase());
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLocaleLowerCase() === string) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
-
-if (string.toUpperCase() === string) {
-  return "YES INDEED!";
-}
-
-if (string === "I love you, Grandma.") {
-  return "I love you, too.";
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  if(string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
 }
