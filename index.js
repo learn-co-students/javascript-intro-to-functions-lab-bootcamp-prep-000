811186cd07782ef797aa96a0ec458bb95e4bdaab
Jeffrey Behnke <jpipermaru.dev@gmail.com>
2016-11-08 00:54:27 +0000

    Done.

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-  } 
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
