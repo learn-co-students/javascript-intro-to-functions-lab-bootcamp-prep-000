598f016355a6d5f1cdc6edc1febe3b9404562859
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-13 21:30:45 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log string.toUpperCase
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+      } else if (string.toUpperCase() === string) {
+        return "YES INDEED!"
+      } else string === "I love you, Grandma."
+       return  "I love you, too."
+  }
