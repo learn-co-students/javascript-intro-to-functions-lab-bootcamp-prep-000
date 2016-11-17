2db957acacaba56305ffc0a1b0458a1f7f1e1212
Bosko Tutnjilovic <bosko_tutnjilovic@live.com>
2016-11-17 06:19:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return "hello".toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string) {
+  if(string.toLowerCase) {
+    return "I can't hear you!";
+  }
+}
+function sayHiToGrandma(string) {
+  if(string.toUpperCase) {
+    return "YES INDEED!";
+  }
+}
+function sayHiToGrandma(string) {
+  if ("I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
