6b964b8bde21506cb8c0e9a2454f836582e2730e
Kato Ali <paperbythek@gmail.com>
2016-11-17 18:09:03 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
-function sayHiToGrandma(string){ if (string = string.toLowerCase() )
-    {return "I can't hear you!"} else (string === string.toUpperCase())
-    {return "YES INDEED!"} else (string = "I love you, Grandma.")
-    {return "I love you, too."}
-
+function sayHiToGrandma(string){ if (string.toLowerCase() === string )
+    {return "I can't hear you!"} else if (string.toUpperCase() === string) {
+        return "YES INDEED!"
+    } else if (string === "I love you, Grandma.") {return "I love you, too."}
 }
