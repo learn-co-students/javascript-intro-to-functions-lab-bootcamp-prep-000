870de14ca581cbbebacb8ba93780a8f4fa5f8fad
Allan (Wei Der) Peng <allanwdpeng@gmail.com>
2016-11-18 08:37:42 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  else if (string === string){
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
