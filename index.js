df6947a0456da81429a4b3bde32552987b82f8fb
Daniel Silverman <daniel.silverman@me.com>
2016-11-30 19:17:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function shout(string) {
+}
+
+function shout(string) {
   return string.toUpperCase()
+}
+
+'Hello!'.toUpperCase()
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log()
+}
+
+function sayHiToGrandma(string) {
+  return console.log()
 }
