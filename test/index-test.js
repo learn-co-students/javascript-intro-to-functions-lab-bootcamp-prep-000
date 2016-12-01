5abbc1bbac2226b1974222e6fa1dc286770032fc
Aidan Bane <aidanfbane@gmail.com>
2016-12-01 05:09:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,13 +4,13 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
-
+//
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
-
+//
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
