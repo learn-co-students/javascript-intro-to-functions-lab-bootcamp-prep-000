63afe78eb3f6d2e6752c6033f3575fcc1ccddfdd
Sarah P <stps87@gmail.com>
2016-12-02 16:04:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var lowercase = string
+  var uppercase = string
+  var mixedcase = string
+  if (lowercase.toLowerCase() === lowercase){
+    return "I can't hear you!"
+  }
+  else if (uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (mixedcase.toLowerCase() !== lowercase && uppercase.toUpperCase()!== uppercase) {
+    return "I love you, too."
+  }
+
+}
