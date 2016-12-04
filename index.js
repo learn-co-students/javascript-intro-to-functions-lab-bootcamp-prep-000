7720af65bfc490799b6ab5a4107b0a95169fa20f
Philip Trinh <phil.trinh@me.com>
2016-12-04 03:23:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  return console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  var uppercase = string.toUpperCase()
+  var lowercase = string.toLowerCase()
+
+  if(string === lowercase){
+    return "I can\'t hear you!"
+  }else if(string === uppercase){
+    return "YES INDEED!"
+  }else if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
