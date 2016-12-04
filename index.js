4fd4d8c852bd6418a4f9ed3d6fcd3f35060a145e
Chris Harvey <chharvey@gmail.com>
2016-12-04 05:55:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()}
+
+function whisper(string) {
+  return string.toLowerCase()}
+
+function logShout(string) {
+  console.log('HELLO')}
+
+function logWhisper(string) {
+  console.log('hello')}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {return "I can't hear you!"}
+  else if (string === string.toUpperCase()) {return "YES INDEED!"}
+  else if (string === "I love you, Grandma.") {return "I love you, too."}}
