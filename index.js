b2bb1dfb7b117d0ed8457bb5603b7e824a2033ad
Anwar Khan <anwarwkhan@gmail.com>
2016-12-07 05:48:43 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = "hello"
 
-  if (string === lowercase){
+  if (string === string.toLowerCase()){
     return "I can't hear you!"
-  }else if (string === uppercase){
+  }else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }else if (string === "I love you, Grandma."){
     return "I love you, too."
