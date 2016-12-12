ad8c67da48d7ad2210140fa86bd67f947e8f5cf4
Hyojin Kim <minihyojin@gmail.com>
2016-12-12 22:59:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string){
+case (string === lowercase):
+  return "I can\'t hear you!"
+  break;
+case (string === Uppercase):
+    return "YES INDEED!"
+    break;
+case (string === "I love you, Grandma.")
+return "I love you, too."
+}
