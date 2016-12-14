02641fb545fb631f6fc63fb665425178d8f51781
Sean Granberg <seangranberg@icloud.com>
2016-12-14 01:38:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase()
+  return (string.toUpperCase())
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return (string.toLowerCase())
 }
 
 function logShout(string) {
@@ -17,11 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return ("I can't hear you!")
-  }
-  else if (string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return ("YES INDEED!")
-    }
-     else if (string === "I love you, Grandma." ) {
-       return ("I love you, too.")
-     }
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
 }
