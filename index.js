14ac23eaece402eb80cc487bac0d8db2b529405e
David Kennedy <dkennedychicago@gmail.com>
2016-12-19 18:29:47 +0000

    Done.

--- index.js
+++ index.js
@@ -26,3 +26,19 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
 }
+// ####### HERE IS THE SOLUTION
+// function sayHiToGrandma(string) {
+//   if (string.toLowerCase() === string) {
+//     return "I can't hear you!"
+//   }
+//
+//   if (string.toUpperCase() === string) {
+//     return "YES INDEED!"
+//   }
+//
+//   if (string === "I love you, Grandma.") {
+//     return "I love you, too."
+//   }
+//
+//   return "Are you eating enough?"
+// }
