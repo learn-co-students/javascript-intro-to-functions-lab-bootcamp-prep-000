b36bbcedf99f6e083c476fab989a08259e548187
manshoor ahmad <dl4mobiles@gmail.com>
2016-12-21 12:51:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+
+  if(string === 'hello'){
+    return `I can't hear you!`;
+  }else if (string === 'HELLO') {
+    return `YES INDEED!`;
+  }else{
+    return 'I love you, too.';
+  }
+  
+}
