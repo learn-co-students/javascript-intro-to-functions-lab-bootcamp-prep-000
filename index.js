e30d39569e09d5008e6dae2b61a560de386c3496
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2016-12-22 04:22:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout('Hello'){
+
+  return Hello.toUpperCase();  
+}
+
+function whisper('Hello'){
+
+  return Hello.toLowerCase();  
+}
+
+function logShout('Hello'){
+
+
+  console.log();
+}
