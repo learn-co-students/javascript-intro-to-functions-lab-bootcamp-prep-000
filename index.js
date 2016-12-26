49451df14bed0217625a7c6ee03526d0ee35e502
German Ruiz <ruiz.german24@yahoo.com>
2016-12-26 04:48:43 +0000

    Done.

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ function logWhisper(string) {
   console.log ( string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-  }
-
-}
-break:
-
-function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-    return 'YES INDEED!';
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else {
+    return ("I can't hear you!")
   }
 }
