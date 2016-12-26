b5528af9bcb6a7b49b7318b2b1dbae79f1bf0d04
Ursh Khan <urshkhan@gmail.com>
2016-12-26 01:10:25 +0000

    Done.

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-  return `I can\'t hear you!`
-  else if (string.toUpperCase() === string)
-  return "YES INDEED!"
-  else if ('I love you, Grandma.' === string)
-  return "I love you, too."
-
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === string){
+    return "I can't hear you!"
+  } else if(string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if("I love you, Grandma." === string) {
+    return "I love you, too."
+  }
 }
