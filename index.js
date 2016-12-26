e486e3d9e6c409c355b6b5381b18649e2312fcd0
Tory Sigmond <tesigmond@gmail.com>
2016-12-26 21:54:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase()===string){
+    return "I can't hear you"
+  } elseif(string.toUpperCase()===string){
+    return "YES INDEED!"
+  } elseif (string==="I love you, Grandma."){
+    "I love you, too."
+  } else{
+    return""
+  }
+}
