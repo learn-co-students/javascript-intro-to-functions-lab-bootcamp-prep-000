96a3e27497d855319a1a806181fffaea485d6ece
Mike Tangaro <mtangaro@gmail.com>
2016-12-31 18:29:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function () {
+  return (logShout(string)) {
+  console.log(logShout(string).toUppercase)
+}
