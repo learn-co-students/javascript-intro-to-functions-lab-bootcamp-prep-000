fa1d407aed6abe832988ad93533f5d167a240524
Mike Tangaro <mtangaro@gmail.com>
2016-12-31 18:42:44 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function () {
-  return (logShout(string)) {
-  console.log(logShout(string).toUppercase)
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
