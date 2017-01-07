0e5c260932663cab8b422f09ff24980640135858
Alexx Mitchell <alexxmitchell25@gmail.com>
2017-01-07 19:50:53 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase() === lowercase {
+  if (string.toLowerCase() === string)  {
   return "I can\'t hear you!"
 }
-  else string.toUpperCase() === uppercase {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
     }
-  else if string("I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
   }
