ef25ee2bede60e496de33c34480b7660558ca2a5
Bradley Grey <bradleyjgrey@gmail.com>
2017-01-08 22:33:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+function shout(string) {return string.toUpperCase()}
+function whisper(string) {return string.toLowerCase()}
+function logShout(string) {console.log(string.toUpperCase())}
+function logWhisper(string){console.log(string.toLowerCase())}
+function sayHiToGrandma(string) {if (string === lowercase) {return "I can't hear you!"}}
