3eef21210bdb271571f7200e319ff28efdeafc8e
nathalie garcia <nathalie.scruz@gmail.com>
2017-01-09 17:36:03 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase() {
+  if (string.toLowerCase() === string) {
     return 'I can\'t hear you!'
   }
-  else if string.toUpperCase() {
+  else if (string.toUpperCase() === string) {
     return 'YES INDEED!'
   }
   else if ('I love you, Grandma.') {
