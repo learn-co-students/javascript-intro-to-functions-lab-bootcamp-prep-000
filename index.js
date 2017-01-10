0460799b2a083e92d45a2b2a06057a77468ce706
Philippa Quadt <philippa.quadt@gmail.com>
2017-01-10 11:49:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 function shout(string) {
-  return string.toUpperCase ()
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase ()
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log('HELLO'.toUpperCase())
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('hello'.toLowerCase())
+  console.log(string.toLowerCase())
 }
-return 'YES INDEED!'
 
 function sayHiToGrandma(string) {
-  if ('string'.toLowerCase()) {
-    return 'I can\'t hear you!' 
-  } else if ('string'.toUpperCase()) {
-    return 'YES INDEED!'
-  } else if ('I love you, Grandma.') {
-    return 'I love you, too.'
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-}
 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
