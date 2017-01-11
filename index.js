d0697abc1c4254de808a7658dc050a99d5a3acf5
Caroline Shields <caroline.shields@amkor.com>
2017-01-11 22:26:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function shout(string) {
   return string.toUpperCase()
 }
@@ -14,24 +16,12 @@ function logWhisper(string) {
 console.log(string.toLowerCase())
 }
 
-
-
-
-function sayHiToGrandma(string){
-  if (string=="I love you, Grandma."){
-    return "I love you, too."
-  }
-}
-
-
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
-  }
+}else if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+}else if (string == "I love you, Grandma.") {
+  return "I love you, too."
 }
-
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase()){
-    return "YES INDEED!"
-  }
 }
