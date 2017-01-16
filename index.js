d5c3b5a51b6970a2f131d3c2237bd0cc40261485
Santita Seesod <ant.csod@gmail.com>
2017-01-16 12:47:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) {
+  return ("I can\'t hear you!")
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase()) {
+    return ("YES INDEED!")
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string `"I love you, Grandma."`)
+  return ("I love you, too.")
+  }
+}
