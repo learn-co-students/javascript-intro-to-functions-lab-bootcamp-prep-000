9a2a6ec7f887acd26d162a972f341eb3220d4050
stephen chun <stephenchun91@gmail.com>
2017-01-18 07:50:36 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } 
 
-  if (string === "I love you, Grandma.")
+  if (string === "I love you, Grandma.") {
     return "I love you, too.";
   } 
 
