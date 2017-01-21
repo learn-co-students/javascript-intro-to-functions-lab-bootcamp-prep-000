1ed7dd07bb8e9d50c515a46cf2d20676b22adc53
Zac McClung <zac.mcclung370@gmail.com>
2017-01-21 18:54:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+
+function sayHiToGrandma(string) {
+  if(string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+
+function sayHiToGrandma(string) {
+  if(string === string('I love you, Grandma')) {
+    return "I Love you too!"
+  }
