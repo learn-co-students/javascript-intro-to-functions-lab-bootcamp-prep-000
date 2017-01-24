fcc45ab2dddbeac0a6509b1212deb91859d25b83
Kristy Peterson <kpeterson.kap@gmail.com>
2017-01-24 05:23:17 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', () => {
+  it('receives one argument and returns it in all caps', () => {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
