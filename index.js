9d1e9291813af3d83d32aec70b148dd9358579b7
Roque Mesa <roque.mesa@gmail.com>
2017-01-26 10:27:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout (string) {
+  return string.toUpperCase ()
+}
+
+function whisper (string) {
+  return string.toLowerCase ()
+}
+
+function logShout (string) {
+  console.log() string.toUpperCase ()
+}
+
+function logWhisper (string) {
+  console.log () string.toLowerCase ()
+}
+
+function sayHiToGrandma (string) {
+  if (lowerCase){
+  return "I can't hear you!"}
+  }
+
+
+function sayHiToGrandma (string) {
+  if (upperCase)
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma (string) {
+  if ("I love you, Grandma.")
+  return "I love you, too."
+}
