dd24094e4bb065109efd92ece9b7519384c7edb2
Terrance Koar <terrancekoar@gmail.com>
2017-01-27 21:34:48 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (string === "I love you, Grandma") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
