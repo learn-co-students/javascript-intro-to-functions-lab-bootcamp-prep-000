0d64eef9c124251da8a9d5941af99cba42569c82
Jess Smith <jsmitten21@gmail.com>
2017-01-27 10:08:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log ('hello'.toUpperCase())
+}
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if ('string' === lowerCase)
+  return "I can't hear you!"
