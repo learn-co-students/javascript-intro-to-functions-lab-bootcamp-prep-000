cce3efc79b5ffe436329b2694f1d6457e6b70f63
Tayeb Mc <gomail654@gmail.com>
2017-01-29 17:22:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(){
+  return 'hello'.toUpperCase()
+}
+function whisper(){
+  return 'HELLO'.toLowerCase()
+}
+function logShout(){
+    console.log('hello'.toUpperCase());
+}
+function logWhisper(){
+    console.log('HELLO'.toLowerCase());
+}
+
+function sayHiToGrandma(){
+  var uppercase="HELLO";
+  var lowercase="hello";
+
+
+    if lowercase.toLowerCase()==="hello" {
+      return"I can't hear you!"
+    }
+    else if uppercase.toUpperCase()==="HELLO"{
+      return"YES INDEED!"
+    }
+    else "I love you, Grandma"{
+      return"I love you, too."
+    }
+  }
