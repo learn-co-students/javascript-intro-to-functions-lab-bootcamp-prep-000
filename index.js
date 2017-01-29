626e4d2e4079ec46b77cd56983bafb9522a013c9
Charlay Davenport <charlayd1993@gmail.com>
2017-01-29 04:17:08 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout() {
-  console.log()
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  debugger;
+if (string.toLowerCase() === string) {
+    return "I can\'t hear you!" }
+else if (string.toUpperCase() === string) {
+  return "YES INDEED!" }
+else {
+   return "I love you, too."
+ }
 }
