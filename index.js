300e1394ce5c4e6bae0cadb08c40a987a1f6301e
Katie McBride <archmcbride@hotmail.com>
2017-01-29 05:14:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {console.log(string.toUpperCase())}
+  logShout("hello")
+
+function logWhisper(string) {console.log(string.toLowerCase())}
+ logWhisper("HELLO")
+
+ function sayHiToGrandma(string) {
+   if (string.toLowerCase() === string) {
+     return "I can\'t hear you!"}
+   else if (string.toUpperCase() === string){
+     return "YES INDEED!"}
+   else (string === ("I love you, Grandma."))
+     return "I love you, too."}
+     
