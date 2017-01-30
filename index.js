6ca1ac8d9eea1dc579ac73db107a87dc3d10b46f
Jacqlene Sifre <jsifre01@gmail.com>
2017-01-30 22:15:49 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log ('HELLO')
+}
+
+function logWhisper(string) {
+  console.log ('hello')
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  var uppercase = 'HELLO!'
+  var mixedCase = "I love you, Grandma."
+
+  uppercase.toUpperCase() === uppercase
+  lowercase.toLowerCase() === lowercase
+  
+}
