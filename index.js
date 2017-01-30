60dadd1384860838f7b53294952bf0b5abc4623d
daniel byun <byunsta@gmail.com>
2017-01-30 05:58:06 +0000

    Done.

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ function logWhisper(string){
 // MOTHER FUCKER FIGURE IT OUT BITCHFACE
 function sayHiToGrandma(string){
 
-  sayHiToGrandma('hello') = "I can't hear you!";
-  
+    if (string.toLowerCase() === string){
+
+        return "I can't hear you!";
+
+    } else if (string.toUpperCase() === string){
+
+        return "YES INDEED!";
+
+    } else if (string === "I love you, Grandma."){
+
+        return "I love you, too.";
+
+    }
 
 }
