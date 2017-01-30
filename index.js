41bcf378918165a3ba821f7cfc3def9847b402f4
daniel byun <byunsta@gmail.com>
2017-01-30 05:55:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+
+  return string.toUpperCase();
+
+}
+
+function whisper(string){
+
+  return string.toLowerCase();
+
+}
+
+function logShout(string){
+
+  console.log("hello".toUpperCase());
+
+}
+
+function logWhisper(string){
+
+  console.log("HELLO".toLowerCase());
+
+}
+// MOTHER FUCKER FIGURE IT OUT BITCHFACE
+function sayHiToGrandma(string){
+
+  sayHiToGrandma('hello') = "I can't hear you!";
+  
+
+}
