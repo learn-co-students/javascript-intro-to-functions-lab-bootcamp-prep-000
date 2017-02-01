ed52d902b0fef43b18a09b8afa32f6ae53ba23b9
Lucas Duffy <lduffy@wesleyan.edu>
2017-02-01 16:51:41 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) { console.log(.toUpperCase(string)) }
+function logShout(string) { console.log(string.toUpperCase()) }
+
+function logWhisper(string) { console.log(string.toLowerCase()) }
+
+function sayHiToGrandma(string) {
+
+  var uppercase = "HELLO!"
+
+  if (uppercase.toUpperCase() === uppercase) {return "Yes Indeed!"} // true
+
+  var lowercase = 'hello!'
+
+  if (lowercase.toLowerCase() === lowercase) {return "I can\'t hear you!"}// true 
+
+  var mixedCase = 'Hi there!'
+
+  mixedCase.toLowerCase() === mixedCase // false
+
+  mixedCase.toUpperCase() === mixedCase // false
+
+  else if (string==="I love you, Grandma.") {return "I love you, too."}
+}
