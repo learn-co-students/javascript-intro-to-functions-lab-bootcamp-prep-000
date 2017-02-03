ec8599e34d10edc03ea153b89abd8da9421ae780
Mandy Losk <losk.m.jchs@gmail.com>
2017-02-03 08:02:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase ()
+  }
+  function whispher(string) {
+    return string.toLowerCase ()
+
+  function logShout(string){
+    console.log string.toUpperCase
+  }
+  
+  function logWhisper(string) {
+    console.log string.toUpperCase
+  }
+  function sayHitoGrandma(string) {
+    return string.toLowerCase
+  }
+function sayHitoGrandma(string) {
+  return string.toUpperCase
+}
+function sayHitoGrandma(string) {
+  return string.
+}
