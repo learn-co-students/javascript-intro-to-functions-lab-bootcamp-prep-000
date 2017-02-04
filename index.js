bdc622f989e86c1f40271fe28cbcf20cc83f0134
Christina Gin <cgin1211@gmail.com>
2017-02-04 19:59:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase()
+  if (string.toLowerCase()===lowercase) {
+  return "I can\'t hear you!"}
+  var uppercase =string.toUpperCase()
+  if (string.toUpperCase()===uppercase){
+  return "YES INDEED!"}
+  var string = "I love you, Grandma."
+}
