d61deeafe6dbf39fa7c073f8886e2e50d2c2abfd
Veronica Acosta <veronica.acosta@ymail.com>
2017-02-07 16:58:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) { return string.toUpperCase()}
+function whisper(string) { return string.toLowerCase()}
+function logShout(string) {
+var uppercase = "HELLO"
+console.log(uppercase)}
+function logWhisper(string) {
+var lowercase = "hello"
+console.log(lowercase)}
+function sayHiToGrandma(string) {
+if(string === string.toLowerCase()) {
+return("I can\'t hear you!")};
+if (string === string.toUpperCase()) {
+return("YES INDEED!")};
+if (string === "I love you, Grandma.") {
+return("I love you, too.")
+    }
+  }
+  
