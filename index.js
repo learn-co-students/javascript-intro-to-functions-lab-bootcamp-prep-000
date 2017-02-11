830d69d6b7dc8103edc5b6470bfd22209e682eb2
Nate Lee <nleepercussion@gmail.com>
2017-02-11 17:13:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = string
+  var lowercase = string
+  if (uppercase.toUpperCase() === uppercase)
+    return ("YES INDEED!");
+  else if (lowercase.toLowerCase() === lowercase)
+    return ("I can't hear you!");
+  else if (string === "I love you, Grandma.")
+    return ("I love you, too.")
+}
