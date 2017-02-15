2d359c4d8b2e39b847dc99f88a8eaaa8e84ae5be
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2017-02-15 15:46:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+/*Problem # 1*/
+function shout(string) {
+  return string.toUpperCase()
+}
+
+/*Problem #2*/
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+/*Problem #3*/
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+/*Problem # 4 */
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+/*Problem #5 */
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase
+  string = lowercase
+  return "I can't hear you!";
+
+  var uppercase = string.toUpperCase
+  string = uppercase
+  return "YES INDEED!";
+
+  var "I can\'t hear you!"
+  "I can\'t hear you!" = 'YES INDEED!';
+
+
+  string = "I love you, Grandma."
+  return "I love you, too."
+}
