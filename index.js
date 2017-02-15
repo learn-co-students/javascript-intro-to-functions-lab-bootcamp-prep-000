8c0dfa903c5756b62f6aa72c289898cd0eb60aa8
tanner strobel <tanner.strobel@live.com>
2017-02-15 02:58:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if
+  {
+  var lowercase = "hello!";
+string.toLowerCase() === lowercase;
+return "I can't hear you!";
+}
+else if {
+  var uppercase = "HELLO!";
+  string.toUpperCase() === uppercase;
+  return "YES INDEED!";
+}
+else {
+  var mixedcase = "I love you, Grandma.";
+  string === mixedcase;
+  return "I love you, too.";
+}
+}
