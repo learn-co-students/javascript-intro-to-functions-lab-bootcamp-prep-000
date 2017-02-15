bcb83a98db476eb06d740db1c490a076e3feaf52
Moshe Delerb <mydelerb@gmail.com>
2017-02-15 16:40:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  var lowercase=
+  string.toLowerCase
+  return "I can't hear you!";
+
+  var uppecase=
+  string.toUpperCase
+  return "YES INDEED!";
+
+  string = "I love you, Grandma."
+  return "I love you, too."
+}
