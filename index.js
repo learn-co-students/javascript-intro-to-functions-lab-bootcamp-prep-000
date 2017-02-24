24659fbca060cde74c8e8082578b07e8b903990e
Andrew Cao <andrew.cao@live.com>
2017-02-24 00:01:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+    var uppercase = string.toUpperCase()
+    var lowercase = string.toLowerCase()
+        
+    if (string === uppercase) {
+        return "YES INDEED!"
+    }
+      
+    if (string === lowercase) {
+        return "I can't hear you!"
+    }
+
+    if (string === "I love you, Grandma.") {
+        return "I love you, too."
+    }
+
+    
+    
+
+
+}
+
+
