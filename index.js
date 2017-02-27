3653557c4285503b63af99d0472c82ced50d6137
Josie Kelley <jcakelley@gmail.com>
2017-02-27 15:47:36 +0000

    Done.

--- index.js
+++ index.js
@@ -15,18 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase() === string){
+  if (string.toLowerCase() === string){
   return "I can\'t hear you!"
   }
-}
-
-function sayHiToGrandma(string){
-  if(string.toUpperCase() === string){
+  else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-}
-
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma.")
+  else if (string === "I love you, Grandma."){
   return "I love you, too."
+}
 }
