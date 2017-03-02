1e8c227e3cfedbc031f07902772dce0af611e7b8
Jaemie Lee <jaelee90@gmail.com>
2017-03-02 21:19:22 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
