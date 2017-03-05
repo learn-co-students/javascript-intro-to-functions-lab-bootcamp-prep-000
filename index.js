621eafa37fa4b5c262e46e2806f967287ea370b0
Timothy Cevallos <timcev@gmail.com>
2017-03-05 13:13:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  
+  var lowercase = string
+  
+   if ( lowercase.toLowerCase() === lowercase) {
+    return "I can\'t hear you!"
+  }
+    var uppercase = string
+    
+    else if ( uppercase.toUppercase() === uppercase) {
+    return "YES INDEED!" 
+      
+    }
+
+    else (string === "I love you") {
+  return "I love you, too."
+}
+}
