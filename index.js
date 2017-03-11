199238ab736baf0393825920fd4c8b41b630f528
Taumata Rolleston <taumata21@gmail.com>
2017-03-11 09:51:05 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string.toLowerCase());
-  return 'I can\'t hear you!'.toLowerCase;
+  if (string.toLowerCase() === string) {
+      return 'I can\'t hear you!';
+  }
+
+  if (string.toUpperCase() === string) {
+      return "YES INDEED!";
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+
+    return "Are you eating enough?";
 }
