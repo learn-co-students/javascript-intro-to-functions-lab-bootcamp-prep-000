e10a5c9b6a47c533ba6a0427966e9b0d936698f6
Taumata Rolleston <taumata21@gmail.com>
2017-03-11 08:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string.toLowerCase()) {
-    return string.toLowerCase();
-  }
+  if (string.toLowerCase() === string.toLowerCase());
+  return 'I can\'t hear you!'.toLowerCase();
 }
