efe0bd73c80a15ab469c63ed3b54359e57f69009
Daniel Ribeiro <asthedayislong@gmail.com>
2017-03-13 20:43:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+function logWhisper (string) {
+  console.log (string.toLowerCase)
+   spyOn (hello.toHaveBeenCalledWith)
+}
+
+function logShout (string) {
+  console.log (string.toUpperCase)
+  spyOn (HELLO.toHaveBeenCalledWith)
+}
+
+function sayHiToGrandma (string) {
+  if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+
+  else if (string === "hello") {
+    return ("I can\'t hear you!")
+  }
+    else if (string === "HELLO") {
+      return ("YES INDEED!")
+    }
+}
