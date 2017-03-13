3386b8d57b0b733c1a401bc5918cd97d86cd6de1
Tabitha Sin <tabitha.sin@gmail.com>
2017-03-13 02:50:57 +0000

    Done.

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHitoGrandma(string) {
-  if (string === string.toLowerCase()) {
-  return "I can't hear you!";
-} else if (string === string.toUpperCase()) {
-  return "YES INDEED!";
-} else if (string === "I love you, Grandma") {
-  return "I love you, too.";
-}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
