3fb01bf219e46bbb9fca549d160b14e68b6fe24e
Hawoly Ba <hawolyba@gmail.com>
2017-03-14 21:20:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = string.toUpperCase();
+  var lowercase = string.toLowerCase();
+  if (string === uppercase) {
+  return "YES INDEED!";
+} else if (string === lowercase) {
+  return "I can't hear you!";
+} else (string === "I love you, Grandma.")
+    return "I love you, too.";
+}
