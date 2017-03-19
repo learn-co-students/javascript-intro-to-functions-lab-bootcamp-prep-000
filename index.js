d5d2fe57862bffd63975a0bf67f05bd550458597
Edgar Pavlovsky <edgarpavlovsky@gmail.com>
2017-03-19 20:55:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(s){
+  return s.toUpperCase()
+}
+
+function whisper(s){
+  return s.toLowerCase()
+}
+
+function logShout(s){
+  console.log(s.toUpperCase())
+}
+
+function logWhisper(s){
+  console.log(s.toLowerCase())
+}
+
+function sayHiToGrandma(s){
+  if (s === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (s === s.toUpperCase()) {
+    return "YES INDEED!"
+  } else {
+    return "I can't hear you!"
+  }
+}
