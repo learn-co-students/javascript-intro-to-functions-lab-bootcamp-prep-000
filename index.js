dc19b492d4991c2195950219246a3996a2b03eaf
Sarah Murphy <s.grossblatt@gmail.com>
2017-03-19 14:30:17 +0000

    Done.

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function  logWhisper (string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
-    return `I can't hear you!`
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
   }
-  else if (string === string.toUpperCase){
-    return `YES INDEED!`
+  else if (string === string.toUpperCase()){
+    return `YES INDEED!`;
   }
   else if (string === "I love you, Grandma.") {
-    return `I love you, too.`
+    return `I love you, too.`;
   } {
 
   }
