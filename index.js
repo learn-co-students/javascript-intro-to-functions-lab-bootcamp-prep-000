c8319bf2fc990ad599395a3ee73a8602d6838ecf
Christina Cornevin <ccornevin@hotmail.com>
2017-03-21 01:45:11 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (var lowercase = 'hello' lowercase.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
   return "I can't hear you!"}
-  else if (var uppercase = "HELLO" uppercase.toUpperCase() === uppercase) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"}
-}
-
-function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
