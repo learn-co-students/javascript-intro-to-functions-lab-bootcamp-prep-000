a95053a946e9c4223831f61d661c4893e45f25f6
Christina Cornevin <ccornevin@hotmail.com>
2017-03-21 01:42:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+  }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (var lowercase = 'hello' lowercase.toLowerCase() === lowercase) {
+  return "I can't hear you!"}
+  else if (var uppercase = "HELLO" uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!"}
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
