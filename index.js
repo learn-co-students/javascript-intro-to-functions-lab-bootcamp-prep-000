c111eaf00056a8e7cf7be5b3ceac20ad309aa2db
Chris Wood <woodcr5@gmail.com>
2017-03-23 22:24:16 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  switch(string) {
-    case string.toLowerCase() == string:
-      return 'I can\'t hear you!'
-      break;
-    case string.toUpperCase() == string:
-      return 'YES INDEED!'
-      break;
-    case 'I love you, Grandma.':
-      return 'I love you, too.'
-      break;
+  if(string.toLowerCase()===string) {
+    return "I can't hear you!";
+  } else if(string.toUpperCase()===string) {
+      return "YES INDEED!";
+    } else {
+      return "I love you, too.";
+    }
   }
-}
