7f5bab708305638f2173e6bac8e4029917a23e44
Chris Wood <woodcr5@gmail.com>
2017-03-23 22:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   } else if(string.toUpperCase()===string) {
       return "YES INDEED!";
     } else {
-      return "I love you, too.";
+      return "I love you, too."
     }
   }
