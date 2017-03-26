8a61e5010c4b7d2ccccf22b8215fd2fa988dbc8b
Dave Pinchoff <dpinchoff@gmail.com>
2017-03-26 20:58:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  var upper = string.toUpperCase()
+  console.log(upper)
+}
+
+function logWhisper(string) {
+  var lower = string.toLowerCase()
+  console.log(lower)
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
