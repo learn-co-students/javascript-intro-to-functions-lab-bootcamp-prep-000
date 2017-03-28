4a7b10f0b58f0f16f9969f8435b5471522b1d791
Robert Ciaccio <robjciaccio@gmail.com>
2017-03-28 22:36:39 +0000

    Done.

--- index.js
+++ index.js
@@ -5,16 +5,27 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log('hello'.toUpperCase())
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('HELLO'.toLowerCase())
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string === ('hello')) {
-    "I can\'t hear you"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
