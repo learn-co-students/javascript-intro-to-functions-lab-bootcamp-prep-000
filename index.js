7c316b79a09b1602e32806577633a2fa411fdcad
viba viba <vibasilly@gmail.com>
2017-03-29 21:36:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+shout('hello')
+
+function (whisper(string){
+  return'HELLO'.toLowerCase()
+}
+whisper('HELLO')
+
+
+function logShout(string) {
+  console.log('hello').toUpperCase()
+  }
+
+logShout('hello')
+
+
+function sayHiToGrandma(string) { if (string === 'hello')
+return ("I can\'t hear you!");
+}
+elseif (string === 'HELLO');
+return ('YES INDEED!');
+ }
+
+sayHiToGrandma('hello')
