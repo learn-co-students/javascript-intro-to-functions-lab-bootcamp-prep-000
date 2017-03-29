d753c2aac6bcfa26ca8c6466466c1d4e9c76f9ac
Kilie Donahue <kiliedonahue@yahoo.com>
2017-03-29 19:16:24 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  return string.toUpperCase()
-  console.log("HELLO");
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()===string){
+    return "I can't hear you!"
+  } else if (string.toUpperCase()===string){
+    return "YES INDEED!"
+  } else if (string ==='I love you, Grandma.'){
+    return "I love you, too."
+  }
 }
