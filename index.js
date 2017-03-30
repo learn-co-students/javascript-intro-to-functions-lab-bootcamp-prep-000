0ebb6e386c8c61d22c91c3cab1bca0cbcacb0dd3
Dana Phillips <dana.e.phillips@gmail.com>
2017-03-30 14:59:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logshout(string) {
+  console.log(logshout(string.toUpperCase())
+}
+
+function logwhisper(string) {
+  console.log(logwhisper(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+   return "I can't hear you!";
+ } else if (string === string.toUpperCase()) {
+   return "YES INDEED!";
+ } else if (string === "I love you, Grandma.") {
+   return "I love you, too.";
+ }
+}
