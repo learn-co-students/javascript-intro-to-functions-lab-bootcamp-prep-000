874230bce3587741dba84783a654e7c9034864eb
Joe Cutajar <jcutajar12@gmail.com>
2017-03-30 18:41:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase){
+    return("I can't hear you!")
+  }
+  if(string.toUpperCase){
+    return("YES INDEED!")
+  }
+  else(string === "I love you, Grandma.")
+}
+
+
