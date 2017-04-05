77e137e592ed943ca7a1331fb98a2292480ade21
James Bland <james.kyle.bland@gmail.com>
2017-04-05 14:15:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout (string) {
+  return string.toUpperCase ()
+}
+function whisper (string) {
+  return string.toLowerCase ()
+}
+
+function logShout (string) {
+  return console.log('HELLO')
+}
+
+function logWhisper (string){
+  return console.log('hello')
+}
+
+function sayHiToGrandma (string){
+  if (uppercase.toUpperCase() === uppercase) {
+    var uppercase = "GGASDF"
+    return console.log("YES INDEED!")
+  }
+  else if (lowercase.toLowerCase() === lowercase) {
+    var lowercase = "xsdfa"
+    return console.log("I can't hear you!")
+  }  
+  else (mixedCase.toUpperCase() === mixedCase) {
+    var mixedCase = "I love you, Grandma."
+    return console.log("I love you, too.")
+  }  
+}
