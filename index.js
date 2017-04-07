f34832fe6c203b8473c4f7f1e9bb3269218f6671
Jacob Tandberg <jaarta99@hotmail.com>
2017-04-07 04:02:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+var uppercase = "HELLO"
+var lowercase = "hello"
+
+function sayHiToGrandma(string) {
+   string.toLowerCase
+}
