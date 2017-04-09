2a89f4f1cae31ee0a5d2bcc4d610fa0692ef2548
Sarah Goshman <sarah.goshman@gmail.com>
2017-04-09 02:41:14 +0000

    Done.

--- index.js
+++ index.js
@@ -14,13 +14,10 @@ function logWhisper(string) {
   console.log(whisper(string));
 }
 
-var uppercase = "HELLO!"
-var lowercase = "hello!"
-
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
-    return "I can't hear you!"
-  } else if (string === uppercase) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma."){
     return "I love you, too."
