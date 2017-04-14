1c8438a01012a39f6ad4014f46a0652df9d743aa
Samuel Booth-Aalund <samaalund12@gmail.com>
2017-04-14 03:17:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return 'Hello'.toUpperCase()
+}
+function whisper(string) {
+  return 'Hello'.toLowerCase()
+}
+function logShout(string) {
+console.log('HELLO')
+  return 'Hello'.toUpperCase()
+}
+function logWhisper(string) {
+console.log('hello')
+  return 'Hello'.toLowerCase
+}
+function sayHiToGrandma(string) {
+if (string === string.toLowerCase())
+return "I can't hear you!"
+else if (string === string.toUpperCase())
+return "YES INDEED!"
+var mixedCase = "I love you, grandma." 
+return "I love you, too."
+}
