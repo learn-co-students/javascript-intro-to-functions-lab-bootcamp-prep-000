bc963d6aa2653dc4cd64fbcebdbba7998260de87
Indah S <ikywindah@gmail.com>
2017-04-17 13:08:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if (string.toUpperCase()) {
+    return "YES INDEED!"
+  } else (string.toEqual()) {
+    return "I love you, too."
+  }
+  }
