38ceb1930d794c98a0221d8ee0fd98ca910d895f
Robert Slagerman <slage008@gmail.com>
2017-04-18 21:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function sayHiToGrandma(string) {
     if(string === lowercase) {
       return "I can't hear you!"
   } else if(string === uppercase) {
-      return "I can't hear you!"
-  } else if(string === "I love you, Grandma"){
+      return "YES INDEED!"
+  } else if(string === "I love you, Grandma."){
       return "I love you, too."
   }
 }
