f1598e828d6755ba144fe709e3b20089e595ee3c
Siddhant  Rao <siddrao94@gmail.com>
2017-04-18 16:47:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  var x=string.toUpperCase()
+   console.log(x)
+}
+function logWhisper(string){
+  var x=string.toLowerCase()
+   console.log(x)
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===string)
+      return "I can't hear you!"
+  else if (string.toUpperCase()===string)
+      return "YES INDEED!"
+  else if (string==="I love you, Grandma.")
+      return "I love you, too."
+}
