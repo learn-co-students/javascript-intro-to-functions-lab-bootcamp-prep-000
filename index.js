845d847e5f11900737826cb78a65657129d0ce17
Lyon Muswere <tmuswere9@gmail.com>
2017-04-19 19:59:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+``` javascript
+
+var string = "hola com estaś"
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){
+    console.log("I can't hear you!")
+    else {
+      console.log("YES INDEED!")
+    }
+  }
+  if (string = "I love you"){
+    console.log("I love you, too.")
+  }
+}
+```
