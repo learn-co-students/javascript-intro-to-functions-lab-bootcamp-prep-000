37932f0027c914d496bbfa6a5921f414dbe6d10b
LMarina7 <LMarina7@users.noreply.github.com>
2017-04-21 12:21:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can\'t hear you!"
+    return "I can't hear you!"
   }
   else if (string === string.toUpperCase()) {
     return "YES INDEED!"
