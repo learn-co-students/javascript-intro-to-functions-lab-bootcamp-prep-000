ec069f323a9c070a7b73273852e77207ca8c1359
Richard Averill <rpaverill1055@gmail.com>
2017-04-22 03:48:07 +0000

    Done.

--- index.js
+++ index.js
@@ -18,24 +18,18 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-//function sayHiToGrandma(string)
-//{
-//  if(string === string.toLowerCase())
-//  {
-//    return("I can't hear you!")
-//  }
-//}
-
-//function sayHiToGrandma(string)
-//{
-//  if(string === string.toUpperCase())
-//  {
-//    return("YES INDEED!")
-//  }
-//}
-
 function sayHiToGrandma(string)
 {
+  if(string === string.toLowerCase())
+  {
+    return("I can't hear you!")
+  }
+
+  if(string === string.toUpperCase())
+  {
+    return("YES INDEED!")
+  }
+
   if(string === "I love you, Grandma.")
   {
     return("I love you, too.")
