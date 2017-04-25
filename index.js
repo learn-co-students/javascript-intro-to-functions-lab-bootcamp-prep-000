6e9f2a72a52bcd881db93a72d5e879ec38af16f9
Michelle Martin <theintrovextrov@gmail.com>
2017-04-25 20:39:17 +0000

    Done.

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase);
 }
-logShout('HELLO')
+logShout(string)
 
 function logWhisper(string) {
   console.log(string.toLowerCase);
 }
-logWhisper('hello')
+logWhisper(string)
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase === string)
-    return 'YES INDEED!';
-    else if
-      (string.toLowerCase === string)
-      return 'I can\'t hear you!';
+  if (string.toLowerCase === lowerCase)
+return 'I can\'t hear you!';
+  else if (string.toUpperCase === upperCase)
+      return 'YES INDEED!';
   else
-  return 'I love you, too.';
+    return 'I love you, too.';
     }
