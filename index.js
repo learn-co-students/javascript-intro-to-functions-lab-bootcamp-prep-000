f67a8a67e7b5dbeeba1ae71da395bc994bf5660a
Megan Shea <mkrbsb@yahoo.com>
2017-04-25 02:31:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase ("I can\'t hear you!")
+if string.toUpperCase ("YES INDEED!")
+
+}
+
+function sayHiToGrandma(string) {
+  return "YES INDEED!"
+}
