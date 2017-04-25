b2fdf0d36bfb112180a970e638497d1532c34918
Ali Adams <ali@aliadams.net>
2017-04-25 12:51:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+
+function whisper (string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toLowerCase())
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  return 'I can\'t hear you!'
+} function sayHiToGrandma(string){
+  return 'YES INDEED!' 
+}
