4a4c8ce910d6076ef61245c2fc3a2417aca8e8e3
Jose Ortiz <jro1025@gmail.com>
2017-04-26 20:32:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+console.log("the string is",string)
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+
+function sayHiToGrandma(string) {
+    return '"I can't hear you!" if `string` is lowercase'
+}
