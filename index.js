7ab3c5092f93acec31afe6d39063abf01b5faaf5
Danielle Bennett <daniellebennettchan@gmail.com>
2017-04-27 23:56:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  return 'I can\'t hear you!'.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return 'YES INDEED!'.toUpperCase()
+}
+
+function sayHiToGrandma('I love you, Grandma') {
+  return 'I love you too.'
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
