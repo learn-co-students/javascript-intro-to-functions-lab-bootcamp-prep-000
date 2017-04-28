6f6051d49eafaa38d1d2c269ce316612da2e141c
Kelvin Minaya <k.minaya@aol.com>
2017-04-28 23:58:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function shout(string){
+return string.toUpperCase(string)
+}
+function whisper(string){
+return string.toLowerCase(string)
+}
+function logShout(string){
+  console.log('HELLO');
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+
+
+
+  }
