01fbea4d61e7581ce530fe88d021033837c43ea2
James Smith <smithjames198955@yahoo.com>
2017-05-02 21:04:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout (string){
+  return string.toUpperCase()
+}
+function whisper (string) {
+  return string.toLowerCase()
+}
+function logShout (string){
+  console.log(string.toUpperCase())
+}
+function logWhisper (string){
+console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var uppercase = "YES INDEED!"{
+   
+  uppercase.toUpperCase() === uppercase 
+}   
+  var lowercase = 'I can\'t hear you!'{
+   
+  lowercase.toLowerCase() === lowercase 
+}   
+
+return var
+}
+
