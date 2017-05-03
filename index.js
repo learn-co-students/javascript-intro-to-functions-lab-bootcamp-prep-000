c1ef186fb7da418ca526cf37d03a8c69dfb437d3
Todd Ciccarelli <todd.ciccarelli@gmail.com>
2017-05-03 15:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-function logshout(string){
-  console.log(toUpperCase)
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+  return "Are you eating enough?"
 }
