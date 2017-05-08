d6e2b5e0a30dfbe089dd1811f0ba5513a953afba
Brooke Meitzler <brooke@curtsyapp.com>
2017-05-08 13:56:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+console.logShout('HELLO')
+console.logWhisper('hello')
+
+function sayHiToGrandma(string){
+  if ('hello'){
+    return "I can\'t hear you!"}
+  else if ('HELLO'){
+    return "YES INDEED!"}
+  else {
+    return "I love you, too."
+  }
