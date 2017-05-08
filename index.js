f7f1494bb078159acc6ed8e6ddedda088181cb50
Brent  Ransom <sadumedusa@gmail.com>
2017-05-08 12:19:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUppercase()
+}
+
+function whisper(string) {
+  return string.toLowercase()
+}
+
+function logShout(string) {
+  console.log("Hello, ${string}!").toUppercase
+
+}
+
+function sayHiToGrandma(string) {
+  console.log('hello').toLowercase
+  return "I Can't hear you"
+}
+function sayHiToGrandma(string) {
+  console.log('HELLO').toUppercase
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string) {
+  console.log('I love you, Grandma')
+  return "I love you, too."
+}
