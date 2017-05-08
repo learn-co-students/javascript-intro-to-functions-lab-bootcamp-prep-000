313ae170d699dae1e79aec3329c6908b9b7ed117
Marsha Hall <mphall77@gmail.com>
2017-05-08 23:33:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase)
+}
+
+var lowerCase = "hello!"
+  //lowerCase.toLowerCase() === lowerCase
+var upperCase = "HELLO!"
+
+function sayHiToGrandma(string) {
+  if (lowerCase === string.toLowerCase())
+    return ("I can\'t hear you!")
+  }
+
+function sayHiToGrandma(string)  {
+  if (upperCase === string.toUpperCase())
+      return ("YES INDEED!")
+    }
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma.")
+    return ("I love you, too.")
+}
