4686516fe6c73ddccfc51b3a0beb90cf51dbf8fb
Brooke Meitzler <brooke@curtsyapp.com>
2017-05-08 14:01:44 +0000

    Done.

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-console.logShout('HELLO')
-console.logWhisper('hello')
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
 
 function sayHiToGrandma(string){
-  if ('hello'){
-    return "I can\'t hear you!"}
-  else if ('HELLO'){
-    return "YES INDEED!"}
-  else {
+  if (string.toLowerCase()===string){
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase()===string){
+    return "YES INDEED!"
+  }
+
+  if (string=== "I love you, Grandma."){
     return "I love you, too."
   }
+}
