1baa9b8026982c99452d6b2142a9d44681f6a783
Marsha Hall <mphall77@gmail.com>
2017-05-09 00:01:18 +0000

    Done.

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
 
 var lowerCase = "hello!"
@@ -19,16 +19,14 @@ var lowerCase = "hello!"
 var upperCase = "HELLO!"
 
 function sayHiToGrandma(string) {
-  if (lowerCase === string.toLowerCase())
-    return ("I can\'t hear you!")
+  if (string === string.toLowerCase()){
+    return ("I can't hear you!")
   }
-
-function sayHiToGrandma(string)  {
-  if (upperCase === string.toUpperCase())
+  else if (string === string.toUpperCase()){
       return ("YES INDEED!")
     }
 
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma.")
+  else if (string === "I love you, Grandma."){
     return ("I love you, too.")
+  }
 }
