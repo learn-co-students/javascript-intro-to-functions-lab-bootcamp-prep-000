e0fa9510a14556379e77a71863f0e9d6e4ed554c
Amanda Wehrman <arwehrman@gmail.com>
2017-05-11 16:56:43 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ function shout(string){
 function whisper(HELLO){
   return HELLO.toLowerCase()
 }
-function logShout(hello){
-  console.log(hello.toUpperCase)
+function logShout(string){
+  console.log(string.toUpperCase())
 }
+logShout('hello')
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+logWhisper('HELLO')
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+} else if ('I love you, Grandma.') {
+  return 'I love you, too.'
+}
+
+sayHiToGrandma('hello')
+sayHiToGrandma('HELLO')
+sayHiToGrandma('I love you, Grandma.')
