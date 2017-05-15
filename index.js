b5ab971350d0d55db86fec7b59aa4f24f1c7dd54
Sherwin Marcelle <sherwinmarcelle@outlook.com>
2017-05-15 21:34:20 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-var logShout = function shout(string) {
-  return string.toUpperCase()
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
-var string = "hello"
-console.log(string)
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.")
+  {return "I love you, too."}
+}
