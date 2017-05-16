da4f62cb474d48386cf8dbd4b60ae05406f8d489
Tacara Solomon <alleyesoncara@gmail.com>
2017-05-16 04:36:09 +0000

    Done.

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-  console.log(sring.toLowerCase())
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
 }
