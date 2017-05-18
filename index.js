206f9df290dd6dbe318fcf31eb0f99c9ed4c6bb5
Catherine Siadat <cat.siadat@gmail.com>
2017-05-18 00:52:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return 'I can\'t hear you!''.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return 'YES INDEED'.toUpperCaseowerCase()
+}
