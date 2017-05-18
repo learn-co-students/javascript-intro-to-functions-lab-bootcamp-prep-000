8a1bae698958c2301171b5617119dd170f3adf68
Raymond Vo <raymondvooo@gmail.com>
2017-05-18 04:58:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function sayHelloTo (name) {
+  console.log(`hello, ${name}`)
+}
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  if (string === 'hello')
+    return "I can't hear you!"
+  else if (string === 'HELLO')
+    return "YES INDEED!"
+  else if (string === "I love you, Grandma.")
+    return "I love you, too."
+}
