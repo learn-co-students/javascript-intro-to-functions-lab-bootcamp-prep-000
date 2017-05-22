4cf9b0e279792f0525bf6434d6758289893f183e
Eric Strickler <eric.strickler@gmail.com>
2017-05-22 20:37:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
 }
 function whisper(string){
@@ -10,6 +10,18 @@ function logShout(string){
 function logWhisper(string){
   console.log('hello')
 }
-function sayHiToGrandma(string){
-  return 'HELLO'.toLowerCase()
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
