30dc164e401371a18a2da859684019d3e1f88330
Charles Fofie Jr <cfofiejr@gmail.com>
2017-05-22 15:40:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string)
+  { return string.toUpperCase() }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string)
+  { console.log(string.toUpperCase()) }
+
+function logWhisper(string)
+  { console.log(string.toLowerCase()) }
+
+function sayHiToGrandma(string){
+  var uppercase = (string.toUpperCase())
+  var lowercase = (string.toLowerCase())
+    if (lowercase === string) {
+      return ("I can't hear you!")}
+    else if (uppercase === string) {
+      return ("YES INDEED!")}
+    else (string === "I love you, Grandma.");
+      return ("I love you, too.")
+      }
