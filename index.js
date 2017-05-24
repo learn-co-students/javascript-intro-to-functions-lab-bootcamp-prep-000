93ee2dfa9712e1e91a326452e1f40934dfa6b75b
Chad Stephenson <chad@coastald.com>
2017-05-24 02:22:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('HELLO')
+}
+function logWhisper(){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === string) {return 'I can\'t hear you!'}
+ else if(string.toUpperCase() === string) {return 'YES INDEED!'}
+ else if(string === 'I love you, Grandma.') {return 'I love you, too.'}
+}
