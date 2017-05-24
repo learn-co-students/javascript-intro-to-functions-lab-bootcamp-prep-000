f570f40930aec4ca570c8dff0ac2092f875c88e3
Okhai Omotuebe <okhaimie@icloud.com>
2017-05-24 22:42:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function wisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+ return 'HELLO'.toLowerCase()
+ console.log ("I can't hear you!")
+}
