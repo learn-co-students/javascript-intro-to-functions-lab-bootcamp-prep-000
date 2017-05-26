72cd922925fd20e90ac3f234c4978a41c65b96ed
Dina Dunn <gabumon78@yahoo.com>
2017-05-26 05:13:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {  
+  return string.toUpperCase()
+}
+function whisper(string) {  
+  return string.toLowerCase()
+}
+function shout(string) {  
+  console.log()
+  return string.toUpperCase()
+}
+function whisper(string) {  
+  console.log()
+  return string.toLowerCase()
+}
