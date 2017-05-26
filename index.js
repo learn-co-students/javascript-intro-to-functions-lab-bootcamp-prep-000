0df2bac2222083213415998ea1894a6244f89b2b
John Tenezaca <john.tenezaca7@gmail.com>
2017-05-26 17:40:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+  }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(hello){
+  if (hello.toLowerCase() === hello){ return "I can\'t hear you!"}
+ else if (hello.toUpperCase() === hello){ return "YES INDEED!"}
+ else {return "I love you, too."}
+}
