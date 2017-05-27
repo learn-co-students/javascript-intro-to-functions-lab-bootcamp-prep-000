6f4f03edfef4ab27bac01efd2266302a0a98f87e
Paul  Hewett <pmhewett@gmail.com>
2017-05-27 05:14:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+// shout
+function shout (string) {
+  return string.toUpperCase()
+}
+
+//whisper
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+//logShout
+function logShout (string) {
+  string = string.toUpperCase();
+  console.log(string)
+}
+
+//logWhisper
+function logWhisper (string) {
+  string = string.toLowerCase();
+  console.log(string)
+}
+
+//sayHiToGrandma
+function sayHiToGrandma (string) {
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+//learn test
