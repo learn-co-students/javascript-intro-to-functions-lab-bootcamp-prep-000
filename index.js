d6b5e7d06aeaf628a87085da45d1512b4f33cd09
Benjamin Geyer <brgeyer49@gmail.com>
2017-05-29 21:37:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  return console.log(shout(string))
+}
+function logWhisper(string){
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string){
+  if(string === whisper(string)){ return 'I can\'t hear you!'}
+  if(string === shout(string)){ return 'YES INDEED!'}
+  if(string === "I love you, Grandma."){ return 'I love you, too.'}
+}
