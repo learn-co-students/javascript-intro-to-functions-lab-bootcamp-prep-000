9dec40fafd05d8fe32ea9df1e0563924be070442
Poke Sam <pokesam10031@yahoo.com>
2017-06-05 08:54:31 +0000

    Done.

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return string.toLowerCase()
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  return string.toLowerCase()
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
