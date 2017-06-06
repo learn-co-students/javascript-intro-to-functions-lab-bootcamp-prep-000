a8af3808a90719f8c75b3ab75fb1f089d4f22ffd
jacob  thompson <jacobtva@gmail.com>
2017-06-06 18:50:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var upperCase = 'HELLO'
+var lowerCase = 'hello'
+var love = 'I love you, grandma.'
+function shout (string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+return('I love you, too.'),return('YES INDEED!')
+}
