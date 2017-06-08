d9dc54c71fb6d4fff772c15df60af25ee149b1e2
Noelle Brooks <nbrooks707@gmail.com>
2017-06-08 00:39:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+
+var uppercase = "HELLO"
+function logShout(string) {
+  console.log(uppercase)
+}
+ var lowercase = "hello"
+ function logWhisper(string){
+   console.log(lowercase)
+ }
+var sayHiToGrandma ="I can't hear you"
+function sayHiToGrandma(string) {
+  return string.toLowerCase("")
+}
