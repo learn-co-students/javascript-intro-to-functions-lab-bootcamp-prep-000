752b3f3294452a2fc07d150c42fe2e5cf08159df
Noelle Brooks <nbrooks707@gmail.com>
2017-06-09 00:03:57 +0000

    Done.

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function logShout(string) {
    console.log(lowercase)
  }
 function sayHiToGrandma(string) {
-   if (string === string.toLowerCase())
-  return "I can't hear you"
-}
+   if (string === string.toLowerCase()) {
+     return "I can't hear you!"
+   } else if (string === string.toUpperCase()) {
+     return "YES INDEED!"
+   } else if (string === "I love you, Grandma.") {
+      return ("I love you, too.")
+   }
+ }
+sayHiToGrandma("hello")
+alert(sayHiToGrandma("hello"))
