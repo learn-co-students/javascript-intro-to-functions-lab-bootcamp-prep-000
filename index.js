e2e471799e84a9bc3158f5930f39fadda59645f7
Aaron Lieberman <aaronmaxmusic@gmail.com>
2017-06-09 23:01:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(HELLO) {
+  return HELLO.toLowerCase()
+}
+function logShout(HELLO) {
+  console.log ("HELLO")
+}
+function logWhisper(hello) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase() )
+  return ("I can\'t hear you!")
+  else if (string === string.toUpperCase() ) 
+  return ("YES INDEED!")
+  else if (string === "I love you, Grandma.")
+  return ("I love you, too.")
+}
