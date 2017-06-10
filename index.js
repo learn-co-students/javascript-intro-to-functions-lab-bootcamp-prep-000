eefe4e38e6765bcdaa1e6626148e2daaf0eefd3a
Franco Aiello <steed430@hotmail.com>
2017-06-10 12:15:51 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ return string.toUpperCase()
 function whisper (string){
   return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log( string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+logShout("HELLO")
+
+logWhisper("hello")
+
+function sayHiToGrandma(string){
+  if (string.toUpperCase()==="I LOVE YOU, GRANDMA."){
+    return "I love you, too."
+  }
+  else if (string.toUpperCase()=== string){
+    return "YES INDEED!"
+  } else  {
+    return "I can't hear you!"
+  }
+}
