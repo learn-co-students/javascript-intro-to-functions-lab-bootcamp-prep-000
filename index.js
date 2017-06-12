5346b5896b949ee5317bae540c0f7e94d1c3d9d0
hingyenyin <yenyin.262@gmail.com>
2017-06-13 07:10:04 +0800

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) { return string.toLowerCase() }
 function logShout(string) { console.log (string.toUpperCase()) }
 function logWhisper(string) { console.log (string.toLowerCase()) } 
-function sayHiToGrandma(string) { return "I can't hear you".toLowerCase } 
+function sayHiToGrandma(string) { return "I can't hear you!" .toLowerCase } 
 function sayHiToGrandma(string) { return string.toUpperCase() } 
 function sayHiToGrandma(string) { return "I love you, too." } 
