d6e6799a881a9e0ae356054eae1d23bd7efe5650
hingyenyin <yenyin.262@gmail.com>
2017-06-13 22:35:12 +0800

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) { return string.toLowerCase() }
 function logShout(string) { console.log (string.toUpperCase()) }
 function logWhisper(string) { console.log (string.toLowerCase()) } 
-function sayHiToGrandma(string) { if  (string.toLowercase() === String)  { 
+function sayHiToGrandma(string) { if  (string.toLowerCase() === string)  { 
 return "I can't hear you!" } 
-else if (string.toUppercase() === string) { return "YES INDEED!" } 
-else { (string === "I love you, Grandma.") { return " I love you, too."} } 
+else if (string.toUpperCase() === string) { return "YES INDEED!" } 
+else if (string === "I love you, Grandma.") { return "I love you, too."} 
+}
