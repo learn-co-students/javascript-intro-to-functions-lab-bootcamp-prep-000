684c481f0c8cf48be913519999841ec3e0070408
Katlyn Folsom <kfolsom90@gmail.com>
2017-06-14 01:05:45 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper (string){
 }
 
 function sayHiToGrandma(string){
-  if (string===string.toLowerCase){
-    return = "I can't hear you!";
-  } else if(string.toUpperCase){
-    greeting = "YES INDEED!";
-  } else(string){
-    greeting = "I love you, too!";
+  if (string.toLowerCase()===string){
+    return ("I can't hear you!")
+  } else if(string.toUpperCase()===string){
+    return ("YES INDEED!")
+  } else if(string==="I love you, Grandma."){
+    return("I love you, too.")
   }
+}
