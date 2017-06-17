83db18850e42ed42b55c56ab28edc0910e1da818
Marz A <alzokarimarwa@gmail.com>
2017-06-17 10:35:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase){
+    return console.log('I can\'t hear you!')
+  else if (string.toUpperCase){
+  return console.log('YES INDEED!')
+  }
+  else (string.console.log('I love you, Grandma.')){
+    return console.log('I love you, too.')
+  }
+}
