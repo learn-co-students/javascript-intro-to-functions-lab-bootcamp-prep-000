f8c249d2138b3435df0a08f8c5560ec5df040204
Jordan Williams <jcwilliams410@gmail.com>
2017-06-20 04:18:03 +0000

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper(string) {
   return string.toLowercase
 }
 function sayHiToGrandma(string) {
-  console.log('hello')
-  return ('I can\'t hear you!')
-}
-function sayHiToGrandma(string) {
-  console.log('HELLO')
-  return ('YES INDEED!')
-}
+  if (string === string.toLowerCase()) {
+  return ("I can't hear you!")
+} else if (string === string.toUpperCase()) {
+  return ("YES INDEED!")
+} else if (string === "I love you, Grandma."){
+  return ('I love you, too.')
+} }
