31728aff1b30f98b6ac68db9eb224988dc6f5b8c
Sophia L <soph915@gmail.com>
2017-06-22 13:44:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function shout(string){
+function shout (string){
   return string.toUpperCase()
 }
 
@@ -11,11 +11,11 @@ function logShout(string){
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase())
+    console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-if (string.toLowerCase() === string ){return "I can\'t hear you!"}
-else if (string.toUpperCase() === string ){return "YES INDEED!"}
-else if (string === 'I love you, Grandma.'){return "I love you, too."}
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){return "I can't hear you!"}
+  else if (string.toUpperCase() === string) {return "YES INDEED!"}
+  else if (string === "I love you, Grandma.") {return "I love you, too."}
 }
