53ebbc12953a8e08da94263061999a4971d9229f
Kevin Augustin <kevinaugustin91@gmail.com>
2017-06-23 02:27:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+var lowercase = 'hello!'
+var uppercase = 'HELLO!'
+
+function sayHiToGrandma(string) {
+if (lowercase)
+return ("I can't hear you!")
+
+if (uppercase)
+return ("YES INDEED!")
+
+if ("I love you, Grandma.")
+return ('I love you, too.')
