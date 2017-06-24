3f47d38696615d9a13de7d13f3e0f5ce9f7fb77f
Yannick Dongo Dongo <dongoyannick@gmail.com>
2017-06-24 11:14:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function  logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  var response="";
+
+  if (string===string.toUpperCase()){
+    response="YES INDEED!";
+  }
+  else if(string===string.toLowerCase()){
+    response="I can't hear you!";
+  }
+  else if(string=="I love you, Grandma."){
+    response="I love you, too.";
+  }
+  return response;
+}
