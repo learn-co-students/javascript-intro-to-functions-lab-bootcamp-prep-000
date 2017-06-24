fa9efc62e8bdd3af08684aa1400f7667ec632a34
Akshay Jassal <maru1upt@iitr.ac.in>
2017-06-24 20:08:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  if(string.toUpperCase === 'uppercase')
+  console.log(string);
+
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
