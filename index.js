ebf3619ec36448773c70a384ab726b59f46c1ac0
Chris Miller <chris.miller8823@gmail.com>
2017-06-25 21:17:01 +0000

    Done.

--- index.js
+++ index.js
@@ -17,11 +17,16 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var lowercase = string.toLowerCase();
   var uppercase = string.toUpperCase();
+
   if (string === lowercase) {
     return "I can't hear you!";
-  } else if (string === uppercase) {
+  }
+
+  if (string === uppercase) {
     return "YES INDEED!";
-  } else {
+  }
+
+  if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
