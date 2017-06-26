016f2f2f9e2acefe109b817272ff0dcab801784e
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-06-26 14:00:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-  } 
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  } 
   if (string === "I love you, Grandma.") {
     return "I love you, too."
-  }
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else
+  return "YES INDEED!";
 }
