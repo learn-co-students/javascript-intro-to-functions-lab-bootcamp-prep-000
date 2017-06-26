0c436f5ab7ff0c9c96b31c619338b32577d1dd73
Steven Ianigro <bxdeadpool@gmail.com>
2017-06-26 16:44:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+​
+function whisper(string) {
+  return string.toLowerCase()
+}
+​
+function sayHiToGrandma(string) {
+  console.log("hello".toEqual"I can\'t hear you!")
+  return
+}
+​
