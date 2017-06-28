0689a667f603663499db85a9912b889274f5db04
giang ha <gi4ngh4_95@yahoo.com>
2017-06-28 18:58:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+  function shout (string) {  return string.toUpperCase() }
+  function whisper(string) { return string.toLowerCase()}
+  function logShout (string) { console.log(string.toUpperCase())}
+  function logWhisper(string) { console.log(string.toLowerCase())}
+
+  var lov= "I love you, Grandma."
+  var a = "I can't hear you!"
+  var b = "YES INDEED!"
+var l = "I love you, too."
+  function sayHiToGrandma(string)
+  { if(string.toLowerCase() === string)
+    return a
+      else if (string.toUpperCase() === string)
+
+    return b
+    if(string ===lov)
+    return l
+  }
