3ba3c4da4e80d225e6a5586fa7d885f2b373a4cb
Steven Ianigro <bxdeadpool@gmail.com>
2017-06-28 21:45:45 +0000

    Done.

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
 function shout(string) {
   return string.toUpperCase()
 }
-​
+
 function whisper(string) {
   return string.toLowerCase()
 }
-​
-function sayHiToGrandma(string) {
-  console.log("hello".toEqual"I can\'t hear you!")
-  return
+
+function logShout() {
+  console.log("HELLO")
 }
-​
+
+function  logWhisper() {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) {
+    return ("I can\'t hear you!")
+    }
+  else if (string === string.toUpperCase()) {
+    return ("YES INDEED!")
+
+  }
+  else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+
+  }
+}
