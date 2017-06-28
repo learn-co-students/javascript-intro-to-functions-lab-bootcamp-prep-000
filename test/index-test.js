46dd6fe0475194c7f008c5b5a690e3ce738d66b8
Rahul Chandiramani <rahulcvi19@gmail.com>
2017-06-28 22:43:26 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -48,3 +48,34 @@ describe('sayHiToGrandma(string)', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
+
+
+function sayHiToGrandma(string){
+  if (string == string.toLowerCase())
+    return "I can't hear you!"
+
+  else if (string == string.toUpperCase())
+    return "YES INDEED!"
+
+  else if (string == 'I love you, Grandma.')
+    return "I love you, too."
+
+}
+
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
