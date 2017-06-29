d30d4db54f8599e5719cfa3941c01fa1c88faf30
Rebecca Patrick <rleepatrick@gmail.com>
2017-06-29 00:40:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+ function whisper(string) {
+   return string.toLowerCase ()
+ }
+
+ function logShout(spy) {
+   console.log('HELLO')
+   return `${spy}`
+ }
+
+ function logWhisper(spy) {
+   console.log('hello')
+   return `${spy}`
+ }
+
+function sayHiToGrandma(string) {
+  console.log('HELLO')
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === string) {
+    console.log("I can't hear you!")
+  } else if (string.toUpperCase === string) {
+    console.log("YES INDEED!")
+  } else {
+    console.log("I love you, too")
+  }
+}
