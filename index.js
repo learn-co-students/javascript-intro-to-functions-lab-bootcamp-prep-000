ec5448a0386c3128e9d6df4a16f7db8bc9f2370c
Delaney Spangler <delaneyspangler@gmail.com>
2017-06-29 17:34:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string.lowercase) {
+  return "I can't hear you!"}
+
+function sayHiToGrandma(string.uppercase) {
+  return "YES INDEED!"}
+
+function sayHiToGrandma(string.mixedCase) {
+  return "I love you, too."}
