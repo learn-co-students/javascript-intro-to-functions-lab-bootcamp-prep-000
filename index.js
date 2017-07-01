8cc688a72a2003db317eb8c5eb4c66cee5470a09
Gina Kang <ginaakangg@gmail.com>
2017-07-01 05:47:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  var uppercase = "HELLO!"
+  console.log(uppercase).toUpperCase
+}
