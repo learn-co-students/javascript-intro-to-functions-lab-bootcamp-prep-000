a7bca31186ecf0352b213cdf35e432877d9d3e6f
Filip Knyszewski <filip_mateus96@hotmail.com>
2017-07-02 10:27:22 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
 
   if (string.toLowerCase() === string){
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   }
   if (string.toUpperCase() === string){
     return "YES INDEED!"
