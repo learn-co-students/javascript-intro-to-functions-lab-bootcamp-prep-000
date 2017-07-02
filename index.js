bb87f84a2b737eb8a5ba3f63e17399f77f795562
Nicole Tams <nicole.cacia@gmail.com>
2017-07-02 22:13:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log (string.toUpperCase()
-)
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var lowercase = string. toLowerCase()
+  var uppercase = string.toUpperCase()
+
+  if (string===lowercase){
+    return "I can't hear you!"
+  } else if (string === uppercase){
+    return "YES INDEED!"
+  } else if (string=== "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
