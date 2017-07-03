222eeccc0d51c5616860492bf9dda1ded950bff6
charles henson <charleshenson999@gmail.com>
2017-07-03 22:23:22 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase());
-  return ('I can\'t hear you!');
-} else if (string.toUpperCase()); {
-  return ('YES INDEED!');
-} else if (string('I love you, Grandma.')); {
-  return ('I love you, too.')
-}
