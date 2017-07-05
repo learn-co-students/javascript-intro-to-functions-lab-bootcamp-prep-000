b32d9126aeb72ecc9e2f51d1ca3059a23229db8d
George Liu <georgeliu96@gmail.com>
2017-07-05 21:27:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(shout(string))
+  return shout(string);
+}
+
+function logWhisper(string){
+  console.log(whisper(string))
+  return whisper(string);
+}
+
+function sayHiToGrandma(string){
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+  else {
+    return "What did you say?"
+  }
+}
