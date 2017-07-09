d99d814c5b5c9b3e7147fa4920822b102af1844d
Kristin Schreiber <kmschreiber.13@gmail.com>
2017-07-09 15:47:27 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (console.log(string.toLowerCase())) {
-    return ("I can't hear you")
-  } else if (string === "uppercase") {
+  if (string === string.toLowerCase()) {
+    return ("I can't hear you!")
+  } else if (string === string.toUpperCase()) {
     return ("YES INDEED!")
   } else if (string === "I love you, Grandma.")
     return ("I love you, too.")
