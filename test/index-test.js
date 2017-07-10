84c6c43724a8f1e723c6550cd3c11bc298d2ce37
Brooke Ryan <brookeryan630@icloud.com>
2017-07-10 15:15:34 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,17 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)'), function('hello') {
+  it('receives one argument and returns it in all caps', function('hello') {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
-describe('whisper(string)', function() {
+describe('whisper(string)'), function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
 
-describe('logShout(string)', function() {
+describe('logShout(string)'), function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
