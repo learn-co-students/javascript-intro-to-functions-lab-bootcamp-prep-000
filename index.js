375e5670e65a9addc81d5d862350a5a0cf3c2975
Karen Truss <truss.karen@gmail.com>
2017-07-10 02:56:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+  }
+  function whisper(string){
+    return string.toLowerCase()
+  }
+  function logShout(string){
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string){
+    console.log(string.toLowerCase())
+  }
+  function sayHiToGrandma() {
+    return(lowerCase) if string=string.toLowerCase()
+}
+var mixedCase = "I love you, too."
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+var upperCase = "YES INDEED!"
+upperCase.toUpperCase() === upperCase
+var lowerCase = "I can\'t hear you!"
+lowerCase.toLowerCase() === lowerCase
