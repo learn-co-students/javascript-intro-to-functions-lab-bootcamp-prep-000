781ecab44df3f527f7a9c7d136c38beb50418a7b
Kate Bennett <kate.t.bennett@gmail.com>
2017-07-12 19:55:14 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function shout (string) {
 function whisper (string) {
   return string.toLowerCase()
 }
-function logWhisper (string) {
-  console.log(string.toLowerCase)
+function logShout (string) {
+console.log(‘hello’)
+return (spy)
 }
