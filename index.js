0e9f4b5171bdd348ca0814445563d8be1774240c
Emily Zhao <tong.zhao.92@gmail.com>
2017-07-17 00:12:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+  var spy = "Spy"
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  var spy = "Spy"
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase () {
+    console.log ("I can\'t hear you!")
+  }
+  if string === string.toUpperCase () {
+    console.log ("YES INDEED!")
+  }
+  if string === "I love you, Grandma." {
+    console.log ("I love you, too.")
+  }
+}
