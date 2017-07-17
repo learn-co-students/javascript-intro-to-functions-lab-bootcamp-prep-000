a78ee65834abe113dd917ca573ea8c203143ada9
Madeleine Rothery <madeleine.rothery@gmail.com>
2017-07-17 18:03:27 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -14,7 +14,6 @@ describe('whisper(string)', function() {
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
-
     logShout('hello')
 
     expect(spy).toHaveBeenCalledWith('HELLO')
