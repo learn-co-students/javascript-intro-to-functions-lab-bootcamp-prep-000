1d2520a120682191d74ada42c844dad8e4a63924
Donivan Gray <gray.donivan@yahoo.com>
2017-07-18 19:19:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+logShout = function whisper(string){
+    return string.toLowerCase()
+}
