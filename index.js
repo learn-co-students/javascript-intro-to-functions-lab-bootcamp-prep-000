7a7931499515cb52ee267bdc451235ad5ca22464
amanda johnson <manda.panda91@yahoo.com>
2017-07-18 00:29:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
 }
 
-function whisper(string) {
+function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string) {
+function logShout(string){
   console.log(string.toUpperCase())
 }
 
-function logWhisper(string) {
+function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-  return `I can\'t hear you!`
+function sayHiToGrandma(string){
+if (string == string.toLowerCase()){
+return `I can\'t hear you!`
 }
-  else if (string.toUpperCase() === string) {
+  else if (string == string.toUpperCase()){
     return `YES INDEED!`
   }
-  else if ('I love you, Grandma.') {
+  else if (string === "I love you, Grandma."){
     return `I love you, too.`
-  }
-}
+  }}
