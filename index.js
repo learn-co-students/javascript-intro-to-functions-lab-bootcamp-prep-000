ed70f1ac3152110792d018d26f111eb7994eadb3
Brian Feibus <brianfeibus@gmail.com>
2017-07-19 01:27:28 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log('string')
-  return string.toUpperCase()
+  console.log('HELLO')
+}
+function logWhisper(string) {
+console.log('hello')
 }
 
-
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase() {
+      return ("I love you, too.")
+} else {
+  if  string === string.toUpperCase() {
+      return ("I can't hear you!")
+} else {
+  if string === ("I love you, Grandma")
+      return ("YES INDEED!")
 }
