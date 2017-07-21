60d18f842b30c5611ca0854cdc271adff13dfa88
Kate Bennett <kate.t.bennett@gmail.com>
2017-07-21 01:08:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ function whisper (string) {
   return string.toLowerCase()
 }
 function logShout (string) {
-console.log(‘hello’)
-return (spy)
+console.log('hello'.toUpperCase())
+}
+function logWhisper (string) {
+  console.log('HELLO'.toLowerCase())
+}
+function sayHiToGrandma (string) {
+  if (string === string.toLowerCase()){
+    return ("I can't hear you!")
+  }
+  else if (string === string.toUpperCase()){
+    return ("YES INDEED!")
+  }
+  else if (string === "I love you, Grandma."){
+    return ("I love you, too.")
+  }
 }
