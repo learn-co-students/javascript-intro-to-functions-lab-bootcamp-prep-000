906b30af0fac4deeb20f30fe72117d01f62d6ca7
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-07-22 01:41:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string)  {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toLowerCase()
+}
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return string.tolowerCase()
+}
