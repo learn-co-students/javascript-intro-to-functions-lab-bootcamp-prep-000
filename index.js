e07348eadbfeffffae930625c4c65774278b4134
thart09 <hartmanntimo@hotmail.de>
2017-07-22 16:49:31 +0000

    Done.

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === lowercase) return "I can\'t hear you!";
+  if(string.lowercase) return "I can\'t hear you!";
 }
