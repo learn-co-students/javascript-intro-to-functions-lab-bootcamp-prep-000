11c8ce5f39497b746ded99e34b450c7faaa60cac
Luis Bello <luisbg9@gmail.com>
2017-07-24 22:51:01 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(('hello').toUpperCase())
+}
+
+  logShout('hello')
+
+function logWhisper(string) {
+    console.log(('hello').toLowerCase())
+  }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+  return "I can't hear you!"
+} else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }  else if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+  }
