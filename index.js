341c78f171d62be3a12c8bd6fe937c369f8ea002
Pauli Payne <pauli.a.payne@gmail.com>
2017-07-24 01:04:53 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === lowercase) {
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+}
