f6404924c96d8bb3310c8c06aabad3020353381f
mustafa hill <loma7@aol.com>
2017-07-28 06:26:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+var string = string.toUpperCase();
+console.log(string);
+  }
+function logWhisper(str){
+var str = str.toLowerCase();
+console.log(str);
+}
+function sayHiToGrandma(str){
+  if str === str.toLowerCase
+  return "I can't hear you!"
+}
