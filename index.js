e82ca3fca2cfad9895b97e68822b7712e9bc87ba
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-07-28 04:41:18 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can't hear you!";
+} else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+} else {
+  return "I love you, too.";
+}
+}
