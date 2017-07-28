41f64eb69256dbef9f0547467c6bdf05819863b4
mevanstron <michael@michaelgregoryevans.com>
2017-07-28 03:38:21 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string)
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
+  }
 }
