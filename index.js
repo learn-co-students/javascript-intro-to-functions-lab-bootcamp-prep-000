dcf7081146a514561d4db680e6eafd8259c381c0
tbfloyd <tbain64@yahoo.com>
2017-07-30 17:54:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  console.log string.toLowerCase()
+}
+Function logShout(string) {
+  console.log string.toUpperCase
+}
