76c5951b438c28a71c19cd4b30601ba584861d92
Steve R <srobic13@gmail.com>
2017-07-30 10:00:08 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,39 @@ function shout(string) {
 }
 
 function whisper(string){
-  return string.toLowerCares()
+  return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log("HELLO")
+}
+
+function logWhisper(string){
+  console.log("hello")
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
