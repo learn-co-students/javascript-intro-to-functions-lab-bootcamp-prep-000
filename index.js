7ad0e70fb59e097009c5ab7ac0d053fb206f38af
Brandon Chin <brandonchin@me.com>
2017-07-30 17:52:45 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function shout(string) {
   return string.toUpperCase()
 }
 function whisper(string) {
-  return string.toLowerCase
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log string.toUpperCase()
 }
