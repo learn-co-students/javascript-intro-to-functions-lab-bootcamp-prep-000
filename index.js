47f67e9774cccd45377b0ba89df8287f6559225c
Michelle Wiraydi <miwiryadi@gmail.com>
2017-07-31 19:25:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string);
+}
+
+function sayHiToGrandma(string){
+  if (string === 'lowercase'){
+    return console.log("I can't hear you!")
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string === 'uppercase'){
+    return console.log("Yes INDEED!")
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string === 'I love you, Grandma.'){
+    return console.log("I love you, too.")
+  }
+}
