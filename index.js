2ec336bc018c8f09a17ad913910f212f55d337f4
Michelle Wiraydi <miwiryadi@gmail.com>
2017-08-01 01:55:49 +0000

    Done.

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string);
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
-  if (string === 'lowercase'){
-    return console.log("I can't hear you!")
+  if (string.toLowerCase() === string){
+    return "I can't hear you!";
   }
-}
 
-function sayHiToGrandma(string){
-  if (string === 'uppercase'){
-    return console.log("Yes INDEED!")
+  if (string.toUpperCase() === string){
+    return "YES INDEED!";
   }
-}
 
-function sayHiToGrandma(string){
   if (string === 'I love you, Grandma.'){
-    return console.log("I love you, too.")
+    return "I love you, too.";
   }
+
 }
