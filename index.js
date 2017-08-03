a980feb6931c7ed613d6a35d2d91ae4a37cc2e34
Parrish Corcoran <parrish.corcoran@gmail.com>
2017-08-03 07:00:47 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  if (string.toLowerCase() === string) {
+  return "I can\'t hear you!";
+} else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+} else if (string === "I love you, Grandma.") {
+  return 'I love you, too.';
 }
+};
