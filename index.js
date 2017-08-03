21c34d57646d524b72e0010be267c3f4f472642d
Peter Meister <prmeister89@gmail.com>
2017-08-03 05:30:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase()
+}
+
+
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+  var lowercase = 'hello!'
+  string.toLowerCase() === string
+  var mixedCase = 'Hi there!'
+  mixedCase.toLowerCase() === mixedCase
+  mixedCase.toUpperCase() === mixedCase
+
+if (string.toLowerCase() === string) {
+  return "I can't hear you!"
+  }
+else if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+  }
+else {
+  return "I love you, too."
+  }
+}
