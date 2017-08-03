4cded6cba54a54e96b1f0aa21b335eacee962a45
Tyrone Scafe <tscafejr@gmail.com>
2017-08-03 02:59:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+return string.toUpperCase ()
+}
+
+function whisper (string) {
+return string.toLowerCase ()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+
+function sayHiToGrandma(string) {  // "hi grandma!" in order to test this, we first convert it to all caps ~> "HI GRANDMA"
+       if (string.toUpperCase () === string) {
+          return "YES INDEED!"
+        }
+          else if (string.toLowerCase () === string) {
+          return "I can't hear you!"
+        }
+          else if (string === "I love you, Grandma.") {
+          return "I love you, too."
+        }
+
+    }
