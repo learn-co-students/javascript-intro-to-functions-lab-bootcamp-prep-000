50a19195532a00d2827544a60ce545b36d33d8ba
Michele Conway <mconway890@gmail.com>
2017-08-04 22:57:33 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ function logWhisper(string) {
 }
 
 
+
 function sayHiToGrandma(string) {
-  var lowercase = "hello!"
-  if (string.toLowerCase())
-  return 'I can\'t hear you!'
+  var uppercase = string.toUpperCase
+  var lowercase = string.toLowerCase
+
+  if (lowercase === string) {
+  console.log('I can\'t hear you!')
+
 }
-  if (string.toUpperCase()) {
-  var uppercase = "HELLO!"
-  return ('YES INDEED!')
+  else if (uppercase === string) {
+  console.log('YES INDEED!')
+
 }
-  else {
-  return ('I love you, too.')
+  else if (string === 'I love you, Grandma.') {
+  return 'I love you, too.'
 }
