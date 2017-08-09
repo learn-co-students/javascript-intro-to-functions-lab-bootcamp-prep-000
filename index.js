cf61815f57f5fa42252bb9f14bb60d601bdde96a
PeterBadal <peter.badal@gmail.com>
2017-08-09 17:32:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout (str) {
+  return str.toUpperCase();
+}
+
+function whisper (str) {
+  return str.toLowerCase();
+}
+
+function logShout(str) {
+  console.log(str.toUpperCase());
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase) {
+    return "I can't hear you!"
+  }
+ }
