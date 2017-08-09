9082fede3ea10c7f4bc6bf92530a972a9fdd07ca
PeterBadal <peter.badal@gmail.com>
2017-08-09 17:50:41 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ function logShout(str) {
   console.log(str.toUpperCase());
 }
 
+// function sayHiToGrandma(string){
+//
+// }
+
+function logWhisper (str) {
+  console.log(str.toLowerCase())
+}
+
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
- }
+}
