547cb6771de882744397ba558378d1459c19f70d
Brandon Chin <brandonchin@me.com>
2017-08-10 02:49:47 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase()) {
+return "YES INDEED!"
+  }
+}
