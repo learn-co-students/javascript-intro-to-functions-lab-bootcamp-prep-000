4d38b5de0f75c83b6cce3dc40a5367b62841f05e
Brandon Chin <brandonchin@me.com>
2017-08-10 03:52:58 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
   return "I can't hear you!"
   }
-  if (string.toUpperCase() === string) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
