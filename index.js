1614e0726832d7acac3155400dad70a1ab45abc3
Kevin Carrier <kacarrier@msn.com>
2017-08-13 02:09:05 +0000

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  return string
+  if(string === string.toLowerCase()){
+    return 'I can\'t hear you!'
+  }
+  else if (string === string.toUpperCase()){
+    return 'YES INDEED!'
+  }
+  else{
+    return   'I love you, too.'
+  }
 }
