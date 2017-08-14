aea8474f3ed827bd2f35351fda42ca4ade2c2e16
Ayana Bando <ayanaflax@yahoo.com>
2017-08-14 19:04:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var shout =  'HELLO'
+var whisper = 'hello'
+var sayHiToGrandma = 'I can\'t hear you!'
+
+
+function shout('HELLO'){
+return string.toUpperCase()
+
+}
+
+
+function whisper('hello'){
+return string.toLowerCase()
+}
