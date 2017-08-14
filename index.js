8767ff57247c81923770b64dcaa3e892c807c083
lauralaura14 <laurao148@gmail.com>
2017-08-14 22:55:44 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   }
 
-else if(string.ToUpperCase() === string) {
+else if(string.toUpperCase() === string) {
     return "YES INDEED!";
   }
 
