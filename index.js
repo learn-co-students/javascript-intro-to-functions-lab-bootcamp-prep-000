2ad813f4766f497f12e3953e3607048ef107678a
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-08-14 14:12:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+var uppercase = string.toUpperCase()
+var lowercase = string.toLowerCase()
+
+  if (uppercase === string) {
+    return string ="YES INDEED!"
+  }
+  if (lowercase === string) {
+    return string = "I can\'t hear you!"
+  }
+ if (string === 'I love you, Grandma.')
+ return string = "I love you, too."
+
+}
