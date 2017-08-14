1f79fc75265dc3df9cb10ec2f013abf8df69ff05
Marlon DuPain <marlonsaddress@gmail.com>
2017-08-14 22:43:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-function shout(string) {
-  return string.toUpperCase();
+function shout(string){
+  return `${string.toUpperCase()}`
 }
 
-function whisper(string) {
-  return string.toLowerCase();
+function whisper(string){
+  return `${string.toLowerCase()}`
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase());
+function logShout(string){
+  console.log(`${string.toUpperCase()}`)
 }
 
-function logWhisper(string) {
-  console.log(string.toLowerCase());
+function logWhisper(string){
+  console.log(`${string.toLowerCase()}`)
 }
 
-function sayHiToGrandma(string) {
-  switch (string) {
-    case "hello":
-      return "I can't hear you!";
-      break;
-    case "HELLO":
-      return "YES INDEED!";
-      break;
-    case "I love you, Grandma.":
-      return "I love you, too.";
-      break;
-
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===string){
+    return `I can't hear you!`
+  } else if (string.toUpperCase()===string){
+    return `YES INDEED!`
+  } else if (string===`I love you, Grandma.`){
+    return `I love you, too.`
+  } else {
+    return;
   }
+
 }
