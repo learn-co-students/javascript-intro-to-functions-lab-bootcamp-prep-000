a719425901933b2de4d14bc849b8916e18d03980
Paul Blake <paulblake_03@hotmail.com>
2017-08-19 17:29:00 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string)
+  return "I can't hear you!"
+  else if (string.toUpperCase() === string)
+  return "YES INDEED!"
+  else (string === "I love you, Grandma.")
+  return "I love you, too."
 }
