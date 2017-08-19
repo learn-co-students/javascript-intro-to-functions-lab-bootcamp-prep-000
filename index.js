5495d2b33a9562c4b1d39fbd9f1acd25c595f92c
Jimben Honeyfield <mooredubsteppin@gmail.com>
2017-08-19 20:30:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout (string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  var lowerCase = string.toLowerCase()
+  var upperCase = string.toUpperCase()
+  //var mixedCase = string.
+  if (string === lowerCase){
+    return "I can't hear you!"
+  }
+  else if (string === upperCase){
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
