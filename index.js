66ca8632c63579d04daf27b88343c4aea6a7bfb6
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-19 02:13:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCse = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
