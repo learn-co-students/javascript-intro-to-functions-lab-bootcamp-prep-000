7365fc423cb0110c7ea72f960660668baae2fefb
Will Chen <will42874aj@gmail.com>
2017-08-20 21:46:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+  console log.toUpperCase()
+}
+function logWhisper(string){
+  console log.toLowerCase()
+}
+function sayHiToGrandma(string){
+  var lowercase = string.toLowerCase();
+  var uppercase = string.toUpperCase();
+  var correct = "I love you, Grandma.";
+  if (lowercase === string) {
+    return "I can't hear you!"
+  }
+  else if (uppercase === string){
+    return "YES INDEED!"
+  }
+  else if (string === correct){
+    return 'I love you, too.'
+  }
+
+}
