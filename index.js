1d4eb675d4041b7d99da555d2872d6f187b2932a
Ricardo Melara <melric91@gmail.com>
2017-08-21 21:09:45 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function logWhisper(string) {
 logWhisper("Peace")
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase
-  return "I can't hear you!"
+  if (string ==string.toLowerCase())
+  {return "I can't hear you!"}
+  else if (string == string.toUpperCase())
+  {return "YES INDEED!"}
+  else if (string == "I love you, Grandma.")
+  {return "I love you, too."}
 }
