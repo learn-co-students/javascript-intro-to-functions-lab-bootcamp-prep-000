40ccd1e5cafec2e940008136c710b682a4208c53
Ricardo Melara <melric91@gmail.com>
2017-08-21 20:09:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+logShout("Hello")
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+logWhisper("Peace")
+
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase
+  return "I can't hear you!"
+}
