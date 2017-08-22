2c0a80a2a0256e5081db3896f30e9f67846dd35d
kkhemani <kunal@khemani.ca>
2017-08-22 17:11:19 +0000

    Done.

--- index.js
+++ index.js
@@ -14,8 +14,19 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  var uppercase ="HELLO!"
-  uppercase.toUpperCase() === uppercase
-  return string.toUpperCase("YES INDEED!")
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
