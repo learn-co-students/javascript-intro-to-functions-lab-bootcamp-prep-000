825834462a661960e8e14f4fa3c850b6f0f6700a
William Birkett <williebirkett@gmail.com>
2017-08-23 20:09:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+  return string.toUpperCase()
+  }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return 'HELLO'.toUpperCase
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return 'hello'.toLowerCase
+}
+
+var lowercase ='hello!'
+lowercase.toLowerCase() === lowercase
+var uppercase ='HELLO!'
+uppercase.toUpperCase() === uppercase
+var mixedCase ='I love you, Grandma.'
+mixedCase.toLowercase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+
+function sayHiToGrandma(string) {
+  console.log('hello!')
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
+  console.log('HELLO!')
+  return 'YES INDEED!'
+}
+
+function sayHiToGrandma(string) {
+  console.log('I love you, Grandma.')
+  return 'I love you, too.'
+}
