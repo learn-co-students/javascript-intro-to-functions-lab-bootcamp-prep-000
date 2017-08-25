7029f38bc96e07336021bd0c423672a6b2abf7fa
Ethan Saul <saul.ethan516@gmail.com>
2017-08-25 14:20:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+var sayHiToGrandma = "hello"
+
+
+  if (sayHiToGrandma === "hello") {
+    console.log("I can't hear you!")
+  } else if (sayHiToGrandma === "HELLO") {
+    console.log("YES INDEED!")
+  } else (sayHiToGrandma === "I love you Grandma") {
+    console.log("I love you, too!")
+}
