101439da761c08e99343c7af12535c7059663032
EPIC448 <samrey2018@gmail.com>
2017-08-26 14:01:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowercase()
+}
+function shout(string){
+  console.log string.toUpperCase()
+}
+
+function whisper(string){
+  console.log string.toLowerCase()
+}
+
+  function sayHiToGrandma(string){
+    return string.toLowercase("I can't hear you!")
+
+  }
+
+  function sayHiToGrandma(string){
+    return string.toUpperCasecase("YES INDEED!")
+  }
+
+  function sayHiToGrandma(string){
+    return string.toUpperCase ("I love you too.")
+  }
