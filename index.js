12af1b2f409dd85dc87cb63d392a6812982fc795
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-08-26 17:26:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+  return string.toUpperCase()
+}
+shout('hello')
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+whisper('HELLO')
+
+function logShout(string){
+  console.log(string.toUpperCase)
+}
+
+logShout('hello')
