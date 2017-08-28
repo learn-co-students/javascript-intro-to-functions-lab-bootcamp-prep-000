68b0265cb9381d7bd3a3d6aa5fbfdbcd66d036db
Fanny Zhao <fannyjzhao@gmail.com>
2017-08-28 03:27:00 +0000

    Done.

--- index.js
+++ index.js
@@ -9,24 +9,26 @@ function whisper(string) {
 //All they're asking you to do in the function is just log whatever string they pass in as an argument to the console but in all capital letters.//
 
 function logShout(string) {
-  shout(string)
   console.log(shout(string))
   }
 
 function logWhisper(string) {
-  whisper(string)
   console.log(whisper(string))
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return ("I can\'t hear you!")
-  } if (string === string.UpperCase()){
-    return ("YES INDEED!")
-  } if (string === "I love you, Grandma.")
+  if (string === "i love you, grandma.") {
     return ("I love you, too.")
+  } else if (string === "I LOVE YOU, GRANDMA.") {
+    return ("I love you, too.")
+  } else if (string === whisper(string)) {
+    return ("I can\'t hear you!")
+  } else if (string === shout(string)){
+    return ("YES INDEED!")
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
 }
 
-sayHiToGrandma("hello")//expect "I can/'t hear you!"
-sayHiToGrandma("HELLO")//expect "YES INDEED!"
-sayHiToGrandma("I love you, Grandma.")//expect "I love you, too."
+if (whiper(string) === "i love you, grandma.")
+  return (I love you, too.)
