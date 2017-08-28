6d3558829c9fb8db7ddba40869cefd2b8a440807
Benjamin Silfen <bfsilfen@gmail.com>
2017-08-28 13:18:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  return string.toLowerCase()
+  return string.toUpperCase()
+  return '${I love you, Grandma.}'
+}
