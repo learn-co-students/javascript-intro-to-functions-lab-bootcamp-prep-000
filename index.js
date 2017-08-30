ed4da2da3ba69fbae7a216a7e055a7df910e8568
Mark Fowler <fowlermark488@gmail.com>
2017-08-30 03:17:28 +0000

    Done.

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase());
+  console.log(shout(string));
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase());
+  console.log(whisper(string));
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
+  if (string === whisper(string)) {
     return "I can't hear you!";
   }
-  else if (string === string.toUpperCase()) {
+  else if (string === shout(string)) {
     return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
