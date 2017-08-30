298e03090a722fd6304aa401815b84353f45ff4c
Alex Denton <alex.jdenton@gmail.com>
2017-08-30 18:46:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string)  {
+  return string.toUpperCase()
+}
+//shout takes argument "string" and returns it in all uppercase
+//shout (hello!) > "HELLO!"
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+//whisper takes argument "string" and returns it in all lowercase
+//whisper (HELLO!) > hello!
