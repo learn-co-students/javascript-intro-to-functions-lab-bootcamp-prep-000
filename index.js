092d4578f90592b9b396ae9095b7611dcb598740
Cory Hack <cory.hack@gmail.com>
2017-08-30 02:01:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase('hello')
+}
+
+function whisper(string) {
+  return string.toLowerCase("HELLO")
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase('hello'))
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase("HELLO"))
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase(string))
+  return "I can't hear you!"
+
+  if (string === string.toUpperCase(string))
+  return "YES INDEED!"
+
+  if (string.toLowerCase === ("I love you, Grandma.").toLowerCase) 
+  return "I love you, too."
+}
