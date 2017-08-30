867d5ac5c5f38ea97060f75acf29506d7af37840
CoachCrazy <live4health@gmail.com>
2017-08-30 02:59:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(toAllCaps)
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase("YES INDEED!")
+}
