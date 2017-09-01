d0274b49f958f6fff0785d76dfda16d0329015f2
Crista Cooper <aine3d@gmail.com>
2017-09-01 21:27:30 +0000

    Done.

--- index.js
+++ index.js
@@ -19,15 +19,11 @@ function logWhisper(spyOn) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase {
+  if (string.toLowerCase() === string) {
     return("I can\'t hear you!")
-  } else {
-    if string === string.toUpperCase {
-      return("YES INDEED!")
-    } else {
-      if string = "I love you, Grandma."
-      return("I love you, too.")
+    } else if (string.toUpperCase() === string) {
+    return("YES INDEED!")
+    } else if (string === "I love you, Grandma.") {
+    return("I love you, too.")
     }
-    }
-
-    }
+}
