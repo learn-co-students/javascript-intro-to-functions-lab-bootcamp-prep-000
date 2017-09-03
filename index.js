4e98785ad0c6832f5936dff7e7f6d552867989eb
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-03 20:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -7,27 +7,24 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  return `string`
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
 }
-
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
