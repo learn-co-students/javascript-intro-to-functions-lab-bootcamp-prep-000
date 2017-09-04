ba27c1b862b76306b6f42e8be545eb21d36275a2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-04 20:40:29 +0000

    Done.

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-    if (string === lowercase) {
-    string = "I can't hear you!";
-    }
-    return string()
+    if (string === string.toLowerCase()) {
+  return "I can't hear you!";
+}  else if (string === string.toUpperCase()){
+    return "YES INDEED!";
+  }  else (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
