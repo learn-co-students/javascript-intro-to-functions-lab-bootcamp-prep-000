d1d421143fd8b9245a9cb7beff3250d8e3a9254e
Sen Tezeno <sen63092@yahoo.com>
2017-09-05 21:57:47 +0000

    Done.

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("HELLO")
+  console.log(string.toUpperCase())
 }
 function logWhisper(string){
-  console.log("hello")
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-  if (string === "hello")
-return string = ("I can\'t hear you!")
-else if (string === "HELLO")
-return string = ("YES INDEED!")
-else if (string === "I love you, Grandma.")
-return string = ("I love you, too.")
+  if (string.toLowerCase() === string){
+    return "I can\'t hear you!"
+  }
+
+if (string.toUpperCase() === string){
+  return "YES INDEED!"
+}
+
+if (string === "I love you, Grandma."){
+  return "I love you, too."
+}
+
 }
