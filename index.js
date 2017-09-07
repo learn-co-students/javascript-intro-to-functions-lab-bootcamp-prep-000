2fcc5ba7b00a767b0e3444e65c9a93545323cec0
Mary <l.skyling@gmail.com>
2017-09-07 13:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
-function lowWhisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase());
+}
+function sayHiToGrandma (string){
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string.toLowerCase() === string){
+    return "I can\'t hear you!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
