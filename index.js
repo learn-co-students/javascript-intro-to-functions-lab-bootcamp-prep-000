e276d90be43eedf0bf47590c8407f4c52ab9b1c5
rsmile74 <smithrds74@hotmail.com>
2017-09-07 03:08:49 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  return string.toLowerCase()
-  return string.toUpperCase()
+function sayHiToGrandma(string) {
+
+ if (string.toUpperCase() === string) {
+    return `YES INDEED!`
+  }
+ else if (string.toLowerCase() === string) {
+    return `I can't hear you!`
+  }
+ else if (string === 'I love you, Grandma.') {
+   return `I love you, too.`
+  }
 }
