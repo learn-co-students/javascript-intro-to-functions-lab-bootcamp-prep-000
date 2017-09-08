82825c57b431918318ab2199bb41456b6199b2d0
Saheel Wagh <saheelwagh@gmail.com>
2017-09-08 08:08:54 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
-
+ 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
