d033bf946a37bd96ef379f8fca0b2b48b8c6123a
Baasandorj Zagdsuren <baaasandorj@gmail.com>
2017-09-09 20:31:35 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(string) {
 function logShout(string) {
   console.log(`${string.toUpperCase()}`)
 }
+
+function logWhisper(string) {
+  console.log(`${string.toLowerCase()}`)
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else {
+    return "I love you, too."
+  }
+}
