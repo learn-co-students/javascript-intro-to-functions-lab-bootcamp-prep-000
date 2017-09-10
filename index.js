c7bb3129c2bc9eaeb15a1ed8d93a0bdeba7a0de8
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-10 20:46:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+  console.log()
+}
+function sayHiToGrandma(string) {
+  returns 'I cant hear you!'
+  if lowercase
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
