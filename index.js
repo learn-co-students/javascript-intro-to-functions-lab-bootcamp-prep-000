2376ea1a5707721ce427447f2e862fffefefd92d
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-11 18:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = string
-
-  if uppercase.toUpperCase() === string {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
+  }
+
+  else if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
