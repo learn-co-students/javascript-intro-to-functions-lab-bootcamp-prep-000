203c38b7bc85cebd4a82515fe12dfe6552cff6fb
Tom Hynes <thomas.hynes@outlook.com>
2017-09-12 16:23:47 +0000

    Done.

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ function logShout(string) {
 function sayHiToGrandma(string) {
   if(string === string.toUpperCase())
     return("YES INDEED!");
-  else if (string === string.toLowerCase())
-    return("I can't hear you!");
+
   else if (string === ("I love you, Grandma."))
     return("I love you, too.");
+  else
+    return("I can't hear you!");
 }
