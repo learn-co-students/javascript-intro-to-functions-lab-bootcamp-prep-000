f64db2064e458dfa77cc5c6892fa81ba5597acac
Jon Exume <jonexume@gmail.com>
2017-09-12 04:57:40 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
   return "I can\'t hear you!"
 }
+if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+}
+
+if (string === "I love you, Grandma.") {
+   return "I love you, too."
+ }
 }
