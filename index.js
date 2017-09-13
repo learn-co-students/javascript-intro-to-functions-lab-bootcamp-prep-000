88a898a9ef4d967b2fc8783013285e1e5ff760cd
Ronald LI <ronaldli21@yahoo.com>
2017-09-13 04:47:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function shout (string) {
+function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper (string) {
+function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout (string) {
+function logShout(string) {
   console.log(string.toUpperCase())
 }
 
@@ -15,17 +15,17 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
+if (string.toLowerCase() === string) {
+return "I can't hear you!"
+}
 
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
+if (string.toUpperCase() === string) {
+return "YES INDEED!"
+}
 
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
+if (string === "I love you, Grandma.") {
+return "I love you, too."
+}
 
-  return "Are you eating enough?"
+return "Are you eating enough?"
 }
