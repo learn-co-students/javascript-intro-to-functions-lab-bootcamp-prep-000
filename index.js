c648632116bca7f4516852395db677e4c20c3008
Jordan Rumeld <jordanrumeld@me.com>
2017-09-14 21:39:41 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ console.log("HELLO")
 function logWhisper(string) {
 console.log("hello")
 }
+function sayHiToGrandma(string) {
+  if (string===string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string===string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string==="I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
