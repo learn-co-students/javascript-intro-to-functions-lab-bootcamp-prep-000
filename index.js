f415ac9642e59ab9e63ab629500d46d027a30f8f
Daniel Rotchford <jimrockford45@gmail.com>
2017-09-15 19:10:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+
+return(string).toUpperCase()
+
+'Hello!'.toUpperCase
+
+}
+
+// Test 1 Complete :)
+
+function whisper(string) {
+
+  return(string).toLowerCase()
+
+  'hello'.toLowerCase
+}
+
+// Test 2 Complete :D
+
+function logShout(string) {
+
+'hello'.toUpperCase
+
+  console.log(string.toUpperCase())
+
+  }
+
+// Test 3 Complete :O
+
+function logWhisper(string) {
+
+  'HELLO'.toLowerCase
+
+console.log(string.toLowerCase())
+
+}
+
+// Test 4 Complete
+
+function sayHiToGrandma(string) {
+
+'hello'.toLowerCase()
+
+return (string).toLowerCase()
+
+}
