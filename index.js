53a05908872f2dbfd12b9032a05382a38a3bbe71
Jarrod Dyer <jarrod@gasganno.com>
2017-09-16 00:22:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+ return string.toUpperCase()
+}
+
+function whisper(string) {
+ return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+ console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+ var text = string
+ if (text.toLowerCase() === text) {
+   return "I can't hear you!"
+ } else if (text.toUpperCase() === text) {
+   return "YES INDEED!"
+ } else if (text === "I love you, Grandma."){
+   return "I love you, too."
+ }
+}
