c85c339aa2f713747a404ec48e529180e9d7f818
RenataCodes <psych.coaching@gmail.com>
2017-09-17 13:53:36 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var string = 'hello'
-  if (string.toLowerCase() === "string") {
-  return "I can\'t hear you" }
+if (string.toLowerCase() === string) {
+  return "I can\'t hear you!"
+}
+if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+}
+if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
 }
