953f076c31ce77e3a0d5184e72b1b15e7ae74562
Marina Kosovic <marinakosovic15@gmail.com>
2017-09-18 14:40:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log (string.toUpperCase())
+}
+
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  console.log(string.toLowerCase())
+  if (true) {
+     return `"I can't hear you!"`
+  }
+console.log(string.toUpperCase())
+if else (true) {
+  return `"YES INDEED!"`
+}
+}
