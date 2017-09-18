5cb5c33eff4b15f2038fe46bb68d5a8f7ad82272
Mohammed Mahdi <mak.zero.x@gmail.com>
2017-09-18 22:50:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase() )
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase() )
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string)
+    return "I can\'t hear you!";
+
+    else if
+
+      (string.toUpperCase() === string)
+      return "YES INDEED!";
+
+      else
+
+      (string === ("I love you, Grandma."))
+      return "I love you, too."
+
+
+
+
+}
