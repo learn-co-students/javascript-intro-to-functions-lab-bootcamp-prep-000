47c5525f40f4d4e50b5d4f3ebcef79acc0975e3c
Lorrie Pearson <pearsonlorrie@gmail.com>
2017-09-19 01:15:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toUpperCase
+}
+console.log('')
+
+function logWhisper(string) {
+  return string.toLowerCase
+
+}
+console.log('')
+
+function sayHiToGrandma(string) {
+  string = toLowerCase
+  return string.toLowerCase
+  "I can\'t hear you!"
+}
