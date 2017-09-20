da6e8f7eafd7e4000b386af700cf4a0841eca8a4
imendoza06 <imendoza9691@gmail.com>
2017-09-20 15:57:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+i
+function shout(string) {
+	return string.toUpperCase()
+}
+
--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,6 @@
 
+
+describe('shout(string)', function() {
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
