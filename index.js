e4581ea645fa6bc353deac937f33ef81e669b647
Jessica Gross <jessicasarahgross@gmail.com>
2017-09-20 03:21:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
- function whisper(string) {
-   return string.toLowerCase();
- }
+function whisper(string) {
+  return string.toLowerCase()
+}
 
-function logShout(string){
-  console.log(string.toUpperCase());
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase());
-}
-//sayHiToGrandma(string) returns "I can't hear you!" if `string` is lowercase:
-function sayHiToGrandma(string) {
-  if(string === string.toLowerCase())
-  return 'I can\'t hear you!'
-}
-//sayHiToGrandma(string) returns "YES INDEED!" if `string` is uppercase:
-function sayHiToGrandma(string) {
-  if(string === string.toUpperCase())
-  return 'YES INDEED!'
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if(string === 'I love you, Grandma.')
-  return "I love you, too."
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
