0d729258f5953d2eaa5f051fa89b03beb5314de8
carlitos321 <ca.cabrera321@gmail.com>
2017-09-20 16:13:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return string.toUpperCase()
+}
+
+console.log(string)
