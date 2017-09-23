40e72299188933e9cfa97f6be69aa4c869ac94cf
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-09-23 00:18:48 +0000

    Done.

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
