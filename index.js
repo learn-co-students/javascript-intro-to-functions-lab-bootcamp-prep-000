4a732cdebc1a169b2e849c0ff473127c41da8789
tredwood89 <tredwood89@gmail.com>
2017-09-23 01:44:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(HOME) {
+  return HOME.toLowerCase()
+}
+
+
+function logShout(loud) { console.log (loud.toUpperCase() )
+}
+
+function logWhisper(LOW) {console.log (LOW.toLowerCase() )
+}
+
+function sayHiToGrandma(HOME) {return "YES INDEED" }
