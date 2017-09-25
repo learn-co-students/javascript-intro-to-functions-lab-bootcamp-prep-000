5dcde5502a9c320483c4b079c3f1c4a10dfa684d
Elisabeth L <elisabethsochacki@gmail.com>
2017-09-25 19:46:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+var uppercase = "HELLO!"
+uppercase.toUpperCase()===uppercase//true
+var lowercase = 'hello!'
+lowercase.toLowerCase()===lowercase//true
+varmixedCase = 'Hi there!'
+mixedCase.toLowercase()===mixedCase//false
+mixedCase.toUpperCase()===mixedCase//false
