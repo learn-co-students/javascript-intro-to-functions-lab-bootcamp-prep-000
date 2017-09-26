22d2b4c24974ad411d91f8a6f33c24191fea5814
Cedric Niguidula <cedcn-9mail@yahoo.com>
2017-09-26 04:39:32 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   }
   else if (string.toUpperCase() === string) {
     return "YES INDEED!"
