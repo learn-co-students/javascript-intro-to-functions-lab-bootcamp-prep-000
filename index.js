fda0a885d22af4c3d7a1cca24172a9957fcf01af
Dan Daly <dan.john.daly@gmail.com>
2017-09-28 17:03:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(str) {
+  return str.toUpperCase()
+}
+
+function whisper(str) {
+  return str.toLowerCase()
+}
+
+function logShout(str) {
+  console.log(shout(str));
+}
+
+function logWhisper(str) {
+  console.log(whisper(str));
+}
