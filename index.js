8f6e964cc811dfcc579a55a52114b603784ace46
Dan Daly <dan.john.daly@gmail.com>
2017-09-28 17:07:20 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(str) {
 function logWhisper(str) {
   console.log(whisper(str));
 }
+
+function sayHiToGrandma(str) {
+  if(str === str.toLowerCase())
+    return "I can't hear you!"
+
+  if(str === str.toUpperCase())
+    return "YES INDEED!"
+
+  if(str === "I love you, Grandma.")
+    return "I love you, too."
+}
