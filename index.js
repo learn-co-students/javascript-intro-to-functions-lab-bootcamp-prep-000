f8e25a91c626c2b6d318eba4b035d65225ba066d
jonathan Morton <jmorton4193@gmail.com>
2017-09-29 01:02:36 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ function whisper (string){
 }
 function logShout (string) {
   console.log('string'.toUpperCase())
-return string.toUpperCase()
+console.log('Hello'.toUpperCase())
 }
+function logWhisper (string) {
+  console.log('string'.toLowerCase())
+console.log('hello'.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  if (string==='hello'.toLowerCase())
+  {return 'I can\'t hear you!'}
+  else if (string==='HELLO'.toUpperCase())
+  {return 'YES INDEED!'}
+  else (string==='I love you, Granma.')
+  {return 'I love you, too.'}
+}
+sayHiToGrandma('hello')
+sayHiToGrandma('HELLO')
+sayHiToGrandma("I love you, Grandma.")
