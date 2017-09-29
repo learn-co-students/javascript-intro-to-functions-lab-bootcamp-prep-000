b534373812b02e308a68572f9a83f7a5769f7f70
Annette Michaels <micha232@umn.edu>
2017-09-29 07:20:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var uppercase = "HELLO!"
+var lowercase = "hello!"
+var mixedCase = ''
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === lowercase) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
