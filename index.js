65d0ea39400a01c8d29cfbc9e2868cbdd350525b
Tyler Wales <walestyl@gmail.com>
2017-09-30 20:24:44 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+
+}
