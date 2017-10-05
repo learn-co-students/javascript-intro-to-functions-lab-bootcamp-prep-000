e21fad77828fc580bf313dc0cb41b1e51087dfa2
Ian Pavlovic <ian_pavlovic@msn.com>
2017-10-05 20:35:17 +0000

    Done.

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return ("I can\'t hear you!")}
+    return ("I can't hear you!")
+  }
   else if (string.toUpperCase() === string) {
-    return ("YES INDEED!")}
+    return ("YES INDEED!")
+  }
   else if (string === "I love you, Grandma.") {
-    return ("I love you, too.")}
+    return ("I love you, too.")
+  }
 }
