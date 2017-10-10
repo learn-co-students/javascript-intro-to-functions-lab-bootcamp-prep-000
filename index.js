b8ea7d1297925d68c1968904a948f3cd19709ac2
Noelle <noelle.legrain@gmail.com>
2017-10-10 18:12:54 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+}
+function sayHiToGrandma(string) {
+    var lowercase = string
+    var uppercase = string
+    var mixedCase = string
+    if (lowercase.toLowerCase() === lowercase) {
+      return "I can't hear you!"
+    } else if (uppercase.toUpperCase() === uppercase) {
+      return "YES INDEED!"
+    } else if ("I love you, Grandma." === mixedCase) {
+      return "I love you, too."
+    }
+
+}
