eeea4b376f10d13fb740d1f73d78fee649382111
bruce leong <brucewleong@gmail.com>
2017-10-11 23:33:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function shout(string) {
-  return string.toUpperCase()
-}
 function whisper(string) {
   return string.toLowerCase()
-}
-function logShout(string) {
-  console.logShout(string).toUpperCase()
 }
