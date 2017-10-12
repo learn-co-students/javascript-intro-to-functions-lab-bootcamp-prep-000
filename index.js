aee02460fe3493e07fc0dfe44a6a89d340e37194
David Brennan <ddbren@gmail.com>
2017-10-12 19:01:13 +0000

    Done.

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string) {
     return 'YES INDEED!'
   } else if (string === "I love you, Grandma.") {
     return 'I love you, too.'
+  } else {
+    return 'Are you eating enough?'
   }
 }
