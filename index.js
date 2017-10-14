1b9ce0244750fe8a16144aa19b1840186c8ad856
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 14:43:12 +0000

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO", lowercase = "hello"
+  var uppercase = "HELLO", lowercase = "hello", loveYou = "I love you, Grandma."
 
   if (string === lowercase) {
     return "I can't hear you!"
   }
   else if (string === uppercase) {
     return "YES INDEED!"
+  }
+  else if (string === loveYou) {
+    return "I love you, too."
   }
 
 }
