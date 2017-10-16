3c9c32ddf900f9cfd4f420c4e68dc87f1ff90f05
Ashlie Witherspoon <ashlie0692@hotmail.com>
2017-10-16 21:47:36 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ console.log (string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
   }
-  else if (string === string.toUpperCase) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
