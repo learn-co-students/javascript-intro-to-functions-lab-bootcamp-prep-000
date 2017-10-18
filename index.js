c77328d6e68a181036d6c447ab2c8fb9f5ed8195
DanceTherapeutics <dancetherapeutics@gmail.com>
2017-10-18 14:48:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function logShout(string){
+  console.log('HELLO')
+}
+if (string === 'hello'){
+function sayHiToGrandma(string){
+  console.log("I can't hear you!")
+
+} else if (string === 'HELLO'){
+function sayHiToGrandma(string){
+  console.log("YES INDEED!")
+
+} else {
+function sayHiToGrandma(string){
+  return("I love you, too!")
+}
