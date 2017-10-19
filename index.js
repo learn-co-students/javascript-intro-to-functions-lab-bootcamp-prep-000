fd85e656ddc8dacaf7b45544d2048da8d2f8d00b
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-19 18:49:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+
+  uppercase.toUpperCase() === "YES INDEED!"
+
+  var lowercase = 'hello!'
+
+  lowercase.toLowerCase() === "I can't hear you!"
+
+  var mixedCase = 'Hi there!'
+}
