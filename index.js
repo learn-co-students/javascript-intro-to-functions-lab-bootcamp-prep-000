36d0d11d4840bc1be2fe1c122273861f8bdf7def
Mia Elisenberg <mi-ya@live.no>
2017-10-20 14:49:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var uppercase = "HELLO!"
+var uppercase = "YES INDEED!"
 uppercase.toUpperCase() === uppercase
 
 var lowercase = 'hello!'
@@ -24,16 +24,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
-    return 'I can\'\ t hear you!'
-  }
-  else if (string === uppercase) {
+  if (uppercase) {
     return 'YES INDEED!'
   }
-  else if (string === 'I love you, Grandma') {
-    return 'I love you, too'
+  else if {
+    return 'I can\'t hear you!'
   }
-  else {
-    return 'What?'
+  else if (sayHiToGrandma(string) === 'I love you, Grandma') {
+    return 'I love you, too'
   }
 }
