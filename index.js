565579bc785d7f0877a267203c2d866b0a94e3a8
David McGrann <dpmcgrann@gmail.com>
2017-10-22 21:14:08 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function logShout() {
 function logWhisper() {
   console.log('hello')
 }
-function sayHiToGrandma(whisper) {
-  return string.toLowercase("I can't hear you!")
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    var lowerCase = string.toLowerCase()
+    return ("I can\'t hear you!");
+  }
+  else if (string === string.toUpperCase()) {
+    var upperCase = string.toUpperCase()
+    return ("YES INDEED!");
+    }
+    else{
+      var mixedCase = "I love you, too."
+      return(mixedCase)
+    }
 }
