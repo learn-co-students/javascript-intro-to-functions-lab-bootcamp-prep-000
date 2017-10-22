c2849ab36d50ae4cfed9ce0a8ecb809fdfefc331
David McGrann <dpmcgrann@gmail.com>
2017-10-22 19:28:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout() {
+  console.log('HELLO')
+}
+function logWhisper() {
+  console.log('hello')
+}
+function sayHiToGrandma(whisper) {
+  return string.toLowercase("I can't hear you!")
+}
