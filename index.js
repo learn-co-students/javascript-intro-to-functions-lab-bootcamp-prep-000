3eb09bd3ee819265bfe47c3f8dd2094f0d12a4ef
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-10-24 18:23:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+
+string.toLowerCase()=== string
+return string
+}
+function sayHiToGrandma(string){
+
+string.toUpperCase()=== string
+return string
+}
+
+function sayHiToGrandma(string)
+{
+  return string
+}
