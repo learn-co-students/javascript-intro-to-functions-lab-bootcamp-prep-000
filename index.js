2f6aea0f9ab810e66975087962e23359cccab3a6
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-27 11:53:07 +0000

    Done.

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 } else if (string.toUpperCase()===string){
   return "YES INDEED!"
+} else if (string==='I love you, Grandma.'){
+  return "I love you, too."
 }}
