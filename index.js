65dd6e99e3b51b3fb0ace05eb288baab3f215323
Tylert Davis <tylerddavis@rocketmail.com>
2017-10-28 00:47:31 +0000

    Done.

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
-  return string.toUpperCase("I can't hear you!")
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
