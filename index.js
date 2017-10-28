f10bc8a48ff71de46816bc723185a0e463653452
Tim Shipman <bricklayertim@yahoo.com>
2017-10-28 17:25:56 +0000

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper(string))
 }
+
+function sayHiToGrandma(string) {
+  if (whisper(string) === string) {
+    return "I can't hear you!"
+  }
+
+}
