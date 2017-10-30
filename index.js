36466cd39db0527b848d52b19f38a69463bdd675
craig sperlazza <craig.sperlazza@gmail.com>
2017-10-30 14:46:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+
+  if (string === "I love you, Grandma." ){
+    return "I love you, too.";
+
+} else if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+
+} else if (string.toUpperCase() === string ) {
+    return "YES INDEED!";
+}}
