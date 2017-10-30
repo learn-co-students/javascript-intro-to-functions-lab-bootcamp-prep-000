dbecd07c37cfed61a2cd7de1ba06516d4c46a4a9
Kent Picou <kpicou@wesleyan.edu>
2017-10-30 21:22:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(HELLO) {
+  return HELLO.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log ('hello'.toLowerCase())
+}
+
+//function sayHiToGrandma(hello){
+//  return "I can't hear you!"
+//}
+
+function sayHiToGrandma(HELLO){
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(I love you, Grandma.){
+  return "I love you, too."
