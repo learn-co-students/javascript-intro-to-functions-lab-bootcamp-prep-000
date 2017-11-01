36c329307a668d4abeb119344b90256325471e48
Laura Travin <laura.travin@gmail.com>
2017-11-01 07:20:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+
+   return string.toUpperCase()
+}
+
+function whisper(string) {
+
+   return string.toLowerCase()
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+
+ }
+
+ function logWhisper(string){
+     console.log(string.toLowerCase());
+
+  }
+
+  function sayHiToGrandma(string){
+
+    if ('hello' == string ){
+       return ("I can't hear you!")
+
+    }
+    if  ('HELLO' == string ) {
+         return ("YES INDEED!")
+
+    }
+    if  ('I love you, Grandma.' == string ) {
+         return ("I love you, too.")
+
+    }
+  }
