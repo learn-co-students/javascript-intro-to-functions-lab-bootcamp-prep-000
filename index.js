dd8ec1a6b6d5ff8e9115f0e738fec128866e9a45
Karen Zhao <karen.zhao18@gmail.com>
2017-11-02 02:36:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return whisper.toLowerCase()
+}
+function logShout(string) {
+  console.log(string).toUppercase()
+}
+function logWhisper(string) {
+  console.log(string).toLowercase()
+}
+if (string.toLowerCase()===string) {
+  return "I can't hear you!"
+}
+if (string.toUpperCase()===string) {
+  return "YES INDEED!"
+}
+if (string==="I love you, Grandma.") {
+  return "I love you, too."
+}
