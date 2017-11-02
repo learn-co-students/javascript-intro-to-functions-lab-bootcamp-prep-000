10334913b319592e01ee725eb1bb89153155cbda
JSpeed8th <jspeed8th@gmail.com>
2017-11-02 03:56:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase() )
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase() )
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+}
+
+function sayHiToGrandma(string) {
+  return "YES INDEED!"
+  string.toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  return "Ilove you, too"
+  string("I love you, Grandma.")
+}
