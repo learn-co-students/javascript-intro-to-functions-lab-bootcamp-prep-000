1ac47a6c148aa8c281107fef1d225098b4fb3084
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 04:09:31 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }else if (string==="I love you, Grandma."){
     return "I love you, too."
-  }else (string=string){
+  }else (string=string)
     return "I can't hear you!"
   }
-}
