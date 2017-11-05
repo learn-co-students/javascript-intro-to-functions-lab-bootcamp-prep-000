c43a4fbcd688eaf19937acffaa6c1ab63585a5ee
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 04:25:40 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase()===string){
-    return "YES INDEED!"
-  }else if (string==="I love you, Grandma."){
-    return "I love you, too."
-  }else (string=string)
+  if (string.toLowerCase()===string){
     return "I can't hear you!"
+  }else if (string.toUpperCase()===string){
+    return "YES INDEED!"
+  }else (string==="I love you, Grandma.")
+    return "I love you, too."
   }
