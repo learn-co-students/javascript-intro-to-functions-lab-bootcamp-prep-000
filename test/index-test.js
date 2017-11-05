71df128f3c53de74aeedf543aaea5e3eabf308ca
renacka burgess <renacka@aol.com>
2017-11-05 00:45:08 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -59,20 +59,21 @@ describe('sayHiToGrandma(string)', function() {
     var lowerCase = string.toLowerCase();
       return ('I can\'t hear you!');
 
-
+}
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
 
-         if (string === string.toUpperCase()){
-        return "YES INDEED!"
-}
+    if (string.toUpperCase() === string){
+          return 'YES INDEED!';
+        }
+
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
 
-    if (string === string.toUpperCase()){
-   return "YES INDEED!"
-}
+    if (string === 'I love you, Grandma.'){
+          return 'I love you, too.';
+        }
 
 
 })
