3f10fbca8c27c40996ef9334b037c051b3c31b59
John Corujo <corujojohn@gmail.com>
2017-11-05 14:37:37 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('hello'.toLowerCase);
+  console.log('hello'.toUpperCase());
 }
+
+function logWhisper(string) {
+  console.log('hello'.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+ else if (string === string.toUpperCase()) {
+   return "YES INDEED!";
+ }
+    else if (string === "I love you, Grandma.") {
+      return "I love you, too."
+}}
