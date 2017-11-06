4f1ccc01cf20e6226c854c779d84747f8a32e93d
Rani <rani76@gmail.com>
2017-11-06 13:02:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+return string.toUpperCase()
+  }
+
+fucntion whisper(string) {
+  return string.toLowerCase
+}
+
+function logShout(string) {
+  return string.caps
+}
+
+console.log(logShout)
