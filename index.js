0282b282d1fdb7320968bc2108138a10597edabc
Ian Graves <ian.graves@gmail.com>
2017-11-07 10:28:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string) {
 }
 function sayHiToGrandma (string) {
   if (string === string.toUpperCase())
-    return "YES INDEED!"}
-  else if (string === string.toLowerCase()){
+    return "YES INDEED!"
+  }else if (string === string.toLowerCase()) {
     return "I CAN'T HEAR YOU!"
-  }else if (string === "i love you, Grandma."){
+  }else if (string === "i love you, Grandma.") {
     return "I love you, too."
   }
