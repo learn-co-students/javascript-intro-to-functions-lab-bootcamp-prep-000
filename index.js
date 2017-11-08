7dc6571912ad081408028ddb91a6d9d258a729ba
marshrawley1 <marshallrawley@gmail.com>
2017-11-08 19:39:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var uppercase = "HELLO!"
+
+if (uppercase.toUpperCase() === uppercase) //true
+return "I can't hear you!"
+
+
+var lowercase = 'hello!'
+
+if (lowercase.toLowerCase() === lowercase) // true
+ return "I can't hear you!"
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+
+
+function sayHiToGrandma(string) {
+  return string.
+}
