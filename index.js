c03a194876b4864c1f742f7bb9feee54b0462ac9
Stefanie Brabec <sbrabec@me.com>
2017-11-10 21:29:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log ("HELLO")
+}
+function logWhisper(string) {
+  console.log ("hello")
+}
+function sayHiToGrandma() {
+  if("hello") return("I can\'t hear you!")
+  if("HELLO") return("YES INDEED!")
+  if("I love you, Grandma.") return("I love you, too.")
+}
