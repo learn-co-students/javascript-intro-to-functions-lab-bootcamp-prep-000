d50dc9f818a5cfce343f772cfc214452de6e2a16
Eduardo Magalhaes <ed83isme@gmail.com>
2017-11-11 01:49:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string){
+  if (string == "I love you, Grandma.") return "I love you, too.";
+  if (string == shout(string)) return "YES INDEED!"; 
+  return "I can't hear you!";
+}
