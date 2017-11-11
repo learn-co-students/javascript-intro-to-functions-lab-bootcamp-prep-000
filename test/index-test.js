f075c56672992c7ddfd93e1369eb371f5bfde30b
Silvio Filho <smfilhospam1@gmail.com>
2017-11-11 19:44:08 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,46 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+//if (string.toLowerCase() === string) { return the phrase}
+function sayHiToGrandma(string){
+
+   if (string.toUpperCase() == string){
+      return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+      return   "I can't hear you!"
+  } else if (string === "I love you, Grandma."){
+      return "I love you, too."
+  }
+}
+
+
+//function sayHiToGrandma(string)
+//{
+  //return string.toUpperCase()
+
+  //else if string.toLowerCase() === lowercase {
+  //  console.log('I cant hear you')
+  //}
+  //else if string === 'I love you, Grandma' {
+  //  console.log('I love you, too')
+  //}
+//}
+
+
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
