43482e2b8c1be50d96c6a8a8ab66be8ba21b6bbe
Mario Rodrigues <mariorodjr92@gmail.com>
2017-11-11 15:48:04 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
    return "I can't hear you!"
- } else if (string.toUpperCase()) {
+ } else if (string === string.toUpperCase()) {
    return "YES INDEED!"
  } else {
    return "I love you, too."
