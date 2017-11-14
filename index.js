754df17a16580005c47ed09171f5b0cd99f59bf8
Veronica Kim <vero.a.kim@gmail.com>
2017-11-14 13:03:53 +0000

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return string.toLowerCase()
 }
-// function sayHiToGrandma(string) {
-//   return string.toUpperCase()
-// }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+    return 'I can\'t hear you!';
+  else if (string === string.toUpperCase())
+    return 'YES INDEED!'
+  else if (string === "I love you, Grandma.")
+    return 'I love you, too.';
+}
