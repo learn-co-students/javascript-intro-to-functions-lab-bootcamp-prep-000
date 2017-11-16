34031cdf139666450da7204a84556b8b0265131a
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-16 23:23:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+
+
+function sayHiToGrandma(string)
+
+
+if {string === string.toUpperCase}
+console.log ("I can't hear you")
+else if{string === string.toLowerCase }
+console.log("YES,INDEED")
