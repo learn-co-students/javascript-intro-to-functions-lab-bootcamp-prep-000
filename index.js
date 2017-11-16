bec05bcb9e1864c6c8593e5ac3edbdc9b81832e2
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-16 04:49:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string = 'hello') {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+  }
+  function logShout(string) {
+    console.log()
+    
+  }
+  
+
+
+
