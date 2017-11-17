a6be984cf1d2f6f787ee145f62ca3ed358a424ff
Mikkel J Ridley <mikkel250@gmail.com>
2017-11-17 07:16:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
+
+function logShout(string) {
+  console.log(logshout(string))
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+  }
+  
+  
