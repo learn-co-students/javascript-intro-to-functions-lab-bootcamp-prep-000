028e4e801dc65685a5ac9bf5251f7bffad42e00d
Mikkel J Ridley <mikkel250@gmail.com>
2017-11-17 08:28:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+var uppercase, lowercase
+
+
 function shout(string) {
   return string.toUpperCase()
 }
@@ -12,12 +15,25 @@ function whisper(string) {
 
 
 function logShout(string) {
-  shout()
-  console.log(logshout(string))
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  console.log(uppercase);
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  console.log(lowercase);
   }
   
-  
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return 'I can\'t hear you!';
+  }
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!'
+  }
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+  }
