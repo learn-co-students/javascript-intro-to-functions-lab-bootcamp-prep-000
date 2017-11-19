e4a4258d7287ed646a687ba8b14a0bac75da61e0
Ronald DeBeras <rdeberas@gmail.com>
2017-11-19 01:44:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(hello) {
+  console.log('HELLO')
+}
+function logWhisper(hello) {
+  console.log('hello')
+}
+function sayHiToGrandma(hello) {
+  return ("I can't hear you!")
+}
+function sayHiToGrandma(hello) {
+  if (hello === hello.toUpperCase()) {
+  return ("YES INDEED!")
+}
+  else if (hello === hello.toLowerCase()) {
+  return ("I can't hear you!")
+}
+  else if (hello === "I love you, Grandma.") {
+  return ("I love you, too.")
+  }
+}
