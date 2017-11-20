2d96dca00cb0b8343988289453e2dd74638465b8
fancyan fan <201500261074@mail.sdu.edu.cn>
2017-11-20 08:56:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 function shout(string){
   return string.toUpperCase()
 }
-
-var upper="HELLO"
-if(upper.shout()==upper)
-consult.log("good")
-
-
-
 function whisper(string){
   return string.toLowerCase()
 }
-
-
 function logShout(string){
-  consolt.log(string.toUpperCase())
+  console.log(string.toUpperCase())
 }
-
-
 function logWhisper(string){
-  consolt.log(string.toLowerCase())
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if(string=="I love you, Grandma.")
+  {return "I love you, too."}
+  else if(string==string.toLowerCase())
+  {return "I can't hear you!"}
+  else if(string==string.toUpperCase())
+  {return "YES INDEED!"}
 }
