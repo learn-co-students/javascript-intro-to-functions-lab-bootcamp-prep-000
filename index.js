be70da77844ac553e14f1b2591bcf6a19a2a4ad2
fancyan fan <201500261074@mail.sdu.edu.cn>
2017-11-20 08:18:52 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function shout(string){
   return string.toUpperCase()
 }
 
+var upper="HELLO"
+if(upper.shout()==upper)
+consult.log("good")
+
+
 
 function whisper(string){
   return string.toLowerCase()
