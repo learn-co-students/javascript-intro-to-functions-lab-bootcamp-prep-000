9ae6eb8385252cd4e979f4ae65ab606bb4ac3f9c
Sushant Parajuli <sushantb13@gmail.com>
2017-11-21 21:44:21 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(String){
-  var case = string.toUpperCase
-console.log  (case)
+function logShout(string){
+console.log (string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log (string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+var test = string.toLowerCase()
+var message = "I can't hear you!"
+var back = "I love you, Grandma."
+if (string === test){
+  return message
+} else if (string === string.toUpperCase()){
+   return "YES INDEED!"
+} else if (string === back) {
+  return "I love you, too."
+}
 }
