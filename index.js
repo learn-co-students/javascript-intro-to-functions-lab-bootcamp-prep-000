bd06c24c7424e60eecc6557a919512fb3022b7c8
Justin Klepach <justin.klepach93@gmail.com>
2017-11-21 21:18:57 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ if (string == string.toLowerCase()){
 return "I can\'t hear you!"}
 if (string == string.toUpperCase()){
 return "YES INDEED!"}
-if (string == console.log("I love you, Grandma.")){
-  return "I love you, too."}
+if (string == "I love you, Grandma.") {
+  return "I love you, too."
+}
 }
