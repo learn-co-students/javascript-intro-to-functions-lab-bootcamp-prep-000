566ff2beb7859fb6601664b1862d9548e5a8d238
William Thompson <wmthmps1@gmail.com>
2017-11-21 22:29:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var function shout(string){
+  return string.toUpperCase()
+}
+
+var function whisper(string){
+  return string.toLowerCase
+}
+
+var function logShout(string){
+  console.log(string).toUpperCase()
+}
