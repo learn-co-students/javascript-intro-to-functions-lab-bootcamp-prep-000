e53ea70409ce5e614467d5f820c4a62be09bdbdc
Megan Collier <meganraec@live.com>
2017-11-22 06:19:31 +0000

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,5 @@ function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) return ("I can\'t hear you!");
   else if (string == string.toUpperCase()) return ("YES INDEED!");
   else if (string == "I love you, Grandma.") return ("I love you, too.");
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase === uppercase
-  var lowercase = "hello!"
-  lowercase.toLowerCase === lowercase
+
 }
