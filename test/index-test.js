4b9cae3c785e17d51eec8c594b09656e7dcb2426
Benjamin Johnson <odiowab@gmail.com>
2017-11-22 11:19:33 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,7 @@ describe('shout(string)', function() {
   })
 })
 
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
