d34e8d5716581dba8f751cd6b077794d1c99c46b
Megan Collier <meganraec@live.com>
2017-11-22 06:13:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var string = "Hello";
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log("HELLO");
+}
+
+function logWhisper(string) {
+  console.log("hello");
+}
+
+shout();
+whisper();
+
+function sayHiToGrandma(string) {
+  if (string == string.toLowerCase) return ("I can\'t hear your!");
+  else if (string == string.toUpperCase) return ("YES INDEED!");
+  else if (string == "I love you, Grandma.") return ("I love you, too.");
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase === uppercase
+  var lowercase = "hello!"
+  lowercase.toLowerCase === lowercase
+}
