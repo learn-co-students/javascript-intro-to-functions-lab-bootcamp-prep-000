8907e206fa95065f33d22920dc9c5a18338f74f5
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-11-26 06:59:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(hello) {
+  return console.log(hello.toUpperCase())
+}
+
+function logWhisper(hello) {
+  return console.log(hello.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+}
