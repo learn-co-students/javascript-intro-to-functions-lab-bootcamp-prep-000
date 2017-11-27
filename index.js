4dd520a9d826ac131893d17219f0b78cb03fd738
Eric Bearfield <embearfield@gmail.com>
2017-11-27 18:52:31 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "hello") { 
+  if (string.toLowerCase() === string) { 
     return "I can't hear you!";
   }
-    else if (string === "HELLO") {
+    else if (string.toUpperCase() === string) {
     return "YES INDEED!";
     }
     else if (string === 'I love you, Grandma.') { 
