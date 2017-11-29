b493d4485914be06318846c0e49a44a8ede4dc5d
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-11-29 23:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,29 @@ console.log( string.toLowerCase())
 console.log( string())
 }*/
 
+function sayHiToGrandma(string, STRING) {
+  return("I can't hear you!")
+  //return(`It's ${days} days until ${holiday}!`)
+  //return(`It's ${days} days until ${holiday}!`)
+}
 
+function sayHiToGrandma(string) {
+  return("YES INDEED!")
+  //return(`It's ${days} days until ${holiday}!`)
+  //return(`It's ${days} days until ${holiday}!`)
+}
+
+function sayHiToGrandma(string) {
+  if (string == "hello") {
+    return("I can't hear you!")
+  }
+  else if (string == "HELLO") {
+    return("YES INDEED!")
+  }
+  else {string == "I love you, Grandma." 
+    return("I love you, too.")
+  }
+  //return(`It's ${days} days until ${holiday}!`)
+  //return(`It's ${days} days until ${holiday}!`)
+}
 
