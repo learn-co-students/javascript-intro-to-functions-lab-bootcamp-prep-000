4981d2e1546181523978b943e19e1ef4443fbef1
BillyPittman1993 <billytrading93@gmail.com>
2017-11-29 01:25:10 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()){
-    return "I can't hear you!"
+ return string.toLowerCase()
   }
