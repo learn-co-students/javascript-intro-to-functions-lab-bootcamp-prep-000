f5494a8929b996baf1bb5b49d7032f7a7dfdaf23
Stephanie <s.a.james5@gmail.com>
2017-11-30 17:51:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function logWhisper(string) {console.log(string.toLowerCase('hello'))}
 function sayHiToGrandma (string) {
   if (string === 'hello')
     return ("I can't hear you!")
+
   if (string === "I love you, Grandma.")
     return ("I love you, too.") 
+
   if (string === 'HELLO') 
     return ("YES INDEED!")}
