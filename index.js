707e3ccdcd2da456a0b13ef86cd3c84d48f32bbc
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-03 11:49:24 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+
+  if (string==="I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else if (string.toUpperCase===string) {
+    return "YES, INDEED!"
+      }
+  else if (string.toLowerCase===string) {
+    return "I can't hear you!"
+  }
+
+
+}
