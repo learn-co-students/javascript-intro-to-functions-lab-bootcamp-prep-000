4bad891df460f5a6a43b8e7301d31a9a606b8ac3
Joseph C <jciv.coding@gmail.com>
2017-12-04 01:09:40 +0000

    Done.

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you, Grandma.") return "I love you, too.";
   else if (string === string.toUpperCase()) return "YES INDEED!"
+  else if (string === "I love you, Grandma.") return "I love you, too.";
 }
 
 sayHiToGrandma("hello");
 sayHiToGrandma("HELLO");
 sayHiToGrandma("I love you, Grandma.");
 
-\\ After two hours, I realized that my approach, initially, was very rigid. I expected an algorithm/function to adapt & mold itself around a
-\\ set of static variables when I should've thought of it as a static algorithm that could handle diverse variables.
-\\ I also hadn't yet figured out that I could chain two functions on the same line, or separate lines with semicolons - but now I know lmao.
+// After two hours, I realized that my approach, initially, was very rigid. I expected an algorithm/function to adapt & mold itself around a
+// set of static variables when I should've thought of it as a static algorithm that could handle diverse variables.
+// I also hadn't yet figured out that I could chain two functions on the same line, or separate lines with semicolons - but now I know lmao.
