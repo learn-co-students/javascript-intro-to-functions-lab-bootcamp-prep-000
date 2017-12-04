2118b2353ca97737ec6a3aee4d6ca44b3212f1a3
Joseph C <jciv.coding@gmail.com>
2017-12-04 01:07:47 +0000

    Done.

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ function sayHiToGrandma(string) {
 sayHiToGrandma("hello");
 sayHiToGrandma("HELLO");
 sayHiToGrandma("I love you, Grandma.");
+
+\\ After two hours, I realized that my approach, initially, was very rigid. I expected an algorithm/function to adapt & mold itself around a
+\\ set of static variables when I should've thought of it as a static algorithm that could handle diverse variables.
+\\ I also hadn't yet figured out that I could chain two functions on the same line, or separate lines with semicolons - but now I know lmao.
