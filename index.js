a2ccfa362b0f9408dbe973a55ec8c9b7f6be88fd
Kevin Nwoke <kevinnwoke@yahoo.com>
2017-12-08 01:11:48 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function whisper(uppercase){
 function logShout(lowercase){
   console.log(lowercase.toUpperCase())
 }
+
+function logWhisper(uppercase){
+  console.log(uppercase.toLowerCase())
+}
+
+function sayHiToGrandma(lowercase){
+  if (lowercase.toLowerCase === lowercase){
+    return "I can't hear you!"
+  }else if (uppercase.toLowerCase === uppercase) {
+    return "YES INDEED!"
+  }else if (lowercase === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
