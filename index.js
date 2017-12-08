5290834a4267df1e4e5aab85baf0a5c3200c8ec8
Kevin Nwoke <kevinnwoke@yahoo.com>
2017-12-08 00:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var uppercase = 'HELLO!'
+var lowercase = 'hello!'
+var mixedcase = 'Hi there!'
+
+function shout(lowercase) {
+  return lowercase.toUpperCase()
+}
+
 function shout(string) {
-  return string.toUpperCase()
+  return string.toLowerCase()
 }
