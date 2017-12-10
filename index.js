207a09abfb2225210fe33351709b4fb34ee5df4e
Charles Mainegra <cmainegra@gmail.com>
2017-12-10 22:40:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(shout) {
+  console.log() .toUpperCase()
+}
+function logWhisper(shout) {
+  console.log() .toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
+function sayHiToGrandma(string) {
+  console.log() .toLowerCase()
+}
