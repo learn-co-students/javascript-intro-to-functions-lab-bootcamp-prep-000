81ee461209df93dc8bb54e51f1ea2b4d8a3946b7
Humza Joseph <humza1born@gmail.com>
2017-12-11 10:19:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return "HELLO".toLowerCase()
+}
+function logShout(string) {
+console.log("HELLO");
+}
+function logWhisper(string) {
+  console.log("hello");
+}
+function sayHiToGrandma(string) {
+console.log("I can't hear you!").toLowerCase()
+}
+function sayHiToGrandma(string){"YES INDEED"=
+return string.toUpperCase();
+}
+function sayHiToGrandma('I love you, Grandma.'){
+return "I love you, too";
+}
