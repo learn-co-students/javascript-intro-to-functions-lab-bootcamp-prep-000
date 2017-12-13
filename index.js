0b14e7f02dc00b20b7abf75cc93f871495360837
Joshua Dror <jdror2@gmail.com>
2017-12-13 17:13:19 +0000

    Done.

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ function sayHiToGrandma(string) {
 }
 var uppercase = "HELLO!"
 
-uppercase.toUpperCase() === uppercase
+uppercase.toUpperCase() === uppercase // true
 
 var lowercase = 'hello!'
 
-lowercase.toLowerCase() === lowercase
+lowercase.toLowerCase() === lowercase // true
 
 var mixedCase = 'Hi there!'
 
-mixedCase.toLowerCase() === mixedCase
+mixedCase.toLowerCase() === mixedCase // false
 
-mixedCase.toUpperCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase // false
