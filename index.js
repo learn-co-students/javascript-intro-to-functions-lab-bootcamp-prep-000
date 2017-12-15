af4d0138aecf0c5c1796714d7be9473816546374
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-15 23:25:08 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function logWhisper(string) {
   console.log('hello'.toLowerCase())
   return string.toLowerCase()
 }
+function sayHiToGrandma(string) {
+if (string.toLowerCase() === string) {
+return "I can\'t hear you!"
+}
+if (string.toUpperCase() === string) {
+return "YES INDEED!"
+}
+if (string ==="I love you, Grandma.") {
+  return "I love you, too."
+}
+return "Are you eating enough?"
+}
