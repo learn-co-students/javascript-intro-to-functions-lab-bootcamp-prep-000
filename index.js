cddf2479ead1c10730eff959c38bc536a59e30e9
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-15 21:00:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+return string.toUpperCase()}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+  return string.toLowerCase()
+}
