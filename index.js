77132fcfc6557ed01dbe090c36dc4d06cd68b4a9
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-17 09:11:51 +0000

    Done.

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
     return "I can't hear you!";
-  }
-  if (string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } 
-  if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+  } else {
+    if (string === "I love you, Grandma.") {
+      return "I love you, too.";
+    }
   }
 }
 
