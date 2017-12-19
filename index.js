05bee10a7628684adc665678a08954b7be81b5bc
michael lynn <michael_lynn@comcast.net>
2017-12-19 17:42:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout() {
+  string.toLowerCase() === mixedCase
+  return string.toUpperCase()
+}
+function wisper(string) {
+  return string.toUpperCase()
+}
+function logShout(string) {
+  return string.toUpperCase()
+}
+function logWisper(string) {
+  return string.toUpperCase()
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
