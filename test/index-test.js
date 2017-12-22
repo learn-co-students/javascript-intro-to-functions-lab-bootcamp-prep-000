b6060170f571e9e703e1715fe0e39f67448220ec
Joseph Epifano <joepifano@gmail.com>
2017-12-22 17:47:57 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,16 +1,23 @@
-
+function shout(string) {
+  return string.toUpperCase()
+}
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
+function whisper(string) {
+  return string.toLowerCase()
+}
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
-
+fucntion logshout(string) {
+  console.log string.toUpperCase()
+}
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
