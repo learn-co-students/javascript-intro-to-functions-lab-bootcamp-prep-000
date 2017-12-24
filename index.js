b0080b37a245f4c042c3cda887118a22db403342
rob <888robey@gmail.com>
2017-12-24 14:38:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) { 
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log ('hello')
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+console.log ("I can\'t hear you!")
+return string.toUpperCase()
+console.log ('YES INDEED!')
+(string === 'I love you, Grandma.')
+console.log ('I love you, too.')
+}
