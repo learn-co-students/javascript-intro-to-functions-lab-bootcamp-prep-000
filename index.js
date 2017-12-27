586c554fe8a7455f4a0a01205aa9ee900194531e
Sean Martin <sean.martin@gmail.com>
2017-12-27 18:44:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
+
+logShout('hello');
+
+function logWhisper(string) {
+  console.log(string.toLowerCase);
+}
+
+logWhisper('HELLO');
