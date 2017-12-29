4f2d061b21743a96dc93cfd0c0a87e46faf6b36a
eddie <psyck_o@hotmail.com>
2017-12-29 06:56:15 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
     return "YES INDEED!";
-  if (string.toLowerCase() === string)
+  else if (string.toLowerCase() === string)
     return "I can\'t hear you!";
-  if ( string === "I love you, Grandma.")
+  else if ( string === "I love you, Grandma.")
     return "I love you, too.";
 }
