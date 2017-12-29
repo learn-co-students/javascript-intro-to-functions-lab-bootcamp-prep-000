8a0660e867419526322fefd838ca07b6937d6fa4
NinjaEagle <kcwang06@gmail.com>
2017-12-29 18:26:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma (string){
+{sayHiToGrandma(string)===lowercase
+  return string("I can't hear you!")
+else
+  return string("YES INDEED!")}
+if sayHiToGrandma(string)===uppercase
+  return string("I love you, Grandma!")
+}
