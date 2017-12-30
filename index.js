c392d04aa59c040019bb8d280bd5efa8b4407914
Troy Froman <90troyfroman@gmail.com>
2017-12-30 20:47:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase 
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase 
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase 
+ 
+mixedCase.toUpperCase() === mixedCase 
+
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === lowercase)
+  {
+  return `I can't hear you!`
+  }
+  else if (string === uppercase) 
+  {
+    return `YES INDEED!`
+  }
+  else (string === "I love you, Grandma.")
+  {
+    return "I love you, too."
+  }
+}
