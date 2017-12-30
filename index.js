c5f89189462c75376b02915a46b1e6099f819977
manassah Watkins <manassahwatkins@gmail.com>
2017-12-30 15:51:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.lowercase()
+}
+function logshout(string){
+  console.log()
+}
+function logwhisper(string){
+  console.log()
+}
+function sayHiToGrandma(string){
+  return ("I cant hear you!") 
+}
+function sayHiToGrandma(string){
+  return " YES INDEED" 
+}
+function sayHiToGrandma(string){
+  return "I love you too"
+}
