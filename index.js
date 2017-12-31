2af8487445dca049d449a9488bbcc5deb0c3acce
Hoang Phuc Nguyen <munhp93@gmail.com>
2017-12-31 17:34:40 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase()===string){
-  return "I can't hear you"
+  return "I can't hear you!"
   }
   if (string.toUpperCase()===string){
-    return "YES INDEED"
+    return "YES INDEED!"
   }
-  if (string==="I love you,Grandma"){
-    return "I love you, too"
+  if (string === "I love you,Grandma.") {
+    return "I love you, too."
   }
-  return "Are you eating enough?"
 }
