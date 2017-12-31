f64588a0cb1c36d80795d16fa7ead47d32f18803
Gurjot Singh <gurjotssidhu08@gmail.com>
2017-12-31 21:07:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logshout(String)
+{
+  Console.log('hello') .toUpperCasse() 
+}
+
