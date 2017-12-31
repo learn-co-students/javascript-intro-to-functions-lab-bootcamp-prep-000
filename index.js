240ab0eebe68c118f3027b9c7f2873125080080c
Gurjot Singh <gurjotssidhu08@gmail.com>
2017-12-31 22:31:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 function shout(string)
 {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string)
 {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
-function logshout(String)
+function logShout(string)
 {
-  Console.log() .toUpperCasse() 
+  console.log(string.toUpperCase());
 }
 
+function logWhisper(string)
+{
+    console.log(string.toLowerCase());
+  }
+  
+function sayHitoGrandma(string)
+{
+ if(string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+
+if(string.toUpperCase() === string) {
+   return "YES INDEED!";
+  }
+
+  if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
