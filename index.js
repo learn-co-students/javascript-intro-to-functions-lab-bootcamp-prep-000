222f0ace53d3f61c7f12846d3f03d186d135b53b
Gurjot Singh <gurjotssidhu08@gmail.com>
2017-12-31 22:36:32 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string)
     console.log(string.toLowerCase());
   }
   
-function sayHitoGrandma(string)
+function sayHiToGrandma (string)
 {
  if(string.toLowerCase() === string) {
     return "I can't hear you!";
