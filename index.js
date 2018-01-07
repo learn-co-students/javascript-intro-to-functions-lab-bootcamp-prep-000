3218ca82b94f36abdef29bf86f7687bc089ddfce
Brendan Capello <capello4@gmail.com>
2018-01-07 02:04:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(shout(string))
+}
+
+function logWhisper(string){
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string){
+  var lowercase = string
+  var uppercase = string
+  if(string =="I love you, Grandma."){
+    return "I love you, too."
+  }
+  else if(string == shout(string)){
+    return "YES INDEED!"
+  }
+  else if(string == whisper(string)){
+    return "I can't hear you!"
+  }
+}
