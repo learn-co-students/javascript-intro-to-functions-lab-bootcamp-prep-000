1e6868252ea41bf16a7d24b5820b548d812973dc
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 03:14:42 +0000

    Done.

--- index.js
+++ index.js
@@ -14,4 +14,25 @@ function logWhisper(string) {
   console.log('hello'.toLowerCase())
 }
 
+function sayHiToGrandma(string)
+{
+if (string.toLowerCase() === string)
+{
+  return"I can't hear you!"
 }
+else if (string.toUpperCase()===string)
+{
+ return "YES INDEED!"
+}
+else if (string==="I love you, Grandma.")
+{
+  return "I love you, too."
+}
+}
+
+
+  
+
+
+
+
