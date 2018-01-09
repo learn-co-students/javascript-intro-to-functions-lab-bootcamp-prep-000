5d5319f54a296083f62a542a79995e90f9912596
Alex Villano <alex.villano@yahoo.com>
2018-01-09 17:08:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase
+  var uppercase
+  var mixedCase
+  
+  lowercase.toLowerCase() === lowercase
+  uppercase.toUpperCase() === uppercase
+  mixedCase.toUpperCase() === mixedCase
+  mixedCase.toLowerCase() === mixedCase
+}
+
+sayHiToGrandma("HELLO")
+sayHiToGrandma("hello")
+sayHiToGrandma("I love you, grandma")
