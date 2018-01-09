adb9ac8f366edddc2c8450d073da24694f354897
getta <mulugetarobertson5@gmail.com>
2018-01-09 04:16:36 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -35,8 +35,8 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+describe('sayHiToGrandma(string'), function()  {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function () {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
