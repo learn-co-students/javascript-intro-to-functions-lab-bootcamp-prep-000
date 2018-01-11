1890d3b11803e6e598f210988adc3421ea9a13d3
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-11 18:31:33 +0000

    Done.

--- index.js
+++ index.js
@@ -14,16 +14,15 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
     return "I can't hear you!";
 }
-}
-function sayHiToGrandma(string) {
+
   if (string === string.toUpperCase() ){
     return "YES INDEED!"
   }
-}
-function sayHiToGrandma(string) {
+
   if (string === 'I love you, Grandma.'){
     return "I love you, too.";
   }
+  return "Are you eating enough?"
 }
 function whisper(string) {
   return string.toLowerCase();
