4a39757d6dc85a872469b69d6190abc8d2acc8cc
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-11 18:27:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+}
+}
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase() ){
+    return "YES INDEED!"
+  }
+}
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.'){
+    return "I love you, too.";
+  }
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
