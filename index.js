22aefd11923d47b738fc3434654e45ff32cde124
Wilson Mendez <wilsonmendez3@gmail.com>
2018-01-12 02:44:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase () === lowercase
+
+var mixedCase = 'Hi there!'
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log("I love you, Grandma")
+}
+function logWhisper(string) {
+  return console.log()
+}
+function sayHiToGrandma(string) {
+  var hi = 
+  return string.toUpperCase()
+}
