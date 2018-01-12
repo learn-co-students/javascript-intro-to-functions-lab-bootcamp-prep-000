2b99cef00c8099782a15e45f1c26a188ab9ebaea
Shauna Spencer <spencersl3@vcu.edu>
2018-01-12 20:17:13 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("string.toUpperCase")
+  console.log(shout(string))
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else {
+    return "Are you eating enough?"
+  }
 }
