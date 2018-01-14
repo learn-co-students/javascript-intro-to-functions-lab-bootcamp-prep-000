17fa4b9a01b40b7ec65edc960ca2b066667b3e32
James Ryan Nielcen <rnielcen@hotmail.com>
2018-01-14 08:24:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+   return string.toUpperCase()
+}
+  
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var uppercase = "HELLO!"
+
+upperCase.toUpperCase() === upperCase
+
+var lowercase = 'hello'
+
+lowercase.toLowerCase() ===lowercase
+
+var mixedCase = 'Hi there!'
+
+mixedCasse.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase 
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string)
+return "I can't hear you!"
+ else if(string.toUpperCase() === string)
+return "YES INDEED!"
+else if(string === "I love you, Grandma.")
+return "I love you, too."
+}
+
