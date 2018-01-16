08e5203b547c8473072bde80d60fdfddf5500a9a
Jordan Rumeld <jordanrumeld@me.com>
2018-01-16 21:50:42 +0000

    Done.

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-console.log("HELLO")
+  console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-console.log("hello")
+  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string===string.toLowerCase()) {
-    return "I can't hear you!"
-  } else if (string===string.toUpperCase()) {
-    return "YES INDEED!"
-  } else if (string==="I love you, Grandma.") {
-    return "I love you, too."
-  }
+if (string===string.toLowerCase()) {
+  return 'I can\'t hear you!'
+} else if (string===string.toUpperCase()) {
+  return 'YES INDEED!'
+} else if (string==="I love you, Grandma.") {
+  return 'I love you, too.'
+}
 }
