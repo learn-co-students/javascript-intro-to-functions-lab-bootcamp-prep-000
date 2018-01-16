33415c31454d4640747874d6e3b07a2857c7962c
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-16 14:28:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logshout(string) {
+  console.log
+  return string.toLowerCase()
+}
