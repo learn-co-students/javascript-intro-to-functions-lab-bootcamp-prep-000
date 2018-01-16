17ef0328781f5f6dcce14d35df983aa20350d621
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-16 05:23:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  var ucstring = string.toUpperCase()
+  console.log(`${ucstring}`)
+}
+function logWhisper(string){
+  var lcstring = string.toLowerCase()
+  console.log(`${lcstring}`)
+}
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
