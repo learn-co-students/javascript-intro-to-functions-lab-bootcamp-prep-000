6352c99e32a4635af34fe8a117725567a3c6dd97
Samson Wong <samson9993@gmail.com>
2018-01-17 20:10:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log('HELLO')}
+  
+function logWhisper(string){
+  console.log('hello')
+}
+
+
+var uppercase ="HELLO!"
+var lowercase = "hello!"
+
+function sayHiToGrandma(string){
+ if(uppercase.toUpperCase() === lowercase) {
+     console.log("I can't hear you!")
+ }else{
+    console.log("YES INDEED!")}}
+
+
+
+
+
+
