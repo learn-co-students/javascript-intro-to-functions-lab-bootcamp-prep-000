bc70294078f23744ce394025b0276fbbf1602c6a
Melissa Donahue <msue1282@gmail.com>
2018-01-17 01:39:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase();
+}
+shout("Hello!");
+
+/*describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})*/
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase();
+}
+sayHiToGrandma("Hello!");
