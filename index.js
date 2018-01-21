5823deb5d18b3a2e37c083113cceb9af5867c187
Joao Neves <j_manuelneves@hotmail.com>
2018-01-21 08:03:19 +0000

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function logWhisper(hello) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  if (string,toLowerCase() === string) {
-    return "I can\'t hear you"
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
   }
-if (string,toUpperCase() === string){
-  return "YES INDEED"
+if (string.toUpperCase() === string){
+  return "YES INDEED!"
 }
-if (string === "I love, you Grandma") {
+if (string === "I love you, Grandma.") {
   return "I love you, too."
 }
 }
