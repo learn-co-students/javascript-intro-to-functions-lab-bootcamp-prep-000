aaea5665ff39018dc9c826d40d3782d3d8f8b721
Joao Neves <j_manuelneves@hotmail.com>
2018-01-21 07:46:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(Hello) {
+  console.log("HELLO")
+}
+function logWhisper(hello) {
+  console.log("hello")
+}
+function sayHiToGrandma(hello) {
+  console.log ("I can\'t hear you!")
+}
+function sayHiToGrandma(HELLO) {
+  console.log ("YES INDEED!")
+}
+function sayHiToGrandma(IloveyouGrandma) {
+  return ("I love you, too.")
+}
