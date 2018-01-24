859d5f51714f0eec80efc55be3f3c3f4f303ffc1
Simon Stroh <simon_stroh@outlook.com>
2018-01-24 01:14:37 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
-    return "I can\'t here you!"
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string == "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
-
-sayHiToGradma('hello!')
