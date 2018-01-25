47c885b0d57306b1cb4d1f6d184523c4fcc0b648
Carol Barron <cbar102@yahoo.com>
2018-01-25 02:06:21 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var uppercase = string
+  var lowercase = string
+  
+  var phrase = 'I love you, Grandma.'
+  if (string === phrase) {
+   return 'I love you, too.';
+  }
+  if (lowercase === lowercase.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  if (uppercase === uppercase.toUpperCase()) {
+    return 'YES INDEED!';
+}
+
+}
