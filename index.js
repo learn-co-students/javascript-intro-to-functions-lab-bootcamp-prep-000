7f11ff3f7b3f414b41b3f6d66c0cfc306ea94a83
jaredianmills <jaredianmills@gmail.com>
2018-01-26 22:23:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var uppercase = "HELLO"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'I love you, Grandma.'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma (string) {
+  if (string === lowercase)
+  return ("I can't hear you!")
+  else if (string === uppercase)
+  return ("YES INDEED!")
+  else if (string === mixedCase)
+  return ("I love you, too.")
+}
