f8ad32f2b5109b74def87ac41ba310dec8110b7a
Anne Edmondson <edmondson.anne@gmail.com>
2018-01-29 05:03:49 +0000

    Done.

--- index.js
+++ index.js
@@ -14,5 +14,25 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  var lowercase = 'hello!'
+  var mixedCase = 'I love you, Grandma.'
+  
+  
+  if(uppercase.toUpperCase() === uppercase) {
+    console.log('YES INDEED!');
+  }
+  else if(lowercase.toLowerCase() === lowercase) {
+    console.log("I can't hear you!");
+  }
+  else if(mixedCase) {
+    console.log('I love you, too.')
+  }
+}
+
 logShout('hello');
 logWhisper('HELLO');
+
+sayHiToGrandma('hello!');
+
