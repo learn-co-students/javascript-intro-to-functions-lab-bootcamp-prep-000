cc62d8ff23ea3895cb18aa0b24c0ebba1548d6da
daniel-unger <danunger24@gmail.com>
2018-01-29 03:11:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+//# 3)
+/*  function whisper(string) {
+       return string.toLowerCase()
+    }*/
+    function whisper(string) {
+         //return string.toLowerCase()
+       console.log(string.toLowerCase());
+      }
+
+      function sayHiToGrandma(string) {
+   return string.toLowerCase()   console.log(string.toLowerCase());
+   }
