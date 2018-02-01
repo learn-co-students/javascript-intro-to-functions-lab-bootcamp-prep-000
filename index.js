568ecef1bf1095294a79d50c395c2781a896bd8b
Jonathan Williams <jtw365@live.missouristate.edu>
2018-02-01 03:48:14 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,26 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    var lowerCase = string.toLowerCase();
+    return ('I can\'t hear you!');
+  }
+
+  else if (string === string.toUpperCase()) {
+    var upperCase = string.toUpperCase();
+    return ('YES INDEED!');
+  }
+
+  else {
+    var mixedCase = 'I love you, too.';
+    return(mixedCase);
+  }
 }
