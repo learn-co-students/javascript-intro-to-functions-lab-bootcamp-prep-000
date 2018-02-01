681cdf5a5d04680afa4f5d1da33231383a369517
Carla Walter <walterc@gmx.com>
2018-02-01 06:23:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function shout(string){return string.toUpperCase()}
+function whisper(string){return string.toLowerCase()}
+function logShout(string){console.log((string).toUpperCase())}
+function logWhisper(string){console.log((string).toLowerCase())}
+function sayHiToGrandma(string){
+  if ((string) === "HELLO") {return "YES INDEED!"}
+  if ((string) === "hello") {return "I can't hear you!"}
+  else {return "I love you, too."}}
