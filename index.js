a2d62801bcb5a4f6720ac398032dd063e2a0135f
jennieboyasun@gmail.com <jennieboyasun@gmail.com>
2018-02-07 18:22:11 +0000

    Done.

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  return string.toLowerCase()
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
 }
