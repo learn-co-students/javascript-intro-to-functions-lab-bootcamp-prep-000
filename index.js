92020b76eef0beeae2894ce6b1b871205e7067a9
Mike McLean <mcleanmikejames@gmail.com>
2018-02-09 19:38:33 +0000

    Done.

--- index.js
+++ index.js
@@ -22,5 +22,12 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  return "I love you, Grandma"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.")
+    return "I love you, too.";
 }
