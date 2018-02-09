cef079ffa390674749decb7f093e0a69511b2a17
Bertha Powell <mopowellb@gmail.com>
2018-02-09 19:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ function sayHiToGrandma(string){
   console.log("Hi")
  // ${Grandma, "I can't hear you!"}
 }
-
 function logShout(string){
   console.log(shout(string))
 }
 function logWhisper(string){
   console.log(whisper(string))
 }
-function sayHiToGrandma(string){
-  console.log("Hi")
- // ${Grandma, "I can't hear you!"}
+function sayHiToGrandma(string) {
+ if (string==="I love you, Grandma.") {
+  return "I love you, too." 
+ }
+
+
+
+
+
+
+
+
+
 
 
   
