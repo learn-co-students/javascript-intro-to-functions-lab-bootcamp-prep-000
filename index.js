c7088299ec10c9adbc2cb5521d2444f8dc5d07da
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-11 22:52:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+  
+}
+
+function logShout(HELLO) {
+  console.log("HELLO")
+  
+}
+
+function logWhisper(hello) {
+  console.log("hello")
+
+}
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+  return "I can't hear you!" ;
+  } 
+  
+  if(string === string.toUpperCase()) {
+  return "YES INDEED!" 
+  } 
+  
+  if(string === mixedCase) {
+  return "I love you, too."
+  } 
+}
