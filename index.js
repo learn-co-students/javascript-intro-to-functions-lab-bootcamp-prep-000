1a448d4e0ccdbcdbb48e1b125c693d7091574cd2
Gerrick King <gerrickkingfitness@gmail.com>
2018-02-11 15:18:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase() // 'HELLO!'
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase() // 'hello'
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
