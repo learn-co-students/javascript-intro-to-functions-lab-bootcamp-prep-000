0b68b9ddd90ad6b553ead28f3d4b161b8b97e858
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-02-13 21:18:53 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ function logWhisper(string)
 function sayHiToGrandma(string)  { 
   if (string.toLowerCase()===string){
     return  "I can't hear you!"
-  }
-}
-
-function sayHiToGrandma(string)  {
-  if (string.toUpperCase()===string){
+  }else if (string.toUpperCase()===string){
     return "YES INDEED!"
+  }else if (string==="I love you, Grandma."){
+    return "I love you, too."
   }
-}
+} 
+
+
+  
+
