775c7d18a5146e87842e87f1b249eb17680f47c4
Sabine Emden <emdensabine@gmail.com>
2018-02-14 02:06:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+//receives one argument and returns it in all caps
+function shout(string) {
+  return string.toUpperCase();
+}
+
+//receives one argument and returns it in all lowercase
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+//calls console.log() its one argument in all caps
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+//calls console.log() its one argument in all lowercase
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+//returns "I can't hear you!" if `string` is lowercase
+//returns "YES INDEED!" if `string` is toUpperCase
+//returns "I love you, too." if `string` is "I love you, Grandma."
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
