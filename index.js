a9c4d0daf70877abb7391919ed8dc7c99d71e68d
Valerie Hudson <vhudsonphoto@gmail.com>
2018-02-15 17:07:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) { console.log('HELLO')}
+function logWhisper(string) {console.log('hello')}
+function sayHiToGrandma(string) { if (string === string.toLowerCase()) { return "I can't hear you!"; } else if (string === string.toUpperCase()) { return "YES INDEED!"; } else{ return "I love you, too.";
+}
+}
