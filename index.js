79339c781aab5a7fc643d3fb1f12ade8a963ccee
Cherlyse Alexander-Reid <cherlyseaar@gmail.com>
2018-02-18 19:42:14 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase{
-      return "I can't hear you"    
+  if (string.toLowerCase() === string)
+  {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string)
+  {
+    return "YES INDEED!"
+  }
+  else (string === "I love you, Grandma")
+  {
+    return "I love you, too."
   }
 }
