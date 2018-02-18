49ec10219746d2a18e0391f73bd47940b41c33db
Peter Ayres <peter.ayresmusic@gmail.com>
2018-02-18 17:12:09 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,7 +7,7 @@ describe('shout(string)', function() {
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
+    expect(whisper('HELLO')).toEqual('hello');('YES INDEED!')
   })
 })
 
