8b5a4106dc94a33bf26419d321adfa8b5010ed01
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-02-22 00:00:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+  
+  var upperCase = string.toUpperCase();
+  var lowerCase = string.toLowerCase();
+  
+  if (string === upperCase) {
+    return "YES INDEED!";
+  } else {
+    return "I can't hear you!";
+  }
+  
+  
+}
