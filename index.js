157a3a8300061930f1218390c0d031797ba3d8af
Sarah Currey <curreysarah28@gmail.com>
2018-02-25 16:13:54 +0000

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ return string.toUpperCase()
 function whisper(string) {
 return string.toLowerCase()
 }
-function sayHelloToGrandma() {
-  console.log("Hello, Grandma!")
+
+function sayHiToGrandma(string){
+  return "I can't hear you!"
 }
-function say(Hi, Grandma) {
-  return `${greeting}, ${firstName}!`
-  console.log('I can't hear you!"")
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === string) {
+  return "I can't hear you!"
+} else if (string.toUpperCase() === string)
+return "YES INDEED!" }
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you too."
+  } else if (string === )
 }
+
+function logShout(string){
+  console.log(string.toUpperCase())
+  }
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+  
+  
