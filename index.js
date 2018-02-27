cdc204a60eff4f8338e24b6e9bed51962c5a6b16
Lynn Ballard <lynn@pingsplash.com>
2018-02-27 21:21:39 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 function sayHiToGrandma(string){
 
   if (string.toLowerCase() === string)
