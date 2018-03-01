b5cfd8734ec06807f28628f0d0e39d11aa279987
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-01 02:30:47 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,8 +8,8 @@ describe('shout(string)', function() {
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
-  })
-})
+  });
+});
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
