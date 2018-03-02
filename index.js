16ddbbd3f3924edfd77b8285f1f726ff1e8109fd
Jian Ting Li <jianting2103@yahoo.com>
2018-03-02 00:17:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+console.log(shout("hello") === "HELLO");
+
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+console.log(whisper("HELLO") === "hello");
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return ("I can't hear you!");
+  } else if (string === string.toUpperCase()) {
+    return ('YES INDEED!');
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.");
+  }
+}
+
+
