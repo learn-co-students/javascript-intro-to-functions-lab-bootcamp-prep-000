171dcb985f40107d9d5d0d2ec9171f7b23b7a40b
Edward Lu <eddd.lu@gmail.com>
2018-03-02 01:13:30 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
-    return ("I can't hear you!")
+  if (string === "I love you, Grandma.") {
+    return ('I love you, too.')
   }
-  else if (string === uppercase) {
-    return ("YES INDEED!")
+  else if (whisper(string) === string) {
+    return ('I can\'t hear you!')
   }
-  else if (string === "I love you, Grandma.") {
-    return ("I love you, too.")
+  else if (shout(string) === string) {
+    return ('YES INDEED!')
   }
 }
