a97bab3be7b95105e51f3f17ac1b75d370373e34
David Haiman <dhaiman2@gmail.com>
2018-03-04 21:43:57 +0000

    Done.

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string === string.toUpperCase()) {
+  } 
+  if (string === string.toUpperCase()) {
   return "YES INDEED!"
-  } else {
+  }
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
