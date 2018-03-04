6149122f003fb8a5a6ad306ea933f2f4b6a32aff
Sean Febo <seanfebo@techie.com>
2018-03-04 09:51:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(STRING) {
+  return STRING.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(STRING){
+  console.log(STRING.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"}
+  else { if (string === "I love you, Grandma.") {
+    return "I love you, too."} }
+    return "I can\'t hear you!" }
