33c396ca0c2ef87ec2b42c18e97d48eaf011f304
David Teitelbaum <david.teitelbaum@wework.com>
2018-03-06 19:25:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
-function logShout(string) {console.log(string) .toUpperCase()}
+function logShout(string) {console.log(string) string.toUpperCase()}
