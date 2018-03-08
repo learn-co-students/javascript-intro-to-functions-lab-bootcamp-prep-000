16874803b1771caf603cb481ee1a72ce90453046
Joanne Lee <vernapii@gmail.com>
2018-03-08 20:07:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logWhisper(string) {
+  return console.log.string.toLowerCase 
+}
+
+}
