55177a230f2260dd1b7fc767e2ee21eaad0c48fc
David Teitelbaum <david.teitelbaum@wework.com>
2018-03-12 12:08:45 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase {
+  if (string.toLowerCase() === string) {
     return ("I can't hear you!")
   }
 
-  else if (string === uppercase {
+  else if (string.toUpperCase() === string) {
     return ("YES INDEED!")
   }
 
