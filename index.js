61c2c4f5717ec447ab0bb2232332d2bf5108ab19
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-03-12 00:33:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = string
+  var lowercase = string
+  var mixedCase = string 
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else { 
+    return "YES INDEED!"
+  }
+}
