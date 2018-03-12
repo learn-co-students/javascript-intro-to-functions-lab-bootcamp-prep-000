e7651e38e2481683d93f89149a7b92209b8b67c0
David Teitelbaum <david.teitelbaum@wework.com>
2018-03-12 12:01:06 +0000

    Done.

--- index.js
+++ index.js
@@ -15,17 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+  if (string === lowercase {
+    return ("I can't hear you!")
   }
 
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+  else if (string === uppercase {
+    return ("YES INDEED!")
   }
 
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
+  else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
   }
-
-  return "Are you eating enough?"
-}
+  }
