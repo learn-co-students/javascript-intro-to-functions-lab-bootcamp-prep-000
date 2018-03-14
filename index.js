d114937fc63adc26cff8ce0755b1fa999cc602b2
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2018-03-14 17:17:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+const shout = (str) => str.toUpperCase();
+
+const whisper = (str) => str.toLowerCase();
+
+const logShout = (str) => console.log( str.toUpperCase() );
+
+const logWhisper = (str) => console.log( str.toLowerCase() );
+
+const sayHiToGrandma = (str) => {
+  if ( str === str.toLowerCase() ) {
+    return "I can't hear you!";
+  } else if ( str === str.toUpperCase() ) {
+    return "YES INDEED!";
+  } else if (str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
