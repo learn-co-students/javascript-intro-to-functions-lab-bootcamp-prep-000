16fa5c4b0056374eaa22db0e135744623bcd3080
acherone92@gmail.com <acherone92@gmail.com>
2018-03-17 22:49:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 function shout(string){
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string){
-  
-  console.log(string.toUpperCase)
+    console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase){
-    return "I can't hear you!"
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
     }
+  else if (string === string.toUpperCase()){
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
   }
