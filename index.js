55bfaabc0189343a7892dec12c1675f3b90acf70
Hollie Lambert <hollielambert@protonmail.com>
2018-03-18 02:21:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
