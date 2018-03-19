2901650776011dce2ffac7acc2a1551d2f580b98
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-19 14:30:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  console.log('hello');
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear that!")
+}
+
+function sayHiToGrandm(string) {
+  return string.toUpperCase()
+}
