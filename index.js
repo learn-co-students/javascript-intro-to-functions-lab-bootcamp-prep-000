71ccd7d4c4b711c6a9f1d86a13828464cf4a6b63
Christopher <christopherccm@msn.com>
2018-03-19 00:33:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('HELLO')
+}
+
+function logWhisper() {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  return `hello`.toLowerCase
+}
