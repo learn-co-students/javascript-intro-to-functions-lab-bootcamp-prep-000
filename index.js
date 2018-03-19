0e3f8bc6025ecf004375b07ac147a21df7d123f4
Christiana Vandermale <christianavdm@gmail.com>
2018-03-19 15:20:39 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ function shout (string) {
 function whisper (string) {
   return string.toLowerCase()
 }
+function logShout (string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma (string) {
+  var alteredString = string
+  if (alteredString.toLowerCase() === string) { 
+    return `I can't hear you!`
+  } else if (alteredString.toUpperCase() === string) { 
+    return `YES INDEED!`
+  } else if (alteredString === ("I love you, Grandma.")) { 
+    return `I love you, too.`
+  }
+}
