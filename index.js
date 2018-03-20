e7aeea342f43233ecedd2560d4921e151a1858b0
Karl Amoguis <ktamoguis@gmail.com>
2018-03-20 07:14:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
