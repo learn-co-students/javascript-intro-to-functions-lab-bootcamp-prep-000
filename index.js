fd2b857ee4bb80e73f31c541d73714c6f7b7c5d2
Erin <erin@lrizal.com>
2018-03-21 14:59:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function shout(string) {
+    return string.toUpperCase()
+  }
+function whisper(string) {
+
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+ console.log('I love you, Grandma.')
+}
