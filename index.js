b5bb7a10c52b33ae28a90e6082beeb102ae86367
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-03-23 01:25:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+//1
+function shout(string) {
+    return string.toUpperCase()
+}
+
+//2
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+//3
+function logShout(string) {
+  console.log(string.toUpperCase())  
+}
+
+//4
+function logWhisper(string) {
+  console.log(string.toLowerCase())  
+}
+
+function sayHiToGrandma(string) {
+
+if (string.toLowerCase() === string) {
+  return ("I can't hear you!")
+}
+
+else if (string.toUpperCase() === string) {
+  return ("YES INDEED!")
+}
+
+else if (string === "I love you, Grandma.") {
+  return ("I love you, too.")
+
+}}
