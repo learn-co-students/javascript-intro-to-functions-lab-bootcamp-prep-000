20e7ee0aa0ca54da9742ffe7f6eda3a1a8e93839
Charlton Doucette <charlesdonline@gmail.com>
2018-03-27 13:38:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function whisper(string) {
+    return string.toLowerCase()
+  }
+  function logShout(string) {
+    console.log('HELLO') ;string.toUpperCase()
+  }
+  function logWhisper(string) {
+    console.log('hello') ;string.toLowerCase()
+  }
+  function sayHiToGrandma(string) {
+    return("I can't hear you!") ;string.toUpperCase()
+  }
+  function sayHiToGrandma(string) {
+    return("I can't hear you!") ;string("YES INDEED!")
+  }
+  function YESINDEED(string) {
+    return string.toUppercase()
+  }
+  function shout(string) {
+    return string.toUppercase()
+  }
