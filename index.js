971346491a4aa5ca41102d3ee66821ffdda4b207
Estevan Bellino <bellino.estevan@gmail.com>
2018-03-27 23:23:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string){
+  return string.toUpperCase();
+}
+function whisper(string){
+  return string.toLowerCase();
+}
+
+if (string===toLowerCase){
+  console.log("I can't hear you!");
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+} else if ('I love you, Grandma.') {
+  return 'I love you, too.'
+}
+
+
