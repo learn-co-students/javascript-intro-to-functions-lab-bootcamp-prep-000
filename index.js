e2d5670d3af0162103989a292d7e20a42884a55f
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 18:11:21 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string===string.toUpperCase()){
-  return 'YES INDEED!';
+  return 'YES INDEED!'
   }
   else if (string===string.toLowerCase()) {
   return 'I can\'t hear you!'
