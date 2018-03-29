23d239f5658c2dd9b85d73245e8641bf386747ef
Austin Curry <austin.curry360@gmail.com>
2018-03-29 15:14:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  console.log('I can\'t hear you!')
+}
+function sayHiToGrandma(string) {
+  console.log('YES INDEED!')
+}
