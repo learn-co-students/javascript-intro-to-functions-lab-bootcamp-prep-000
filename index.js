97e1ba34e61605c9551f6a9d94cd32c77e4bf921
William Roberts <willbarronroberts@gmail.com>
2018-04-01 01:21:55 +0000

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
-whisper('hello!');
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  let tooQuiet = string.toLowerCase();
+  let loud = string.toUpperCase();
+  let love = "I love you, too.";
+  
+  if (string === tooQuiet) {
+    return "I can't hear you!";
+  } else if (string === loud) {
+    return "YES INDEED!";
+    } else if (string === "I love you, Grandma.") {
+    return love;
+  }
+}
