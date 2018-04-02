acde54710c3879aae3f68eb538261dadfb74f43e
Ekaterina Rodkina <katrinrodkina@gmail.com>
2018-04-02 15:48:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function shout(string) {
    return string.toUpperCase()
-<<<<<<< HEAD
+
 }
 
 function whisper(string) {
@@ -16,18 +16,19 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-   if (string.toLowerCase() === string) {
-      console.log("I can't hear you!")
-   }
-   else if (string.toUpperCase() === string) {
-     console.log('YES INDEED!')
-   }
-   else if (string === 'I love you, Grandma.') {
-     console.log('I love you, too.')
-   }
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
 
 
-=======
-}
->>>>>>> c81c597917751959bd2b801f3a86f1c6a7d31617
