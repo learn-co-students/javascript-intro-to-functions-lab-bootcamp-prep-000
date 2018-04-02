a619d819fbf615e5a7decb6c3bb10623d486f137
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 19:37:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function whisper(String) {
+  return String.toLowerCase()
+}
+function shout(string) {
+  return string.toUpperCase()
+}
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+function sayHiToGrandma(string){
+return string.toLowerCase("I can't hear you!")
+}
