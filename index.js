3abbb096c034ea0ce6566dc31ec58820cbdffcf1
Charlton Doucette <charlesdonline@gmail.com>
2018-04-03 18:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
 function whisper(string) {
-    return string.toLowerCase()
-  }
-  function logShout(string) {
-    console.log('HELLO') ;string.toUpperCase()
-  }
-  function logWhisper(string) {
-    console.log('hello') ;string.toLowerCase()
-  }
-  function sayHiToGrandma(string) {
-    return("I can't hear you!") ;string.toUpperCase()
-  }
-  function sayHiToGrandma(string) {
-    return("I can't hear you!") ;string("YES INDEED!")
-  }
-  function YESINDEED(string) {
-    return string.toUppercase()
-  }
-  function shout(string) {
-    return string.toUppercase()
-  }
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can\'t hear you!";
+ } else if(string.toUpperCase() === string) {
+  return "YES INDEED!";
+ } else if (string === "I love you, Grandma.") {
+   return "I love you, too.";
+ }
+}
