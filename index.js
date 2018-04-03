18613c4f2ff60f8d854fa89c1f6570df70872000
Rebecca Porter <ra.porter19@gmail.com>
2018-04-03 14:11:45 +0000

    Done.

--- index.js
+++ index.js
@@ -15,21 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "hello!") {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
+}
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
-
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase
