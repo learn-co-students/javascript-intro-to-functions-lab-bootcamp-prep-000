efc8c736020fab60c540a84d66bbf370279d8c01
Vicki Hamm <vicki.cello@gmail.com>
2018-04-05 07:47:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function
-shout(string) {
-}
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -11,8 +7,25 @@ whisper(string) {
   return string.toLowerCase()
 }
 
-var string = "YES INDEED!"
-function logShout() {
-  return string.toUpperCase()
+function 
+logShout(string) {
+  console.log('HELLO')
+}
+
+function 
+logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+if (string === string.toLowerCase()){
+return ("I can't hear you!")
+}
+else if (string===string.toUpperCase()) {
+return ("YES INDEED!")
+}
+else {
+return ("I love you, too.")
+}
 }
 
