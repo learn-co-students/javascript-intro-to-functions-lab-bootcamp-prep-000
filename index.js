c7d3ed2215a6a4b7d123086319beef2c48847557
Vicki Hamm <vicki.cello@gmail.com>
2018-04-05 02:49:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function
+shout(string) {
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function
+whisper(string) {
+  return string.toLowerCase()
+}
+
+var string = "YES INDEED!"
+function logShout() {
+  return string.toUpperCase()
+}
+
