0f060bac5e66274563f1cfa2d2f047ea4ed571fd
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-05 22:46:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(sayHiToGrandma) {
+  return sayHiToGrandma.toUpperCase()
+}
+function whisper(sayHiToGrandma) {
+  return sayHiToGrandma.toLowerCase()
+}
+function sayHiToGrandma() {
+  console.log("Hi, Grandma!")
+}
