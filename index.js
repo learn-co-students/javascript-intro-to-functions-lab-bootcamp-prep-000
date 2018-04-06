bf7fe4327623356d00f833c047f2f5a867ee0178
Kyran McCann <kyran.mccann@gmail.com>
2018-04-06 06:22:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function shout(string){
-  return string .toUpperCase()
+  return string.toUpperCase()
 }
 
 function whisper(string){
-  return string .toLowerCase()
+  return string.toLowerCase()
 }
 
 function logShout(string){
-    console.log(string .toUpperCase())
+    console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-    console.log(string .toLowerCase())
+    console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
