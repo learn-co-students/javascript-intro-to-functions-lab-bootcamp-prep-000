5fc5d56a5af50c6ce81b6432f5d863723d9155d8
Micah Ash <micahash97@gmail.com>
2018-04-06 17:16:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  var lowercase = "I can't hear you!"
+  lowercase.toLowerCase() === lowercase
+}
+function sayHiToGrandma(string){
+  var uppercase = "YES INDEED!"
+  .toUpperCase() 
+}
+function sayHiToGrandma(string){
+  
+}
