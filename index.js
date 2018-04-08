19d0d27fe31be09b9264ab777d36415ddd1db94a
Marcia Johnson <marcia.johnson87@gmail.com>
2018-04-08 06:54:44 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(shout(string))
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  if(string === whisper(string)) 
+    return "I can't hear you!"
+  else if(string === shout(string))
+    return "YES INDEED!"
+  else if(string === "I love you, Grandma.")
+    return "I love you, too."
+}
