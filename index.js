de6b9bb62b3cba26c3d90441be36f8d482597d5a
Jessica Green <jgreen629@yahoo.com>
2018-04-09 10:15:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+/* describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
+*/
+
+function shout(string) {
+    return 
+  'hello'.toUpperCase()
+
+
+{ var upperCase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase }
+
+}
+
+
+
+
+
+function whisper(string) {
+  return
+'HELLO'.toLowerCase()
+
+
+
+
+{ var lowercase = "hello!"
+
+lowercase.toLowerCase() === lowercase }
+
+}
+
+
+
+
+
+
