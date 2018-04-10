a27fc00d4281897d3b6581bec9ee9e758f7ed153
Talya D <talyadouek@gmail.com>
2018-04-10 15:58:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase() ;
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase() ;
 }
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase()) ;
 }
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase()) ;
 }
-function sayHiToGrandma(string) { 
-    if (lowercase.toLowerCase() === lowercase) {
-    return "I can\t hear you!"}
-      }
-      else if (uppercase.toUpperCase()===uppercase) {
-        return "YES INDEED!" } 
-        else "I love you too" ; {
-       }   
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === string) {
+    return "I can't hear you!" ; }
+   else if (string.toUpperCase() === string) {
+        return "YES INDEED!" ; }
+      else { 
+       return "I love you, too." ; 
+     }
+}
