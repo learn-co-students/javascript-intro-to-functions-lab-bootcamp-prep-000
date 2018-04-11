8030967d4e9e4fe8bf24df323494f8a97140f425
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-11 19:45:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function shout(sayHiToGrandma) {
   return sayHiToGrandma.toUpperCase()
 }
+function logShout(sayHiToGrandma) {
+  var upper = sayHiToGrandma.toUpperCase()
+  console.log(upper)
+}
 function whisper(sayHiToGrandma) {
   return sayHiToGrandma.toLowerCase()
 }
+function logWhisper(sayHiToGrandma) {
+  var lower = sayHiToGrandma.toLowerCase()
+  console.log(lower)
+}
 function sayHiToGrandma() {
   console.log("Hi, Grandma!")
+}
+
+function sayHiToGrandma(sayHiToGrandma) {
+  if (sayHiToGrandma === sayHiToGrandma.toLowerCase())
+  return "I can't hear you!"
+  console.log("I can't hear you!")
+if (sayHiToGrandma === sayHiToGrandma.toUpperCase())
+return "YES INDEED!"
+console.log("YES INDEED!")
+ if (sayHiToGrandma === "I love you, Grandma.")
+ return "I love you, too."
+ console.log("I love you,too.")
 }
