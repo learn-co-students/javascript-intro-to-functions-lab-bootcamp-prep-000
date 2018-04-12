cacaecceceb6c88f8e4d8fb168e1b5d477a379e6
Colin Brence <cbrence@ramapo.edu>
2018-04-12 22:10:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+ return(string.toLowerCase())
+}
+
+function sayHiToGrandma() {
+  return("YES INDEED") if (string uppercase === uppercase
