c243f9592f277b9325bc1c9e42e218f4aed08558
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-13 23:31:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase(
+)
+}
+
+function whisper(string) {
+  return string.toLowerCase(
+)
+}
+
+function logShout(string) {
+  console.log("HELLO")
+  }
+logShout(string)
+
+function logWhisper(string) {
+  console.log("hello")
+}
+logWhisper(string)
+
+
+var lowercase = "string"
+function sayHitoGrandma('hello')
+  string.toLowerCase() === lowercase
+ {console.log("I can't hear you!")
+  }
+sayHitoGrandma()
+
+
+
+
+
+
