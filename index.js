010b01d93b8f93f6bb64eb311c4d9c5b59fc8a9a
Dana Palmer <roiai4@hotmail.com>
2018-04-22 07:38:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout() {
+  console.log('HELLO')
+}
+function logWhisper(){
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  console.log(string.toLowerCase)
+  return "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+  console.log(string.toUpperCase)
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string) {
+  console.log("I love you, Grandma.")
+  return 'I love you, too.'
+}
