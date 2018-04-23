56fd63f724c9255871e23d9e12d9746b3ed3dac7
Dana Palmer <roiai4@hotmail.com>
2018-04-23 05:06:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function shout(string) {
+function shout (string) {
   return string.toUpperCase()
 }
-function whisper(string) {
+function whisper (string) {
   return string.toLowerCase()
 }
-function logShout() {
+function logShout () {
   console.log('HELLO')
 }
-function logWhisper(){
+function logWhisper (){
   console.log('hello')
 }
-function sayHiToGrandma(string) {
-  console.log(string.toLowerCase)
+function sayHiToGrandma () {
+  console.log(`${string}.toLowerCase)
   return "I can't hear you!"
 }
-function sayHiToGrandma(string) {
-  console.log(string.toUpperCase)
+function sayHiToGrandma (string) {
+  console.log(`${string}.toUpperCase`)
   return "YES INDEED!"
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma (string) {
   console.log("I love you, Grandma.")
-  return 'I love you, too.'
+  return "I love you, too."
 }
