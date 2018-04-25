319f470b991e35e3dedb115435b0bab031a0113b
stefan.vicentijevic@gmail.com <stefan.vicentijevic@gmail.com>
2018-04-25 21:22:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+
+function logShout(string) {
+  console.log ("HELLO")
+}
+
+
+function logWhisper(string) {
+  console.log ('hello')
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  hello.toLowerCase() === "I can\'t hear you!"
+
+  var uppercase = 'HELLO'
+  HELLO.toUpperCase() === "YES, INDEED!"
+
+  var mixedCase = "I love you, Grandma."
+  I love you, Grandma.toLowerCase() === "I love you, too."
+  I love you, Grandma.toUpperCase() === "I love you, too."
+}
