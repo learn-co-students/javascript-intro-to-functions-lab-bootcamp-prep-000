c057c32fb3563552015678d3e2c42516a798b23b
maduri.vassaramo@hotmail.co.uk <maduri.vassaramo@hotmail.co.uk>
2018-04-26 17:46:41 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string)===toLowerCase
-  {
-    console.log("I can\'t hear you!")
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
+  
+}
+
+
+
