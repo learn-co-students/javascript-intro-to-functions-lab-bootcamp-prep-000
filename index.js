6020b5efa353309994537b18f6688f1f17e066da
Adrian Samarripas <asamarripas@gmail.com>
2018-04-26 05:34:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+var lowercase = 'hello'
+var uppercase = 'HELLO'
+
+function sayHiToGrandma(string){
+  if (string === lowercase){
+    return "I can't hear you!"
+    } 
+    
+  if (string === uppercase){
+    return "YES INDEED!"
+  } 
+    
+  if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
