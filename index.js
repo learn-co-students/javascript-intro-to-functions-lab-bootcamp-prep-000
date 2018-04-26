9de37a415dc5781161889dc0d2710f90c835061f
maduri.vassaramo@hotmail.co.uk <maduri.vassaramo@hotmail.co.uk>
2018-04-26 15:28:19 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 function logShout(string) {
-  console.log(`${string}`).toUpperCase
+  console.log(string.toUpperCase())
 }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string)===toLowerCase
+  {
+    console.log("I can\'t hear you!")
+  }
