2a07cf1c64547e15767669e83e251e8fa3a3c0c7
taryn yaeger <tsyaeger@gmail.com>
2018-04-30 15:04:39 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()){
-    return console.log("I can\'t hear you!")
+  if (string === string.toLowerCase()) {
+    console.log("I can\'t hear you!")
+  } 
+  else if (string === string.toUpperCase()) {
+    console.log("YES INDEED!")
+  }
+  else if (string === "I love you, Grandma") {
+    console.log("I love you, too.")
   }
 }
