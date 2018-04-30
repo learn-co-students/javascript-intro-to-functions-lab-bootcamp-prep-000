3bf3d91e861c80b5ebcc1cbcc95a45de0da9a81f
taryn yaeger <tsyaeger@gmail.com>
2018-04-30 15:12:28 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    console.log("I can\'t hear you!")
+    return console.log("I can\'t hear you!")
   } 
   else if (string === string.toUpperCase()) {
-    console.log("YES INDEED!")
+    return console.log("YES INDEED!")
   }
-  else if (string === "I love you, Grandma") {
-    console.log("I love you, too.")
+  else if (string === "I love you, Grandma.") {
+    return console.log("I love you, too.")
   }
 }
+
+sayHiToGrandma("HI")
+sayHiToGrandma("hi")
+sayHiToGrandma("I love you, Grandma.")
