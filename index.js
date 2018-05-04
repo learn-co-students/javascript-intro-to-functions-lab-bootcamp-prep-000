6035375db35ca51fe384467afb82f1a9c769bec9
Alex S <alex.sunga@fliptech.co>
2018-05-04 09:03:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("HELLO")
+}
+
+function logWhisper(string) {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string) {
+    var uppercase = "HELLO!"
+     
+    uppercase.toUpperCase() === uppercase // true
+     
+    var lowercase = 'hello!'
+     
+    lowercase.toLowerCase() === lowercase // true
+   
+     
+    var mixedCase = 'Hi there!'
+     
+    mixedCase.toLowerCase() === mixedCase // false
+     
+    mixedCase.toUpperCase() === mixedCase // false
+    
+    if (lowercase === 'hello!'){
+      return 'I can\'t hear you!'
+    } 
+   
+}
