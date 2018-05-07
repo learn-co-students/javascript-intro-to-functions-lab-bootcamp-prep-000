2aaaa4b6c365ab99206440630e18592f01401327
Dyasanti V <506thinfantry@gmail.com>
2018-05-07 09:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  if (string.toUpperCase() === string)
+  }
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  if (string === "I love you, Grandma.")
+  }
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
+  }
 } 
