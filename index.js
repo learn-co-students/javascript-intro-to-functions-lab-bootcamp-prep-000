d2b76d0fec43e1953bd0adb1751385c09d83d11b
Brad McCollum <btmcco8@gmail.com>
2018-05-08 04:34:34 +0000

    Done.

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  if (string.toUpperCase) === string) {
-    return "YES, INDEED!"
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
   }
   if (string === "I love you, Grandma.") {
     return "I love you, too."
