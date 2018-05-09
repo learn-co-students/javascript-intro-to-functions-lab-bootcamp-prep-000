200566acb856f3d3b0b0f4232d4849319bb226ef
Martin <martinvelarde21@gmail.com>
2018-05-09 05:07:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase ('Hello!')
+}
+var uppercase = "HELLO!"
+uppercase.toUpperCase()===uppercase
+
+var lowercasse = 'hello!'=== lowercase
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase()=== mixedCase
+
+mixedCase.toUpperCase()=== mixedCase
