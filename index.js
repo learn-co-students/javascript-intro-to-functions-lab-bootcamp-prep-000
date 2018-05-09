b00c4001137ca006cdfc39219d54b1056521509d
Kelly Lwakatare <devkells00@gmail.com>
2018-05-09 04:22:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO")
+}
+function logWhisper(string) {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  if lowercase "I can't hear you!"
+  if uppercase "YES INDEED!"
+  else ("I love you, too.")
+}
