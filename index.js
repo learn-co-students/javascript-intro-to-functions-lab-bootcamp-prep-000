ada7049736026c673b101c66ce0dd47328003d18
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-13 07:31:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string.toLowerCase){
+  string === `string`;
+  return 'I can\'t hear you!'}
+
+function sayHiToGrandma(string){
+  string.toUpperCase() === `string`;
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string){
+  "I love you, Grandma" === `string`
+  return "I love you, too."
+}
