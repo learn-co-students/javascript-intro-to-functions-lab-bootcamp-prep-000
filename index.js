0e036858ba88297017cec6fa74ec027673120830
Alex Bacic <fasbrix@gmail.com>
2018-05-14 17:45:37 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
-
-
-
-
-
-
-
-
+function sayHiToGrandma(string){
+ if(string.toUpperCase()===string){
+   return "YES INDEED!";
+ }
+ else if(string.toLowerCase()===string){
+  return  "I can't hear you!";
+}
+ else if(string === "I love you, Grandma.")
+   return  "I love you, too.";
+}
