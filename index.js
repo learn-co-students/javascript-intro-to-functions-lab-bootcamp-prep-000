3ca4e8c929158bc66e5e42e610cc1880aa283834
Nat A <natalicea.14@gmail.com>
2018-05-14 16:48:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string
+}
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) { 
+  const a = 'spy'
+  console.log(string.toUpperCase()) 
+}
+
+function logWhisper(string)  { 
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return 'I can\'t hear you!'
+  console.log(string.toLowerCase)
+}
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return `I love you, too.`
+  } else if (string === string.toLowerCase()) {
+    return `I can't hear you!` 
+  } else if (string === string.toUpperCase()) {
+    return `YES INDEED!`
+  }
+}
