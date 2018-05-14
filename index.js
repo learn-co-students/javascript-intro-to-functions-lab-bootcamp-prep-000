082f14508f58902f92b159ee6b91bb5f8c507043
donald.coffin@gmail.com Coffin <donald.coffin@gmail.com>
2018-05-14 21:39:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function shout(string) {
   return string.toUpperCase()
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
+function whisper(string) {
+  return string.toLowerCase()
+}
