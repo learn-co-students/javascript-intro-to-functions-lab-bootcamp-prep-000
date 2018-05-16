1445f6ca062415b2ed6256e8d035078c3bf1b658
Daniel Neil Darling <danieldarling94@gmail.com>
2018-05-16 13:30:26 +0000

    Done.

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log ('HELLO')
+  console.log (string.toUpperCase())
 }
 function logWhisper(string){
-  console.log ('hello')
-}
-function sayHiToGrandma(string){
-  if (string === string.toLowerCase())
-  { return ('I can\'t hear you!');}
-}
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase())
-  { return ('YES INDEED!');}
+  console.log (string.toLowerCase())
 }
 
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string) {
+   return "I can't hear you!"
+}
+
+  
+  if (string.toUpperCase() === string){
+   return "YES INDEED!"
+}
+  
+  
+  if (string === "I love you, Grandma."){ 
+    return "I love you, too."
+}
+}
