04722204c61071261089047fd4e04b052f11a873
Gina Giardinieri <ggiardinieri@gmail.com>
2018-05-17 16:00:22 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,3 @@ function logShout(string){
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-    return "I can't hear you!"
