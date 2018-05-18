19b29faddbb1a3f3a23d639f34c017a1789de3e4
Esayas Gebre <dave183278@gmail.com>
2018-05-18 00:05:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  string=string.toUpperCase()
+  return string
+}
+
+function whisper(string) {
+  string=string.toLowerCase()
+  return string
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase())
+  string ="I can't hear you!"
+  return string
+  }
+  
+
+function sayHiToGrandma(string){
+  if (string==="I love you, Grandma.")
+  string = "I love you, Grandma."
+  return string
+  }
+  
+  
+  
