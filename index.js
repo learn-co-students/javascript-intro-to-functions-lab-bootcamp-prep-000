1906db19749d9fc5098df427d3964577d52fd9d4
trevor.cooke101@gmail.com Cooke <trevor.cooke101@gmail.com>
2018-05-18 14:21:05 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function sayHiToGrandma(string){
 console.log(`I can't hear you, ${string}`)
 return string.toLowerCase()
 }
+function shout(string){
+  return string.toUpperCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase()
+  )
+}
+function logWhisper(string){
+  console.log(string.toLowerCase()
+  )
+}
+expect('sayHiToGrandma(string)').toEqual("I can't hear you"
+)
+function sayHiToGrandma(greeting, firstName) {
+  console.log('I was called!')
+  return `${greeting}, ${firstName}!`
