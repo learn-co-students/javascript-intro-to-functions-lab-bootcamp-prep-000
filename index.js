19dfe36b0ee010a43d243781409763475bd5979c
Michael Peterson <mik3wk@gmail.com>
2018-05-18 02:49:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  console.log(string.toLowerCase)
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+
+function logWhisper(string)
+{
+  console.log('hello')
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string)
+{
+  if (string.toLowerCase() === string)
+  return "I can't hear you!"
+
+  else if (string.toUpperCase() === string)
+  return "YES INDEED!"
+  
+  else ("I love you, Grandma" === string)
+  return "I love you, too."
+}
