1fae8550c6379d8b7e4fc6939eaabf927e51bded
Angie Goza <angielg9888@gmail.com>
2018-05-19 22:37:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  if ((string) === string.toUpperCase())
+  return "YES INDEED!"
+
+  else if ((string) === string.toLowerCase()) 
+    return "I can't hear you!"
+  else 
+    return "I love you, too."
+  }
+  
