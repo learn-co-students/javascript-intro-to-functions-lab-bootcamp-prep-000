d869e642eb245eded91c02993b07ff1378bf9102
Rafael Lopez <lpz.raf@gmail.com>
2018-05-19 16:34:09 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
 
 
