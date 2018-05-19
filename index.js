7eb5254bf33592403f600557c496c8c9133b2a72
Rafael Lopez <lpz.raf@gmail.com>
2018-05-19 15:37:48 +0000

    Done.

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function sayHiToGrandma(string) {
   if((string) === string.toUpperCase())
   {
   return "YES INDEED!"
-  }
+  } else if ((string) === string.toLowerCase) {
+    return "I can't hear you!"
+  } else ((string) === "I love you, Grandma."); {
+    return "I love you, too."
+  } 
 }
