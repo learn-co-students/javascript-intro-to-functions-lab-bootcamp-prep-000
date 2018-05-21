f10a985a17756db881110137ea53a6c9f0f504a3
jinyuli91@gmail.com <jinyuli91@gmail.com>
2018-05-21 21:36:23 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase()) return "I can't hear you"
-}
-
-function sayHiToGrandma(string){
-  if(string === string.toUpperCase()) return "YES INDEED!"
-}
-
-function sayHiToGrandma(string){
-  if(string === "I love you, Grandma.") return " I love you, too."
+  if(string.toLowerCase() === string) return "I can't hear you!"
+  else if(string === string.toUpperCase()) return "YES INDEED!"
+  else if(string === "I love you, Grandma.") return "I love you, too."
 }
