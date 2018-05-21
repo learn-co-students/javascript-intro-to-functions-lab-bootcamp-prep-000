35858eb586a9ff6e82a3d232861fa4ab554155ad
Princess Shuri <webdevdeja@gmail.com>
2018-05-21 20:58:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  return string(whisper('hey grandma'));
+}
