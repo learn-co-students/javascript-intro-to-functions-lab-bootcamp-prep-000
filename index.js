cfa89c4835d6356c09561392ef96f1199a20f294
Rosa Tu <rosalina.tu@gmail.com>
2018-05-22 16:30:28 +0000

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,19 @@ function whisper(string)
 {
   return string.toLowerCase()
 }
-function logShout(string)
-{ console.log('ARGUMENT') }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  if (string.toLowerCase() === string)
+  return "I can\'t hear you!"
+  else if (string.toUpperCase() === string)
+  return "YES INDEED!"
+  else (string === "I love you, Grandma")
+  return 'I love you, too.'
+}
