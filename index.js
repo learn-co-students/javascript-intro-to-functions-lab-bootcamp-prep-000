39505a3d0c4e9b46c0dfed9612f0048649c99f32
Michelle Seyler <michelle.m.seyler@gmail.com>
2018-05-23 02:13:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logshout(string) { 
+  console.log(string.toUpperCase())
+  }
