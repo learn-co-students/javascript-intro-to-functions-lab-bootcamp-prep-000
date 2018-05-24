14a5e2a13475ccaee1fd04e5bd4402f641ac9109
Michelle Seyler <michelle.m.seyler@gmail.com>
2018-05-24 04:15:04 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logshout(string) { 
+function logShout(string) { 
   console.log(string.toUpperCase())
+  }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+    if (string.toLowerCase() === 
+    string) {
+      return "I can't hear you!"
+    }else if (string.toUpperCase() ===
+  string) {
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma.")
+  return "I love you, too."
   }
