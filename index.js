e3a9f19b2a4ebe00b19bc91b65f9d76436de8d58
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-24 15:07:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+  
+}
+
+function logShout(string){
+  
+  logShout('hello')
+  
+  console.log.restore()
+  
+}
