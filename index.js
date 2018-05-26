f51a8b007d311a27053a1c2f5d092df05c47269e
Juan Peralta <peraltajg@yahoo.com>
2018-05-26 02:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log.toLowerCase()
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+  else if (string === "I love you, Grandma.") return "I love you, too."
 }
