84036cded293ab5c2d5cd5ec37df5a8b05d672ac
donald.coffin@gmail.com Coffin <donald.coffin@gmail.com>
2018-05-29 19:21:34 +0000

    Done.

--- index.js
+++ index.js
@@ -6,24 +6,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string){
-  return string.toUpperCase;
-}
-var hello = 'HELLO'
-  console.log(logShout('hello'));
-
-var logShout = 'HELLO'
 function logShout(string) {
-  return string.toUpperCase('hello')
+  console.log(string.toUpperCase())
 }
-console.log(logShout('hello'));
-// expected output: HELLO
 
 function logWhisper(string) {
-  return string.toLowerCase()
+  console.log(string.toLowerCase())
 }
-var logWhisper = 'hello'
-console.log(logWhisper())
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {return "I love you, too.";  }
