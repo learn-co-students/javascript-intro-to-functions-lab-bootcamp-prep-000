657b2f4d83b9564b96d3a4448086b329abf83392
Justin Saadein-Morales <jsaadein@gmail.com>
2018-05-29 00:20:53 +0000

    Done.

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-logShout("hello")
-
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  return string
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
-sayHiToGrandma(hello).toLowerCase()
-sayHiToGrandma(hello).toUpperCase()
-sayHiToGrandma('I love you, Grandma.')
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
