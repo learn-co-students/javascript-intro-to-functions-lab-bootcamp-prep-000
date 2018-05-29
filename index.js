a01823467e3d77f2767bae70cfcb099310c85c54
donald.coffin@gmail.com Coffin <donald.coffin@gmail.com>
2018-05-29 19:07:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 function shout(string) {
   return string.toUpperCase()
 }
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
 
 function whisper(string) {
   return string.toLowerCase()
 }
-function sayHelloToIsabel() {
-	console.log("Hello, Isabel!")
+
+function logShout(string){
+  return string.toUpperCase;
+}
+var hello = 'HELLO'
+  console.log(logShout('hello'));
+
+var logShout = 'HELLO'
+function logShout(string) {
+  return string.toUpperCase('hello')
+}
+console.log(logShout('hello'));
+// expected output: HELLO
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+var logWhisper = 'hello'
+console.log(logWhisper())
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {return "I love you, too.";  }
+  else if (string === "HELLO") {return "YES INDEED!";}
+  else if (string === "hello") {return "I can't hear you!"; }
+  else {
+    return ("arent you sweet")
+  }
 }
