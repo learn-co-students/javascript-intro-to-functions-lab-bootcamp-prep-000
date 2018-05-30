8a493d492f8da29c9cce361dae4e6312954f5516
Joshua Wessels <joshuawessels04@gmail.com>
2018-05-30 14:33:52 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,23 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return string.toUpperCase()
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if(string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+}
+
+
+
