1ca0b34f654e702cf2bdf5442740f2979477b3c9
Griffin H Stein <grste001@mail.goucher.edu>
2018-05-30 00:09:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+  console.log(string.toLowerCase())
+} 
+
+function sayHitoGrandma(string) 
+{ 
+  if (string.toLowerCase() === string)
+  return "I can't hear you!"
+}
+  
