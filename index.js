f9c95873bd2ecae59f887713bf0dddfa3a5ca980
Griffin H Stein <grste001@mail.goucher.edu>
2018-05-30 00:47:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function shout(string) {
-return string.toUpperCase()
+return (string.toUpperCase())
 }
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
 
 function whisper(string) {
   return string.toLowerCase()
 } 
 
-function sayHiToGrandma(string) 
-{ 
-  if (string.toLowerCase() === string)
-  return "I can't hear you!"
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+} 
+
+function sayHiToGrandma(string){ 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!" 
+  }
+    else if (string === "I love you, Grandma.") {
+    return "I love you, too." 
+  }
   
   
 }
