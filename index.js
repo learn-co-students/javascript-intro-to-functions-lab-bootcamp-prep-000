b9b2dcf8e6928f2d74423362e8d1cc7d8b514972
Anthony Udezi <tonywaka@uniben.edu>
2018-06-01 15:11:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  string.toLowerCase()
+  return ('I can\'t hear you!')
+}
