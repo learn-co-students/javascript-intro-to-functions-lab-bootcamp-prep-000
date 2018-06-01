558e94f02ec9dd81b6e73c689aa8b1d5938033b4
Anthony Udezi <tonywaka@uniben.edu>
2018-06-01 19:51:01 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  string.toLowerCase()
+  if (string.toLowerCase() === string)
   return ('I can\'t hear you!')
+
+  if (string.toUpperCase() === string){
+  return ('YES INDEED!')
+}
+  if (string === "I love you,Grandma."){
+  return ("I love you, too.")
 }
