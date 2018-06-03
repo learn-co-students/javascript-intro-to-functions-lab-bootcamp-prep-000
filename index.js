29d11158a6a0ae586466ad71fd7d82277bcb939c
T L <tom.kennedy.lee@gmail.com>
2018-06-03 01:16:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase() 
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('HELLO')
+}
+
+function logWhisper() {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (string == whisper(string)) {
+    return "I can't hear you!" 
+  }
+  else if (string == shout(string)) {     
+  return "YES INDEED!"
+  }
+  else (string = "I love you, Grandma.")
+  return "I love you, too."
+  }
+    
+    
