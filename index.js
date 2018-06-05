c5604b7ce07a02383db50f7caee66409dcad3996
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-05 01:04:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  var noAnswer = "I can't hear you!";
+  var yesAnswer = "YES INDEED!";
+  var loveAnswer = "I love you, too.";
+  if (string.toLowerCase(string) === string) {
+    return noAnswer
+}
+  else if (string.toUpperCase(string) === string) {
+
+    return yesAnswer
+}
+  else if ("I love you, Grandma." === string) {
+    return loveAnswer
+  }
+}
