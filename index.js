9e242f151e0d5eda814d4b7791f3474d5cfa719b
BHAGYARAJ HARANALE <bhagyarajharanale34@gmail.com>
2018-06-05 05:39:35 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  
-  if(string === string.toLowerCase){
-    console.log("I can't hear you!")
+  if(string === string.toLowerCase()){
+    return "I can't hear you!"
   }
-  if(string ===string.toUpperCase){
-    console.log("YES INDEED!")
+  else if(string === string.toUpperCase()){
+    return "YES INDEED!"
   }
-  if(string === "I love you, Grandma."){
-    console.log("I love you, too.")
+  else if(string === "I love you, Grandma."){
+    return "I love you, too."
   }
-  
 }
+
+
