f031a7a6de81fa5a6ed385c12a2da57c2b323fa8
Azhar Shah <azharshahstl@yahoo.com>
2018-06-07 18:25:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout("Hello") {
-  console.log(string.toUpperCase)
+function logShout(string) {
+  console.log(HELLO)
 }
