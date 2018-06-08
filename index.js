7aa8ef7c821b4d758d35fafaca9b14c3efbe1c6f
Ki Ki  Bush <donyabush@gmail.com>
2018-06-08 02:09:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(sting) {
+  return string.toLowerCase()
+}
+
+var upperCase = "HELLO!"
+upperCase.toUpperCase() === uppercase // true
