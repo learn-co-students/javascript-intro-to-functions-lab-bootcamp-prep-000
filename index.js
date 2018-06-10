a0f90f8548433600e53ee9687fd4884f53ff3c73
Michael <schwar6.michael@gmail.com>
2018-06-10 21:16:36 +0000

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return ("I can't hear you!")
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) {
-    return ("YES INDEED!")
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
-    return ("I love you, too.")
+  if (string.toLowerCase()) {
+    return ("I can't hear you!");
+  } else if (string.toUpperCase()) {
+      return ("YES INDEED!");
+  } else if (string === "I love you, Grandma.") {
+      return ("I love you, too.")
   }
 }
