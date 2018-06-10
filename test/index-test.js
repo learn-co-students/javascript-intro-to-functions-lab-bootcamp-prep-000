a18911a2c34da132918a759ea80027f2c1f799a4
frankcao@me.com Cao <frankcao@me.com>
2018-06-10 19:15:00 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,50 +1,50 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', () => {
+  it('receives one argument and returns it in all caps', () => {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
+describe('whisper(string)', () => {
+  it('receives one argument and returns it in all lowercase', () => {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
+describe('logShout(string)', () => {
+  it('calls console.log() its one argument in all caps', () => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
 
     expect(spy).toHaveBeenCalledWith('HELLO')
 
-    console.log.restore()
+    console.log()
   })
 })
 
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
+describe('logWhisper(string)', () => {
+  it('calls console.log() its one argument in all lowercase', () => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logWhisper('HELLO')
 
     expect(spy).toHaveBeenCalledWith('hello')
 
-    console.log.restore()
+    console.log()
   })
 })
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+describe('sayHiToGrandma(string)', () => {
+  it('returns "I can\'t hear you!" if `string` is lowercase', () => {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
+  it('returns "YES INDEED!" if `string` is uppercase', () => {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
   })
 
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', () => {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
