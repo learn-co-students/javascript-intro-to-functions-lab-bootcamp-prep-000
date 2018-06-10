13a42da4fd8988f0eacb9749cc9e0e9e6b017544
Kseniia Tereskina <iv.ksenia@gmail.com>
2018-06-10 23:34:47 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  {console.log(shout(string))}
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  if (string===string.toLowerCase()) 
+return ("I can't hear you!");
+else if (string===string.toUpperCase()) 
+  return ("YES INDEED!");
+else if (string=== "I love you, Grandma.") 
+  return ("I love you, too.");
+}
