9c4dba1c43fad3d7b5b0e889ee81042ea6f65889
Riley Crayton <asuriley@gmail.com>
2018-06-11 13:23:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+return string.toUpperCase() }
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO") 
+}
+  function logWhisper(string) {
+    console.log("hello")
+  }
+  function sayHiToGrandma(string) {
+ if console.log("hello") 
+    return string.toLowerCase()}
+  if console.log("HELLO") {
+  return string.toUppercase()
+  }
+  console.log("I love you, Grandma")
