61c62ecc7484ecd233cc397fc8fe95e939963d9c
brittany collier <prettyawesomeswagg@gmail.com>
2018-06-11 18:45:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+var uppercase = "Hello!"
+uppercase.toUppercase() ===uppercase// true
+var lowercase = 'hello!'
+
+var lowercase='hello!'
+
+lowercase.toLowerCase()===lowercase//true
+
+var mixedCased='Hi there!'
+
+mixedCased.toLowerCase()=== mixedCased// false
+
+mixedCased.toUppercase()===mixedCased//false
