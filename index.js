5d85701be2a35766ff21ea645face894be4d63be
PJ Valentini <pj.valentini@gmail.com>
2018-06-11 21:09:36 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(logShout("hello").toUpperCase());
+  console.log("hello".toUpperCase());
 }
+
+function logWhisper(string) {
+  console.log("HELLO".toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can\'t hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else {
+      (string === "I love you, Grandma.");
+      return "I love you, too."; 
+  }
+}
+
+
 
