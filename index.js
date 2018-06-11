b4ffced22c12a36fd9a33feb36f0a0242d1f41d8
Shravani PL <pl.shravani@gmail.com>
2018-06-11 21:27:32 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
@@ -23,5 +30,6 @@ var art = function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 art=logWhisper(string.toLowerCase())
+
 
 
