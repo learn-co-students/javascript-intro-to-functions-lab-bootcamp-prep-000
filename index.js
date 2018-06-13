647039e5c1b88dd3468eae04a1c0cf198497235b
Lauren Days <lmchambers0420@gmail.com>
2018-06-13 00:45:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('HELLO'); string.toUpperCase;
+}
+
+function logWhisper(string) {
+  console.log('hello'); string.toLowercase;
+}
+function sayHiToGrandma(string) {
+  if ((string) ===uppercase) {
+    return 'YES INDEED';
+  }
+}
+function sayHiToGrandma(string) {
+  if ((string) ===lowercase) {
+    return "I can't hear you!";
+  }
+}
+function sayHiToGrandma(string) {
+  if ((string) ==="I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
+  }
+}
