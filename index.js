19029420b17091a8fdd7f0fb9cbaa09fb2053c3c
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-15 21:10:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,67 @@
+function shout (string)
+
+{
+ 
+  return string.toUpperCase()
+  
+}
+
+function whisper (string)
+
+{
+  
+  return string.toLowerCase();
+  
+}
+
+
+
+
+function logShout (string)
+
+{
+  
+  console.log(string.toUpperCase())
+  
+}
+
+
+
+function logWhisper (string)
+
+
+{
+  console.log(string.toLowerCase())
+  
+}
+
+
+
+function sayHiToGrandma (string) 
+
+{
+  var word = string
+  
+  if (string.toLowerCase() === string)
+ { return 'I can\'t hear you!'}
+ 
+  if (string.toUpperCase() === string)
+ 
+ {return 'YES INDEED!'}
+ 
+ if (string === 'I love you, Grandma.')
+ 
+ {return 'I love you, too.'}
+  
+}
+
+
+whisper("HELLO")
+shout("hello")
+logShout("hello")
+logWhisper("HELLO")
+sayHitoGrandma("hello")
+sayHiToGrandma("HELLO")
+sayHiToGrandma("I love you, Grandma.")
+
+
