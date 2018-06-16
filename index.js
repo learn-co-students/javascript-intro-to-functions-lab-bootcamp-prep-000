a4bd308fbd39df5962e602dbb92cbb95a36c5b52
Thomas Tam <thomasttam@gmail.com>
2018-06-16 06:42:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('Hello'.toUpperCase())
+}
+
+function logWhisper() {
+  console.log('HELLO'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase())
+  return ("I can't hear you!")
+  else if (string === string.toUpperCase())
+  return("YES INDEED!")
+  else (string === "I love you, Grandma.")
+  return("I love you, too.")
+}
