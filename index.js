ba7e74c6644985158f99f3fd997f6c82b4742eff
Susann Weissova <suzi.weissova@gmail.com>
2018-06-18 13:41:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase(); 
+}
+
+function logShout(string){
+  console.log().toUpperCase();
+}
+
+function logWhisper(string){
+  console.log().toLowerCase(); 
+}
+
+function sayHiToGrandma(string){
+ var lowercase = "i can't hear you";
+ lowercase.toLowerCase() === lowercase;
+  return sayHiToGrandma(string); 
+}
