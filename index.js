eb855113f059ec533b898f1a86d6d590aa37cb62
Israel Manzo <israelmanzo814@gmail.com>
2018-06-18 02:03:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  var uppercase = "HELLO!"
+  
+  if (lowercase === 'hello') {
+    return lowercase.toLowerCase()
+  }
+  
+  
+  
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
