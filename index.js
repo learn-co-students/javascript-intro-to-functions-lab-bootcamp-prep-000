56dc35f25d33d0f51b9b874642482c785f3d99fc
Susann Weissova <suzi.weissova@gmail.com>
2018-06-19 09:26:42 +0000

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ function whisper(string){
   return string.toLowerCase(); 
 }
 
-function logShout(string){
-  console.log().toUpperCase();
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
-function logWhisper(string){
-  console.log().toLowerCase(); 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
- var lowercase = "i can't hear you";
- lowercase.toLowerCase() === lowercase;
-  return sayHiToGrandma(string); 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+
 }
