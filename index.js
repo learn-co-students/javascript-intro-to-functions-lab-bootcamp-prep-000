79442eb45871a1485c94fb20dd93befb96645caa
Alexa Schaeffer <alps231@gmail.com>
2018-06-19 02:17:03 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper (string) {
 function logShout (string) {
   console.log(string.toUpperCase())
 }
+
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  if (string === string.toLowerCase()) {
+  return "I can't hear you!"}
+  
+  if (string === string.toUpperCase()) {
+  return "YES INDEED!"}
+  
+  if (string === "I love you, Grandma.") {
+  return "I love you, too."}  
+}
+
