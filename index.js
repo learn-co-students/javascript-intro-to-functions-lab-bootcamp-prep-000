cf875a580e205b6d86934610a5d630a75a649d23
Priyanka Danda <priyankadanda47@gmail.com>
2018-06-19 15:37:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(shout(string))
+}
+function logWhisper(string){
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string){
+  var uppercase = string
+ if(uppercase.toUpperCase() === uppercase)
+{ return"YES INDEED!"
+}
+else if (uppercase === "I love you, Grandma.")
+{return "I love you, too."
+}
+else
+{ return "I can't hear you!"
+}}
+
