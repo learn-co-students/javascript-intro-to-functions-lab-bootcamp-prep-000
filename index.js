1c9674b3ac47f3b79b156066c4b38dc116f79489
Adam Johnson <adamwjo@gmail.com>
2018-06-19 16:16:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(hello) {
+  return 'hello'.toUpperCase()
+}
+
+function whisper(HELLO) {
+  return 'HELLO'.toLowerCase()
+}
+
+function logShout(hello) {
+  console.log('${hello}.toLowerCase')
+}
