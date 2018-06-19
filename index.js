f2f4e99d3080d0af1f3b156beba8cc6b9862f4a1
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-19 16:05:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase("Hello")
+}console.log ("Hello")
+
+function whisper(string) {
+  return string.toLowerCase("Hello")
+}console.log("Hello")
+
+function logShout(string) {
+  console.log(string.toUpperCase("Hello"))}
+ 
+function logWhisper(string) {
+  console.log(string.toLowerCase("Hello"))}
+  
+var string = "Hello"
+function sayHiToGrandma(string){ 
+  if (string===string.toLowerCase()){
+    return ("I can't hear you!")
+  }else if (string===string.toUpperCase()){
+    return ("YES INDEED!")
+  }else {(string==="I love you, Grandma.")
+    return ("I love you, too.")
+  }}
+  
+ 
+   
+   
+   
+    
+    
+ 
