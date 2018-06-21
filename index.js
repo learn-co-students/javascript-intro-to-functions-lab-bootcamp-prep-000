89e6b699ba25859ff14d3f0f7f78b0f9214eb61c
Ali T <alicewtien@gmail.com>
2018-06-21 22:14:09 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.")
-  return "I love you, too."
+    return "I love you, too." 
+
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
 }
 
-function sayHiToGrandm(string) {
-  console.log(string.toLowerCase())
-  if (string === string.toLowerCase()) {
-    return "I can't hear you"
-  }
+if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+}
 }
