d3c9492d7e54a229de526a1a93992334498c673b
fatimah <rini.hasyim@gmail.com>
2018-06-21 16:05:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log( string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string==='hello'){
+    return "I can't hear you!"
+  }else if (string==='HELLO'){
+    return "YES INDEED!"
+  }else if (string==='I love you, Grandma.'){
+    return "I love you, too."
+  }
+  
+}  
+  
+
+  
