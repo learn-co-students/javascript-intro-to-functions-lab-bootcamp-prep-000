5c0e0994ad7446b032417da0034c93ba919714c6
Christian V <viray.christianj@gmail.com>
2018-06-22 00:13:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log.toUpperCase(string)
+}
+
+function logWhisper(string) {
+  console.log(string)
+}
+
+function sayHiToGrandma(string) {
+  return.toLowerCase("I can't hear you!")
+}
+
+function sayHiToGrandma(string) {
+  return.toUpperCase("yes indeed!")
+}
+
+function sayHiToGrandma(string) {
+  return "I love you, too."
+}
