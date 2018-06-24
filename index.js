bd0a17450391e397f61961b6a1ad7a065afb7e10
pritiadhikari@gmail.com <pritiadhikari@gmail.com>
2018-06-24 17:51:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-Function shout(String)
+function shout(string)
 {
   return 
   string.toUpperCase()
 }
+
+function whisper(string)
+{
+  return
+  string.toLowerCase()
+}
+
+funtion logShout(string)
+{
+  console.log("$(string).toUpperCase()")
+}
+
+
+funtion logWhisper(string)
+{
+  console.log("$(string).toLowerCase()")
+}
+
+funtion sayHiToGrandma(string)
+{
+  if(string.toLowerCase()===string)
+  return "I can't hear you!"
+
+  else if(string.toUpperCase()===string)
+  return "Yes INDEED!"
+  
+  else if(string=="I love you,Grandma.")
+  return "I love you,too"
+
+}
+
+
