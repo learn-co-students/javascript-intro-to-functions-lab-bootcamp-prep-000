fcc57436969dd579eae846256f22425d49c99bb1
monthergholam@gmail.com <monthergholam@gmail.com>
2018-06-25 23:21:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+//first
+function shout (string){
+
+  return string.toUpperCase()
+}
+
+//Second
+function whisper (string){
+
+return string.toLowerCase()
+}
+
+//Third
+
+function logShout(){
+  console.log("HELLO")
+}
+
+//Fourth
+function logWhisper(){
+  console.log("hello")
+}
+//Fifth
+
+function sayHiToGrandma(string){
+
+  }
+//Sixth
+function
