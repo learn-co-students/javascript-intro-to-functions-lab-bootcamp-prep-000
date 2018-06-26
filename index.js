35a48d17e057131b28d9523ace3b691b38ee941d
Gabriel Williams <gwgabew@gmail.com>
2018-06-26 13:10:28 +0000

    Done.

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return ("I can't hear you!")
-}
-
-function sayHiToGrandma(string){
-  return ("YES INDEED!")
-}
-
-function sayHiToGrandma(string){
-  return ("I love you, too.")
+  if(string === string.toLowerCase()){
+      return("I can't hear you!")
+  }
+  else if(string===string.toUpperCase()){
+      return("YES INDEED!")
+  }
+  else if(string==="I love you, Grandma."){
+      return("I love you, too.")
+  }
 }
