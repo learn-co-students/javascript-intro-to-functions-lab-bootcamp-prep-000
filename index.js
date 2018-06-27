7f1635c47ab54c12cc38cf96e4e143723d6f4a9c
Sophie Gu <sophieqgu@gmail.com>
2018-06-27 02:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   } else if(string.toUpperCase() === string){
     return "YES INDEED!";
   } else if(string === "I love you, Grandma."){
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
 
