f8f513e9257dcdca510d0bd44a99cc0c2dba4144
jonroger3@gmail.com <jonroger3@gmail.com>
2018-06-27 20:34:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+  
+}
+function whisper(string) {
+  return string.toLowerCase()
+
+}
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string){
+ 
+ var lowercase = 'hello!'
+ lowercase.toLowerCase() === lowercase
+ 
+ var uppercase = 'HELLO!'
+ uppercase.toUpperCase() === uppercase
+ 
+  if (string === lowercase){
+    return "I can\'t hear you!"
+  }
+  
+  if (string === uppercase){
+    return "YES INDEED!"
+    
+  }
+  
+ if (string === 'I love you, Grandma'){
+   return "I love you, too"
+ }
+}
+  
