5623cb7cc3f16606e7494f166b7f8b94f251338a
Amaris <monyei96@hotmail.com>
2018-06-27 15:00:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) { 
+  return string.toLowerCase() 
+  } 
+function logShout(string) { 
+  console.log('HELLO') 
+  } 
+function logWhisper(string) {
+  console.log('hello')
+  }
+function sayHiToGrandma(string) {
+  if ("hello")
+  return "I can\'t hear you!"
+}
+function sayHiToGrandma(string) {
+  if ("HELLO")
+  return "YES INDEED!"  
+  }
+function sayHiToGrandma(string) {
+  if ("I love you, Grandma.")
+  return "I love you, too."
+}
