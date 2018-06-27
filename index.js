707d441cd5a95e74bdee4eb78f180145b914eb7c
Markell Owens <markellowens@mac.com>
2018-06-27 21:00:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 function shout(string) {
-  return string.toUpperCase ()
+  return string.toUpperCase()
 }
+
 function whisper(string) {
-  return string.toLowerCase ()
+  return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
-  return "I can't hear you";
-}
-function sayHiToGrandma(string) {
-  return string.toUpperCase ("YES INDEED!")
-}
-function sayHiToGrandma(string) {
-  var string = "I love you, Grandma"
-  return "I love you, too"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
