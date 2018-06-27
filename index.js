2cd285aceb5bd61d415e9fac92fb7c739cdbd62d
Markell Owens <markellowens@mac.com>
2018-06-27 16:36:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  return "I can't hear you";
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase ("YES INDEED!")
+}
+function sayHiToGrandma(string) {
+  var string = "I love you, Grandma"
+  return "I love you, too"
+}
