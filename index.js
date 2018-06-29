2a604bb2956c886d9f08ed4272112bffa5c77a58
kosatun@aol.com <kosatun@aol.com>
2018-06-29 03:28:49 +0000

    Done.

--- index.js
+++ index.js
@@ -16,21 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-  }
-    return "I can't hear you!"
-}  
-   
-function sayHiToGrandma(string) {   
-  if (string.toUpperCase === string) {
-   }   
-    return "YES INDEED."
-}  
-  
- 
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
-  }   
+   return "I can't hear you!"
+} else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+} else if (string === "I love you, Grandma.") {
     return "I love you, too."
+}
 }
 
 
