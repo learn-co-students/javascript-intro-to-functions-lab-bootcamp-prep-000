6e8a592337ba5a1a63213345d41793a6bd5a83a6
Barney Loosemore <flatiron@barney.co.uk>
2018-07-01 13:43:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string){return string.toUpperCase()}
+function whisper(string){return string.toLowerCase()}
+
+var logShout = "string"
+
+if (logShout === ""
+
+console.log(string).toUpperCase()}
+
+
