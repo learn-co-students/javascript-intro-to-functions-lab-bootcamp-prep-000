9110c015b997cd880c8bf840b13065e0d4f6493a
cd <coraldarling@outlook.com>
2018-07-02 15:50:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string)  {
+  return string.toUpperCase()  }
+
+shout('hello')
+
+function whisper(string)  {
+  return string.toLowerCase()}
+
+  whisper('HELLO')
+
+function logShout()  {
+  console.log('HELLO')  }
+
+  logShout('HELLO')
+
+function logWhisper()  {
+  console.log('hello')  }
+
+  logWhisper('hello')
+
+
+function sayHiToGrandma(string) {
+
+  if (string === string.toLowerCase())  {
+  return ("I can't hear you!")
+
+} else if (string === string.toUpperCase())  {
+  return  ("YES INDEED!");
+
+} else ('string' === 'I love you Grandma')
+  return ('I love you, too.')
+
+
+}
