f0d2b81a70d40f4a33c4e2d2430be5de257b9c7a
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 07:51:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 function shout(string) {
-  return string.toUpperCase
+  return string.toUpperCase()
 }
 
-function whisper(string){
-  return string.toLowerCase
+function whisper(string) {
+  return string.toLowerCase()
 }
 
-function sayHiToGrandma(string){
-  string = 'I love you, Grandma.' 
-  {
-    return 'I love you, too.'
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-  if (`$string`.toUpperCase){
+
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  if (`$string`.toLowerCase) {
-    return "I can't hear you!";
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 
+  return "Are you eating enough?"
 }
-
-
