9ac7b02eac0afa675090420467dea8798e4ad2b0
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 07:47:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase
+}
+
+function whisper(string){
+  return string.toLowerCase
+}
+
+function sayHiToGrandma(string){
+  string = 'I love you, Grandma.' 
+  {
+    return 'I love you, too.'
+  }
+  if (`$string`.toUpperCase){
+    return "YES INDEED!"
+  }
+  if (`$string`.toLowerCase) {
+    return "I can't hear you!";
+  }
+
+}
+
+
