b78f01d3c505f72cf57e0dc9f00600901988a40d
Zeshawn Khan <zeshawnk@gmail.com>
2018-07-08 19:37:01 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  
-  
+    if (string.toLowerCase() === string){
+        return 'I can\'t hear you!'
+    }
+    
+    if (string.toUpperCase() === string){
+        return 'YES INDEED!'
+    }
+    
+    if (string === 'I love you, Grandma.'){
+        return 'I love you, too.'
+    }
 }
