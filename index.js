6a60ff3f8245807e1dac877f09ee44e2f79c7db8
Benjamin Noyes <noyesbenjamin@gmail.com>
2018-07-08 19:43:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+  return 
+  string.toUpperCase()
+}
+
+function whisper(string) {
+  return 
+  string.toLowerCase()
+}
+
+function logShout(hello) {
+  console.log(
+    'HELLO'.toUpperCase())
+}
+
+function logWhisper(HELLO) {
+  console.log(
+    'hello'.toLowerCase())
+}
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === 
+uppercase
+
+var lowercase() === "Hello!"
+
+lowercase.toLowerCase() ===
+lowercase
+
+var mixedCase = "Hi there!"
+
+mixedCase.toLowerCase() ===
+mixedCase
+
+mixedCase.toUpperCase() ===
+mixedCase
+
+function sayHiToGrandma(string) {
+  if lowercase() === true
+  console.log(
+    "I can't hear you!")
+}
