a53b6e21e05c69ccb01a40495c879e29c4278dec
Lili Rodriguez <lilimrod@gmail.com>
2018-07-08 02:36:32 +0000

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma(string){
     return 'YES INDEED!'
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return 'I love you, too.'
   }
 }
