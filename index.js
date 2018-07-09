75a5eadfb5d583cc1d50bd6196665240083dbe41
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-09 18:36:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function shout(string) {return string.toUpperCase() }
+function whisper(string) {return string.toLowerCase() }
+function logShout(string) { console.log('HELLO') } 
+function logWhisper(string) { console.log('hello') }
+function sayHiToGrandma(string) { return string.toUpperCase() }
+function sayLoveToGrandma(string) { console.log('I LOVE YOU, GRANDMA.'); return('I love you, too.') }
