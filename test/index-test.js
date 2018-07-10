d32d21941f66429e8a0f1524269e1fd75fdfdcc0
diegonovoa@me.com <diegonovoa@me.com>
2018-07-10 16:08:45 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,16 +1,22 @@
-
+function shout(string) {
+  return string.toUpperCase()
+}
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
-
+function whisper(string) {
+  return string.toLowerCase()
+}
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
-
+function logShout(string) {
+  return string.toUpperCase()
+}
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
@@ -47,4 +53,10 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
+  function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+
 })
