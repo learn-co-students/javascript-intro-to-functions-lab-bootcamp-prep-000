3a4d11dc3171f727dd8c6450ba9f2f2fb9b88df4
Anthony Schons <amschons@gmail.com>
2018-07-10 01:43:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(sayHiToGrandma) {
+  return string.toUpperCase()
+}
+
+function whisper(sayHiToGrandma) {
+  return string.toLowerCase()
+}
+
+function Shout(sayHiToGrandma) {
+  console.Shout ;string.toUpperCase()
+}
+
+if (sayHiToGrandma) (sayHiToGrandma) === "string.toLowerCase" ;{
+  console.log("I can't hear you!")
+  }
+  if (sayHiToGrandma === "string.toUpperCase")
+    {console.log("YES INDEED!")
+  }
+  
+if (sayHiToGrandma === "I love you, Grandma") {
+  console.log("I love you, too")
+  }
