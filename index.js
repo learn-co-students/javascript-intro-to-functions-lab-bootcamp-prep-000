ba59759094f1868ec615cf8e5b714ef05f856006
Joshua Smith <jjsmith260@hotmail.com>
2018-07-11 06:20:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  //string = string.toUpperCase
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  //string = string.toLowerCase
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)	{
+	if(string.toLowerCase() === string)	{
+		return "I can't hear you!";
+	}
+	
+	if(string.toUpperCase() === string)	{
+		return "YES INDEED!";
+	}
+	
+	if(string === "I love you, Grandma.")	{
+		return "I love you, too.";
+	}
+}
