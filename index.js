b26a16830e30fd4188c497aea7927f9693b96339
Jason Rosenbaum <jsr5215@gmail.com>
2018-07-11 23:24:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-function shout(string){
+function shout(string) {
   return string.toUpperCase()
 }
-function whisper(string){
+
+function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string){
-  console.log(string.toUpperCase());
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
-function logWhisper(string){
-  console.log(string.toLowerCase());
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
-  var string = "I love you, Grandma";
-  return "I love you too"
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
