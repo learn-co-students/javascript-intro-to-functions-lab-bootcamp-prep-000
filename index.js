020678ae8452a524b627da3a9f6710ae59837496
Ashley Pereira <ashleypereira70@gmail.com>
2018-07-11 11:31:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {return string.toUpperCase}
+
+var uppercase = "Hello!"
+
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
