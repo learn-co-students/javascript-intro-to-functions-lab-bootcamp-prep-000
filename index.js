eccebd518707627c8f7768fddaea920c7a306aab
edwardcastillo@me.com Castillo <edwardcastillo@me.com>
2018-07-12 19:29:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  console.log('hello');
+}
