691b5ebabbb9a442e166fe2386eaa8fa413442f5
Ashley Pereira <ashleypereira70@gmail.com>
2018-07-12 08:19:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function shout(HiToGrandma) {return ('HiToGrandmaf!').toLowerCase()
   
 }
-console.log .toLowerCase = ("HiToGrandma!")
-
+{console.log .toLowerCase() === lowercase("HiToGrandma!")
+}
