00ca10f61ed0943876ad6045d7bae250bbdf5d7d
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-13 05:53:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  var uppercase = string.toUpperCase();
+  var lowercase = string.toLowerCase();
+
+
+  if (string === uppercase){
+    return ("YES INDEED!");
+  }
+  else if (string === lowercase) {
+    return ("I can\'t hear you!")
+  }
+  else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+}
