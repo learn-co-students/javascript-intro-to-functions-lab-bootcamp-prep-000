1afb645c03edb74939b74fd3239d02a4aeada13a
livingshine@gmail.com <livingshine@gmail.com>
2018-07-13 19:56:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var statement;
+  
+ string.toLowerCase === string
+ statement = 'I can\'t hear you!'
+  
+ string.toUpperCase === string
+ statement = 'YES INDEED!'
+   
+ statement = 'I love you, too.'
+  
+  return statement
+}
