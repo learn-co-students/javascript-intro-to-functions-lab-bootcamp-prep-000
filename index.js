1a837d2bf4b612fa8304604375bcdae41f1a8ddb
ktochow1 <ktochow1@binghamton.edu>
2018-07-13 19:51:24 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function whisper(string) {
 function logShout(string) {
   console.log.toUpperCase
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())  {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+
+  function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
