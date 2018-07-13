a857e4f0475104496ed61d2135aa0acb3393222e
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-07-13 02:18:47 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string)
-  console.log(expect.spyOn.toUpperCase())
+function logShout(string){
+  return console.log(string.toUpperCase())
+}
   
-  
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!"
+  else if (string === string.toUpperCase()) return "YES INDEED!"
+  else if (string === "I love you, Grandma.") return "I love you, too."
+}
