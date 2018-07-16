b9e0041fd91c1db66399b1c236bf1a7f88f4d57d
Brenda  Almonte <breal0811@gmail.com>
2018-07-16 19:47:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+    return string.toUpperCase() 
+}
+function whisper(string) {
+  return 'HELLO'.toLowerCase()
+}
+function logShout(string){
+ return 'hello'.toUpperCase()
+}
