b5b9168508dfcef83f954329e36d41776a432a71
Dmitry D <vantive2000@gmail.com>
2018-07-17 23:33:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) { 
+  return (string.toUpperCase())
+}
+
+function whisper(string) {
+  return (string.toLowerCase())
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var uppercase = string
+  var lowercase = string
+  var iLoveYou = "I love you, Grandma."
+  
+
+  if (string === iLoveYou)
+    {return ("I love you, too.")}
+    
+  if 
+    (lowercase.toLowerCase() === lowercase)
+    {return (`I can't hear you!`)}
+  else
+    (uppercase.toUpperCase() === uppercase)
+    {return ("YES INDEED!")    }
+
+}
