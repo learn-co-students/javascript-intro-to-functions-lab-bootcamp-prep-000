16b867eb8533bd58aac169bc19fc1b108b278767
Brenda  Almonte <breal0811@gmail.com>
2018-07-17 18:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,26 @@ function whisper(string) {
   return 'HELLO'.toLowerCase()
 }
 function logShout(string){
- return 'hello'.toUpperCase()
+ console.log(string.toUpperCase()) 
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  console.log("I love you, Granda".toUpperCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
