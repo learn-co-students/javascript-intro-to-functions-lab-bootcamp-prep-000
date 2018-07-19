8d4d02229e4320853a74cc28738088655b7fabb0
felixyangj@gmail.com <felixyangj@gmail.com>
2018-07-19 02:40:02 +0000

    Done.

--- index.js
+++ index.js
@@ -15,17 +15,10 @@ function logShout(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === 'I love you, Grandma');
-  return `I love you, too.`;
-
-}
-
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase());
-  return `YES INDEED!`;
-}
-  
-function sayHiToGrandma(string){
-  if (string === string.toLowerCase());
-  return `I can't hear you!`
+  if (string === 'I love you, Grandma'){
+  return `I love you, too.`}
+  if (string === string.toUpperCase()){
+  return `YES INDEED!`}
+  if (string === string.toLowerCase()){
+  return `I can't hear you!`}
 }
