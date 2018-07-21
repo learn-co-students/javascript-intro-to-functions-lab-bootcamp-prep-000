cde4f2e76820ba719d4b9cebd4fbc24b65ad11a3
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-21 20:22:55 +0000

    Done.

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var aPhrase = string
   console.log("Grandma tries to hear",aPhrase)
-  if (lowercase.aPhrase === lowercase) {
-    console.log("Grandma says I can\'t hear you!")
-  } else if (uppercase.aPhrase === uppercase) {
-    console.log("Grandma says YES INDEED!") 
-  } else if (uppercase.aPhrase === "I love you, Grandma") { 
-  //          (lowercase.aPhrase === "I love you, Grandma") {
+  if (aPhrase.toLowerCase() === "i love you, grandma") { 
+  //          (aPhrase.toUpperCase() === "I LOVE YOU, GRANDMA") {
     console.log("Grandma says I love you, too.") 
+  } else if (aPhrase.toLowerCase() === lowercase) {
+    console.log("Grandma says I can\'t hear you!")
+  } else if (aPhrase.toUpperCase() === uppercase) {
+    console.log("Grandma says YES INDEED!") 
   }
 }
 // ReferenceError: sayHiToGrandma is not defined 
