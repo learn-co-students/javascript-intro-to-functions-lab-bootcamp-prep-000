53b4222e73bb54c1ab59a51fca34070532f4c9b6
Aranya Iyer <aranya6776@gmail.com>
2018-07-21 00:15:13 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
 function logShout(string){
-  console.log() string.toUpperCase()
-  logShout(`hello!`)
+  console.log("hello".toUpperCase())
+}
+
+function logWhisper(string){
+  console.log("HELLO".toLowerCase())
+}
+
+
+function sayHiToGrandma(string){
+  if("hello".toLowerCase() ==="hello") {
+    return("I can't hear you!")
+  }
+  if("HELLO".toUpperCase()==="HELLO"){
+    return("YES INDEED!")
+  }
+  if("I love you, Grandma" === "I love you, Grandma"){
+  return("I love you, too.")
+  }
 }
