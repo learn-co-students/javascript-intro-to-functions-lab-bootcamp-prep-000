b7823206eefff363c8b8f2e2b065846c98772be8
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-21 16:44:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function shout(string) {
   return string.toUpperCase()
 }
-// fixed ReferenceError: shout is not defined
+// fixed ReferenceError: shout is not defined by creating function
 
 function whisper(string) {
-  return string.toLowerCase()
+//  return string.toLowerCase()
 }
-// fixed ReferenceError: whisper is not defined
+// fixed ReferenceError: whisper is not defined by creating function
 
 function logShout(string) {
 //  console.log(string)
 }
-// fixed ReferenceError: logShout is not defined
+// fixed ReferenceError: logShout is not defined by creating function
 // NEW Error: spy was never called with [ 'HELLO' ]
 
 function logWhisper(string) {
 //  console.log(string)
 }
-// fixed ReferenceError: logWhisper is not defined
+// fixed ReferenceError: logWhisper is not defined by creating function
 // NEW Error: spy was never called with [ 'HELLO' ]
 
 function sayHiToGrandma(string) {
 //  console.log(string)
 }
-// fixed ReferenceError: sayHiToGrandma is not defined
+// fixed ReferenceError: sayHiToGrandma is not defined by creating function
 // NEW Error: Expected undefined to equal 'I can\'t hear you!'
 // NEW Error: Expected undefined to equal 'YES INDEED!'
 // NEW Error: Expected undefined to equal 'I love you, too.'
