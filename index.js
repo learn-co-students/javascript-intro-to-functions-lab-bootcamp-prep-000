5aec8c65111d479c65a4c353ff8488cbac9bc204
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-21 18:26:20 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function shout(string) {
   return string.toUpperCase()
   logShout(string.toUpperCase())
 }
-// fixed ReferenceError: shout is not defined by creating function
+// fixed ReferenceError: shout is not defined 
+// -- fixed by creating function
 // NEW Error: Expected undefined to equal 'HELLO'
 // -- fixed with string.toUpperCase()
 
@@ -14,20 +15,23 @@ function whisper(string) {
   return string.toLowerCase()
   logWhisper(string.toLowerCase())
 }
-// fixed ReferenceError: whisper is not defined by creating function
+// fixed ReferenceError: whisper is not defined 
+// -- fixed by creating function
 // NEW Error: Expected undefined to equal 'hello'
 // -- fixed with string.toLowerCase()
 
 function logShout(string) {
   console.log('shout argument is ${string}')
 }
-// fixed ReferenceError: logShout is not defined by creating function
+// fixed ReferenceError: logShout is not defined 
+// -- fixed by creating function
 // NEW Error: spy was never called with [ 'HELLO' ]
 
 function logWhisper(string) {
   console.log(string)
 }
-// fixed ReferenceError: logWhisper is not defined by creating function
+// fixed ReferenceError: logWhisper is not defined 
+// -- fixed by creating function
 // NEW Error: spy was never called with [ 'hello' ]
 
 function sayHiToGrandma(string) {
