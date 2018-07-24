a85520fd2f75e3bacef5fe466baf83c094b6bdcc
David Z <davidzabran@gmail.com>
2018-07-24 03:53:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+// #1
+function shout(string) {
+  return string.toUpperCase()
+}
+
+// #2
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+// #3
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+// #4
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+  return "I can't hear you!"
+} else if(string === string.toUpperCase()) {
+  return "YES INDEED!"
+} else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+  }
+}
