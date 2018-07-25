185839e5921dd56bc5c1be7952926fc7d756c6f5
Colette Mahoney <colettemahoney@hotmail.com>
2018-07-25 20:00:07 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,23 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
- console.log ('its one argument in all caps')}
- 
+  console.log('HELLO')
+}
+ function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  if (string === 'hello') {
+    return "I can't hear you!"
+  }
+
+  if (string === 'HELLO') {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
