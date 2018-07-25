b0ce3f437f598fd8a6cda1ee842ed8df10bbb848
Angela Deen <adeen1221@gmail.com>
2018-07-25 03:36:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string){
+return string.toUpperCase()
+}
+function whisper(string){return string.toLowerCase()}
+function logShout(string){console.log(string.toUpperCase())}
+function logWhisper(string){console.log(string.toLowerCase())}
+function sayHiToGrandma(string) 
+ { if (string) is lowercase {return ("I love you, too")
+  } else {return ("YES INDEED!")
+}
+
