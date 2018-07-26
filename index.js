dec3c64f8bb39bbbefeb86d3a36a9cd2796c8fc2
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-26 13:30:51 +0000

    Done.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function whisper(string) {
 
 function logShout(string) {
   var inShout = string
-  console.log('I shouted',inShout)
+  console.log('I shouted', inShout)
   return inShout
 //  console.log('I shouted','HELLO') // workaround FAILED to clear NEW Error: spy...
 //  console.log('HELLO') // workaround to clear NEW Error: spy...
