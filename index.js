11bb2a2c8f666358c4ce69be5b56e2939b86fecf
abadawi2 <abadawi@terpmail.umd.edu>
2018-07-26 00:29:45 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase() === string){
   return "YES INDEED!"}
   if (string.toLowerCase() === string){
-  return "I can/'t hear you!"}
+  return "I can\'t hear you!"}
   if (string === "I love you, Grandma."){
     return "I love you, too."
   }
