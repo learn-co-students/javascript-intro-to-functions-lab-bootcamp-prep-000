37c6acce6a2cb540a45abc9600a1f8c5bbedde7a
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-26 12:52:32 +0000

    Done.

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function sayHiToGrandma(string) {
 // when inPharse is lowercase return outPhrase1
 // when inPhrase is uppercase return outPhrase2
 // when inPhrase is 'I love you, Grandma.' return outPhrase3
-/* will pass without neither lowercase or uppercase test 
-  if (inPhrase.toLowerCase() === inPhrase) { // ReferenceError: lowercase is not defined
+
+  if (inPhrase.toLowerCase() === inPhrase) { 
     console.log("Grandma replied",outPhrase1)
   }
  
@@ -82,7 +82,7 @@ function sayHiToGrandma(string) {
   if (inPhrase.toLowerCase() === inPhrase) { 
     console.log("Grandma replied",outPhrase2)
   }
- */
+ 
  /*
   if (inPhrase === "I love you, Grandma.") {
     console.log("Grandma replied",outPhrase3)
