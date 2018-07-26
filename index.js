0736217b41d923d61bc96cdf05e7a74a425a8c0a
Angela Deen <adeen1221@gmail.com>
2018-07-26 03:05:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function shout(string){
-return string.toUpperCase()
+return string.toUpperCase();
 }
 function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string){console.log(string.toLowerCase())}
-function sayHiToGrandma(string) 
- { if (string) is lowercase {return ("I love you, too")
-  } else {return ("YES INDEED!")
-}
 
+function sayHiToGrandma(arg){
+  if(arg===whisper(arg)){
+    return "I can't hear you!" ;
+  }
+  else if(arg===shout(arg)){
+    return"YES INDEED!";
+  }
+else if(arg==="I love you, Grandma."){
+  return"I love you, too.";
+
+}
+}
