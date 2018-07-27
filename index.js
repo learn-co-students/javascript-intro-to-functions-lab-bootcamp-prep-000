fb9a7d003b7317ae6e8ee4704436c01471f80f63
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-27 12:35:07 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function logShout(string) {
   var loudShout = string.toUpperCase
   console.log('I shouted', inShout)
   console.log('I should have shouted')
-  consol.log(loudShout)
+  console.log(loudShout)
 //  console.log('I shouted','HELLO') // workaround FAILED to clear NEW Error: spy...
 //  console.log('HELLO') // workaround to clear NEW Error: spy...
   return inShout
