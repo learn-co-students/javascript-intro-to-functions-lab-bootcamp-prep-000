f881a0eaf1e9b9f0062a836ee5e496f9b1ab141a
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-28 17:57:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(){
+  const spy = "Hello";
+  console.log(spy.toUpperCase())
+}
+function logWhisper(string){
+  const spy = "Hello";
+  console.log(spy.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+  return "I can't hear you!"
+} else if(string === string.toUpperCase()) {
+  return "YES INDEED!"
+} else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+  }
+}
