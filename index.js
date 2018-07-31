47687e6bbe2f79f4b155fb79ef173ee9957d89c6
sam ditson <samditson2@hotmail.co.uk>
2018-07-31 11:58:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string){return string.toUpperCase()}
+
+function whisper(string){return string.toLowerCase()}
+
+function logShout(string){ var x = string.toUpperCase(); console.log(x); }
+
+function logWhisper(string){var x = string.toLowerCase(); console.log(x);}
+
+function sayHiToGrandma(string){var x = lowercase.toLowerCase() === lowercase;}
