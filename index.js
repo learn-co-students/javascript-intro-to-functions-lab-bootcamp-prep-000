40cc48289ad1bf779fc745b38de28065d4e1a282
sam ditson <samditson2@hotmail.co.uk>
2018-07-31 16:42:35 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function logShout(string){ var x = string.toUpperCase(); console.log(x); }
 
 function logWhisper(string){var x = string.toLowerCase(); console.log(x);}
 
-function sayHiToGrandma(string){var x = lowercase.toLowerCase() === lowercase;}
+ function sayHiToGrandma(string){
+   if(string.toLowerCase() === string){
+     return `I can't hear you!`;
+     
+   } else if (string === "I love you, Grandma."){
+     return `I love you, too.`;
+     
+   } else {
+     return `YES INDEED!`;
+     
+   }}
