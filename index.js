9d96514093be0d2028b41417c16857d944dad227
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-01 11:40:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
@@ -20,4 +21,8 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase() === string)
   return "YES INDEED!"
   else return "I love you, too."
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 }
