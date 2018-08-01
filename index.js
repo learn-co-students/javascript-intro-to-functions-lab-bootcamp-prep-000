332f50912efcc48e49b3411133e591d0f3b1dc55
Ignacio R Espino <irespino@capozios.com>
2018-08-01 14:47:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(anArg) {
+  return anArg.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) return "YES INDEED!";
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  if (string === 'I love you, Grandma.') return "I love you, too.";
+ }
