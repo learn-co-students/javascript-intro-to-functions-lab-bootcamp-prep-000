083f8d66023f5a0dba26e5003dc2e96ef68f8a2e
Luis J Feliz <lfjo.mobil@gmail.com>
2018-08-04 16:03:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logshout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
