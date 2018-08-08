f59b700fe5b9bc8c6267d9bff3b876b48a9f6d3d
Preston A Arnette <arnettepreston@gmail.com>
2018-08-08 00:40:41 +0000

    Done.

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ function sayHiToGrandma(string) {
   else if(string === "I love you, Grandma.")
     return "I love you, too.";
 }
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
