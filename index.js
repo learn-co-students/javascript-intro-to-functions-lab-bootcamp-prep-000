e62b9df3b64df0cb05f584342ecf13ce6ec43793
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-08 15:13:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function shout(string) {
-  return string.toUpperCase()
-}
+  return string.toUpperCase();
+};
 
 function whisper(string) {
-  return string.toLowerCase()
-}
+  return string.toLowerCase();
+};
 
 function logShout(string) {
-  console.log(string.toUpperCase())
-}
+  console.log(string.toUpperCase());
+};
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
-}
+  console.log(string.toLowerCase());
+};
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
