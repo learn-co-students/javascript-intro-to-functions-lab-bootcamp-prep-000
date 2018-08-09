70feb8b52c238779ea1f57b389973dc0318f1656
rayzafir@aol.com <rayzafir@aol.com>
2018-08-09 23:06:10 +0000

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ function logShout(string) {
 }
 function logWhisper(string) {
   console.log("hello")
+  }
+function greetingsGrandma(string) {
+  console.log("I love you, Grandma.")
 }
+  
 function sayHiToGrandma(string) {
-  if(whisper==="hello")
-  return string ('I can\'t hear you!')
+  if (string=== string.toLowerCase())
+  return "I can't hear you!"
+
+else if (string=== string.toUpperCase())
+return "YES INDEED!"
+
+else (string===greetingsGrandma)
+return "I love you, too."
 }
