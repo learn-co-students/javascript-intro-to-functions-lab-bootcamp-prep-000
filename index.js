e01b11ee1c812b9686048aa448d49a95d8cafe34
Ismail Jalloh <ijalloh21@gmail.com>
2018-08-11 23:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  return string.toUpperCase()
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === "hello") {
+    return "I can\'t hear you!"
+  }
+  else if (string === "HELLO") {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
