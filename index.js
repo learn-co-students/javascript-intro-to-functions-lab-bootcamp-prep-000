251482093a1edf55efe6541b6e661e0cf2fb46ea
Manju Thomas <kudukacar@hotmail.com>
2018-08-12 19:11:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) { 
+  return string.toLowerCase() 
+}
+function logShout(string) { 
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) 
+string === string.toLowerCase() {
+  return "I can\'t hear you Grandma!"
+}
+function sayHiToGrandma(string)
+string === string.toUpperCase() {
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string)
+string === string.mixedCase() {
+  return "I love you, too."
+}
