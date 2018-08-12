d0f3532c49b069f09b33c583e56084d6665d77b3
Chiara Cokieng <chiara@chiaracokieng.com>
2018-08-12 01:14:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return `I love you, too.`
+  } 
+
+  var lowercase = 'hello'
+  lowercase.toLowerCase() === lowercase
+  
+  var uppercase = 'HELLO'
+  uppercase.toUpperCase() === uppercase
+  
+  if (string === lowercase) {
+    return `I can\'t hear you!`
+  }
+  
+  if (string === uppercase) {
+    return `YES INDEED!`
+  }
+
+}
