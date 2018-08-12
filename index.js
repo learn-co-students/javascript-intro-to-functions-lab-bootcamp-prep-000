f5f3b28088b2bf466b81c9bf72db8c333fe8c77a
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-08-12 21:18:16 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log('string.toUpperCase')
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";}
 }
