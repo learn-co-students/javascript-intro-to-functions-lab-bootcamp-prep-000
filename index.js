329b73d84015416c5c92a02692e0bb390f730e0f
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-12 13:26:21 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,26 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
 function sayHiToGrandma(string) {
-  var lowercase = 'hello!'
-  string.toLowerCase() === lowercase
- if (string === lowercase) {
-   return "I can\'t hear you!"
- }
-  
+ 
+if (string === "hello")
+{return "I can't hear you!"}
+if (string === "HELLO")
+{return "YES INDEED!"}
+if (string === "I love you, Grandma.")
+{return "I love you, too."}
 }
