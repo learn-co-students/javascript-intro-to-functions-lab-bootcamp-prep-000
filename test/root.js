b47be0f761bfff169600b8fbf6401e6d3bd42226
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-12 17:13:41 +0000

    Done.

--- test/root.js
+++ test/root.js
@@ -4,12 +4,13 @@ const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
 
+
 before(function(done) {
   const babelResult = babel.transformFileSync(
     path.resolve(__dirname, '..', 'index.js'), {
       presets: ['es2015']
     }
-  );
+  ); 
 
   const html = path.resolve(__dirname, '..', 'index.html')
 
