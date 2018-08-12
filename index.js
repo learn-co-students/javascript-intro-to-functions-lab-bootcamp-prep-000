a8be404058a2555b5b1afcc3cbf3bf7edcb8d0ae
Colette Mahoney <colettemahoney@hotmail.com>
2018-08-12 10:43:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
+
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
+
 function logShout(string) {
-  console.log('HELLO')
+  console.log('HELLO');
 }
- function logWhisper(string) {
-  console.log('hello')
+
+function logWhisper(string) {
+  console.log('hello');
 }
+
 function sayHiToGrandma(string) {
   if (string === 'hello') {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
-
-  if (string === 'HELLO') {
-    return "YES INDEED!"
+  else if (string === 'HELLO') {
+    return "YES INDEED!";
   }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
-
-  return "Are you eating enough?"
+  
 }
