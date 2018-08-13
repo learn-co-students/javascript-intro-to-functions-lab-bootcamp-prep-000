590c5b6b657d8e9b95c80fa15ed997cb18d5c3cf
Keshia White <keshiamwhite@gmail.com>
2018-08-13 01:43:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return('HELLO')
+  string.toUpperCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  string.toLowerCase()
+}
+
+function whisper(string) {
+  return('hello')
+  string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {{ 
+ if (string === string.toUpperCase()) 
+  {return('YES INDEED!')
+}}
+{ if (string === string.toLowerCase())
+  {return("I can't hear you!")
+}}
+{  if (string === "I love you, Grandma.")
+ {return("I love you, too.")
+}}}
+
