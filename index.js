035249546fbfdff83fb37f83f429e14a0ceea865
Dhouglas Oaktree <dhouglas@gmail.com>
2018-08-15 16:57:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+var lowercase = "I can't hear you!"
+
+ mixedCased.toLowerCase() === lowercase
+
+  return (string)
+
+}
