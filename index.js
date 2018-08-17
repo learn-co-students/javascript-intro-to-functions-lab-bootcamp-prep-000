4cdd100e205cc6de0d3ad034c25faaeacd6b68e7
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-17 20:59:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,85 @@
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
+
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('sayHelloToGrandma')).toEqual('sayHelloToGrandma')
+  })
+})
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
+
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+function shout(string) {
+  return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function  sayHiToGrandma(string) {
+  return string.toEqual
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase
+}
+function sayHiToGrandma(string) {
+  return string.toEqual
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
