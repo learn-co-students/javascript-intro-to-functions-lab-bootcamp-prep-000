09327d5531ac5b6411a8b39e355494a60d83fe17
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-08-18 13:54:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log();
+}
+
+function logWhisper(string){
+  console.log();
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase();
+}
