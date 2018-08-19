efcc9f015afcb496cf2479e0471abe43fcb3c325
dwolfe1987@gmail.com <dwolfe1987@gmail.com>
2018-08-19 20:45:18 +0000

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return
-  string.toLowerCase()
+  if(string.toLowerCase() === string)
+  {
+  return 'I can/'t hear you!';
+  }
 }
