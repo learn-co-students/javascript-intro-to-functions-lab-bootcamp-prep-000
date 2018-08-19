49a871392fe4fa7022c0a998112faaa9ed94bcc1
Glorious Lemon <gloriouslemon@gmail.com>
2018-08-19 13:36:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+// var word = "I love you, Grandma."
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+//logWhisper (word)
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else {
+    return "Em rocks"
+  }
+}
+
+
+// var em = sayHiToGrandma(word)
+// console.log(em)
+
+
+
