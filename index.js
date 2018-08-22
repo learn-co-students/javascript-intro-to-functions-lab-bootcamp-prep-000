6cf47382c0526e17678549de899f83332e103c85
Dorothy Davenport <dorothympdx@gmail.com>
2018-08-22 16:55:57 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase()){
+  if(string.toLowerCase()=== string){
   return "I can't hear you!"
   }
+  else if (string.toUpperCase()===string){
+  return "YES INDEED!"
+  }
+  else if(string==="I love you, Grandma."){
+  return "I love you, too."
+}
 }
