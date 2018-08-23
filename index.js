744140db984417521f2a16705dc59b26d8121599
Sarah Pai <pai.sarah89@gmail.com>
2018-08-23 00:47:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function whisper(shout){
+  return shout.toLowerCase()
+}
+
 function logShout(hello){
   console.log(hello.toUpperCase())
 }
 
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logWhisper(hello){
-  console.log(hello.toLowerCase())
+function logWhisper(HELLO){
+  console.log(HELLO.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-  if(string == "I love you, Grandma."){
-  return 'I love you, too.'
-  } else if (string == string.toUpperCase()){
+  if(string === string.toLowerCase()){
+    return 'I can\'t hear you!'
+  } else if( string === string.toUpperCase()){
     return 'YES INDEED!'
-  } else{
-  return 'I can\'t hear you!'
+  } else if (string === "I love you, Grandma.") {
+    return 'I love you, too.'
   }
 }
