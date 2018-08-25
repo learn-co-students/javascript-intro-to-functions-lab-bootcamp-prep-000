1089f82c3919eb10d42582b146ff8b13f141e407
Luis Zabala <luis.zabala076@gmail.com>
2018-08-25 13:41:33 +0000

    Intro to func lab1 not working

---
+++ index.js
@@ -0,0 +1,4 @@
+function shout(Hello) { 
+ return
+ Hello.toUpperCase()
+}
--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,9 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
-
+function shout(string) {
+  return
+  Hello.toUpperCase()
+}
+ 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
