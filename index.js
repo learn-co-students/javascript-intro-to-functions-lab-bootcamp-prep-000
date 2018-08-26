7898106daa631f6d3ba1d17c10de978d5643c2e8
demian sims <demiansims@gmail.com>
2018-08-26 02:29:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ function whisper(string) {
   return string.toLowerCase()
     }
 function logShout(string) {
-  console.log.restore()
+  console.log('HELLO')
 }    
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return ("I can't hear you!")
+  }
+  if (string.toUpperCase() === string) {
+    return ("YES INDEED!")
+   }
+   if (string === "I love you, Grandma.") {
+    return ("I love you, too.") 
+   }
+     
+} 
+
