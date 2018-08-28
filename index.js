735cd86c070c11d5bcd12508ecbb56ca26f17120
nickreit@icloud.com <nickreit@icloud.com>
2018-08-28 19:38:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('HELLO')
+}
+
+function logWhisper() {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (string === "hello")
+  return ("I can't hear you!")
+  else if (string === "HELLO")
+  return ("YES INDEED!")
+  else if (string === "I love you, Grandma.")
+  return ("I love you, too.")
+      }
