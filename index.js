43b0ca28340a36eb63a75fb20f22c5b7bf80cd47
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-28 18:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function logWhisper(string){
   console.log(whisper(string))
 }
 function sayHiToGrandma(string){
-  if (string===whisper(string))
-  return ('I can\'t hear you!');
-  else if (string===shout(string))
-  return ('YES INDEED!');
-  else if (string==='I love you, Grandma.')
-  return ('I love you, too.');
-  else
-  return('you\'re the best grandma');
+  if (string===whisper(string)){
+  return ('I can\'t hear you!')}
+  else if (string===shout(string)){
+  return ('YES INDEED!')}
+  else if (string==='I love you, Grandma.'){
+  return ('I love you, too.')}
+  else{
+  return('you\'re the best grandma')}
 }
