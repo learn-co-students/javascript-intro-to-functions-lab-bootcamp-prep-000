7e19bab05f48fe6bfbaa79befedc6deb78d05dc2
boodieroneil@gmail.com <boodieroneil@gmail.com>
2018-08-29 20:11:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  'Hello!'.toUpperCase()
+  return string.toUpperCase()
+}
+function whisper(string) {
+  'HELLO'.toLowerCase()
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO")
+}
+function logWhisper(String) {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+  return "I can't hear you!" 
+  }
+function sayHiToGrandma(string) {
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+        }
+}
