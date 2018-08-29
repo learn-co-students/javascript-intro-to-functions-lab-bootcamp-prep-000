e1f9f973ab55872a5d173389f09553509c4917d1
Arti M <artismarti@gmail.com>
2018-08-29 21:34:08 +0000

    Done.

--- index.js
+++ index.js
@@ -19,11 +19,3 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
 }
-
-/*
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-*/
