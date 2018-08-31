98904070943bcad7f19c82303c80362a32baa85b
Cara D <ephemere@protonmail.com>
2018-08-31 03:41:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase()
+  var uppercase = string.toUpperCase()
+
+  if (string == "I love you, Grandma.") {
+    return "I love you, too." 
+  } else if (string === lowercase ) {
+    return "I can't hear you!"
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+}
