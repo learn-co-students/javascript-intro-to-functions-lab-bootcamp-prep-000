fdaa0c35a133fa7c9f443198428ab66e5255761d
Jubair Hossain <jubair.hossain28@gmail.com>
2018-08-31 22:47:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('Hello'.toUpperCase() )
+}
+function logWhisper(string) {
+  console.log('Hello'.toLowerCase() )
+}
+function sayHiToGrandma(string) {
+  return 'YES INDEED!'.toUpperCase()
+}
+sayHiToGrandma('YES INDEED!')
+
+function 
