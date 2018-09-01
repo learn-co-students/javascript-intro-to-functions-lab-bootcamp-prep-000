9743829ccf607d48f5fbdf3eee3060365db99408
l.l.wijker@gmail.com <l.l.wijker@gmail.com>
2018-09-01 19:05:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+var string = "hello"
+if (string.toLowerCase() === lowercase)
+return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
+var uppercase = "HELLO"
+if (uppercase.toUpperCase() === uppercase)
+return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+ var phrase = "I love you, Grandma"
+ if(phrase  === phrase)
+ return "I love you, too."
+}
