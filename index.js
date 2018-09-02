3b6e6f00c6403c844394249d810cf7e1d9b26595
Julian Brown <julianxbrown@gmail.com>
2018-09-02 18:52:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase 
+
+var lowerCase = 'hello!'
+
+lowercase.toLowerCase() === lowercase 
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
+
