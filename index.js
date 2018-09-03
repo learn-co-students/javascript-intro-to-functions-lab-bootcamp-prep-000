534b8086cd12b9a5321277039587d72f6586144e
dwolfe1987@gmail.com <dwolfe1987@gmail.com>
2018-09-03 23:19:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 function shout(string) {
-  return string.toUpperCase()
-}
-function whisper(string) {
-  return string.toLowerCase()
-}
-function logShout(string) {
-  console.log(string.toUpperCase())
-}
-function logWhisper(string) {
-  console.log(string.toLowerCase())
+  return
+string.toUpperCase()
 }
 
-function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string)
-  {
-  return 'I can/'t hear you!';
-  }
-}
