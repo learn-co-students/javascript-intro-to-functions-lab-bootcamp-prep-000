1bc37ff850d217fcc4ce83dabfd916efff0ee347
manon-pilaud <mandyypilaud@gmail.com>
2018-09-03 18:25:53 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -23,7 +23,7 @@ describe('logShout(string)', function() {
   })
 })
 
-describe('logWhisper(string)', function() {
+describe('logShout(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
