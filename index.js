ab31fa096fa6e29036f0ebdfc2559acfbf085e16
Makeba Webster <makebaw@hotmail.com>
2018-09-06 17:11:34 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ function shout(string) {
   function whisper(string) {
     return string.toLowerCase()
   }
-  function sayHiToGrandma (string) {
-    return string()
+  function logShout(string) {
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase())
+  }
+  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) 
+    return "I can't hear you!"
+  
+  if (string.toUpperCase() === string)
+  return "YES INDEED!"
+  
+  if (string === "I love you, Grandma.")
+    return "I love you, too."
   }
