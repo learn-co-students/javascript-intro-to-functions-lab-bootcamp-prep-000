65cc065da97030084c0f4af6ca256208b1a44fa7
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-07 18:57:48 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string) {
  return "I can't hear you!" }
  if (string === string.toUpperCase()) {
    return "YES INDEED!"}
-  if  (string = "I love you, Grandma.") {
+  if  (string === "I love you, Grandma.") {
   return "I love you, too." }
 }
