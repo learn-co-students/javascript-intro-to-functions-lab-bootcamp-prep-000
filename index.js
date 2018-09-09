addf552ee3b85435038d7b2b51e19ef8e73d4da2
Todd R Gilbert <trgamma@yahoo.com>
2018-09-09 04:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can\'t hear you!";
+    return "I can't hear you!";
   }
-  else if (string.toUpperCase() === string) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!";
   }
-  else if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
