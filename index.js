43cc1b83a4130d7eec02e50403b92126e7cb7a0b
MARY <mcruz2946@gmail.com>
2018-09-09 02:30:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+('hello, world');
+describe('shout(string)',()=>{})
+'Hello!.toUpperCase()// 'HELLO!'
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase()=== uppercase//true
+
+var lowrcase = 'hello!'
+
+lowercase.toLowerCase() === lowercase //
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toUpperCase() === mixedCase // false
