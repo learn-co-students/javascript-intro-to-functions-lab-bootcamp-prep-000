1ee157a81896e969093d1fc996192408a5ef0a86
Jarrod Dyer <jarrod@gasganno.com>
2018-09-10 22:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can't hear you!"
+}
+  else if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+}
+  else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
+}
+
+/*function shout(string) {
  return string.toUpperCase()
 }
 
@@ -23,4 +51,4 @@ function sayHiToGrandma(string) {
  } else if (text === "I love you, Grandma."){
    return "I love you, too."
  }
-}
+}*/
