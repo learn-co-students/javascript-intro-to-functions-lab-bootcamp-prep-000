27c156720c3905a2613f904306aaec0eb23b779f
Jeff Carter <jeff@pepperfly.com>
2018-09-12 17:28:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase) {
+      return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+  }
+    if (string === "I love you, Grandma.") {
+      return "I love you, too.";
+  }
+}
+
+console.log(shout("hello"));
+console.log(whisper("WHAT'S UP!"));
+console.log(sayHiToGrandma("hi, grandma"));
+console.log(sayHiToGrandma("HEY'YA, GRAMMS!"));
+console.log(sayHiToGrandma("I love you, Grandma."));
