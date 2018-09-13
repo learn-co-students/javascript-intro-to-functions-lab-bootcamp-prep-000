72929e219d1f187a7d0ec91cb5bccab3f26ba5f4
Paul Clark <clark.paul123@gmail.com>
2018-09-13 11:08:14 +0000

    Done.

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
   else if (string === uppercase)
   return "YES INDEED!"
-  else
+  else if (string === "I love you, Grandma.")
   return "I love you, too."
 }
