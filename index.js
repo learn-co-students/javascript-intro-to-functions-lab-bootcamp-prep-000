127927881b086383c315c283334b1397480d089a
Lucile Meunier <meunier.lucile531@gmail.com>
2018-09-14 17:31:55 +0000

    Done.

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ function sayHiToGrandma(string) {
   return "YES INDEED!";
   else if (string === "I love you, Grandma.")
   return "I love you, too.";
+  console.log(string)
+  return string.toUpperCase()
 }
