828dfd8159be63cdf6f5afefad737d7230c5cea9
Rosen Toshev <toshev@usc.edu>
2018-09-15 01:50:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(spy) {
+  console.log(spy.toUpperCase())
+}
+
+function logWhisper(spy) {
+  console.log(spy.toLowerCase())
+}
