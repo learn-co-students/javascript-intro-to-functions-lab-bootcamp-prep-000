e799e84a93d918690dff13ca15fa7dc894ac7172
Nasser Sanou <techman@Nassers-MacBook-Pro.local>
2018-09-14 23:21:37 -0400

    SayHi completed

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-test
+
+// TESTING
+const call = (string) => {
+  string.toUpperCase();
+}
+
+call('hello world');
+
+const shout = (string) => {
+  return string.toUpperCase();
+};
+shout("Hello world again")
+
+
+const add = ( a , b) => {
+  return a + b
+}
+
+add(1,2);
+
+console.log(typeof add()) // undefined
+
+
+// LAB WORK
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
