c4bb87ddc5290ee2feb9a652ca8a04eecff559f5
Rosen Toshev <toshev@usc.edu>
2018-09-15 13:55:42 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(spy) {
 function logWhisper(spy) {
   console.log(spy.toLowerCase())
 }
+
+function sayHiToGrandma(string) { 
+var lowerCase = string.toLowerCase; 
+var upperCase = string.toUpperCase; 
+if(string.toLowerCase === lowerCase) { 
+  return("I can\'t hear you!") 
+} else if(string.toUpperCase === upperCase) { 
+  return("YES INDEED!")
+  } else if (string === "I love you, grandma"){ 
+    return("I love you, too.") 
+  } 
+}
