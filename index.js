cdec3c1cd0863fb8174495a417cf0fe23a07f224
Gigi M <mmgamao1@gmail.com>
2018-09-15 05:37:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function shout(string){
   return String.toUpperCase()
 }
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
