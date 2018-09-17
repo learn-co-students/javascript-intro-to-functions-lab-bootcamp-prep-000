a7319ec31eca41b569c963ce4c8a8b8c8c41fb41
Brenden SJ <sj50179@gmail.com>
2018-09-17 21:09:13 +0000

    Done.

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-    console.log("HELLO")
+    console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-    console.log("hello")
+    console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
