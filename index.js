83530a91fc32a2d10049158e212e25eec471c0c3
manon jacquin <manon.jacquin@gmail.com>
2018-09-18 15:39:02 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  // Verify if string is lower case
   if(string.toLowerCase() === string){
-    return "I can\'t hear you!";
+    return "I can't hear you!";
+    // Verify if string is upper case
   } else if (string.toUpperCase() === string){
     return "YES INDEED!";
+    // Verify if string is equal to an exact string
   } else if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
