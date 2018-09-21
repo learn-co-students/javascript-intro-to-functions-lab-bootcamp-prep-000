0e9079bfd02e7561cdab93e2c53b81058f79ffda
Rachel Lum <rlum894@gmail.com>
2018-09-21 23:54:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+function shout(string) {
+ return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string)
+  if (string === uppercase) {
+    return string
+  }
+  else
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log(string)
+  return string.toLowerCase()
+}
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase === uppercase
+
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase()=== mixedCase
+
+
+function sayHiToGrandma(string) {
+  console.log(string)
+  if (string === lowercase) {
+    console.log("I can't hear you!")
+  return "I can't hear you!"
+  }
+  else if (string === uppercase) {
+    console.log("YES, INDEED!")
+    return "YES, INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    console.log("I love you, too")
+    return "I love you, too."
+  }
+  else {
+    return string
+  }
+  
+}
