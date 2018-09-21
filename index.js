e1773506a599921617f89f127739d038a3419d6d
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-21 21:03:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+ return string.toUpperCase()
+  }
+function whisper(string) {
+  return string.toLowerCase()
+} 
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  console.log('hello')
+  return string.toLowerCase ("I can't hear you!")
+function sayHiToGrandma(string) {
+  console.log('HELLO')
+  return string.toUpperCase ("YES INDEED!")
+}
+function sayHiToGrandma(string) {
+  console.log("I love you, Grandma.")
+  return("I love you, too.")
+}
+
