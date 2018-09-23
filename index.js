b914cb679db80cb0a5f37cd798832ca31f097c60
M H <ptcb795@gmail.com>
2018-09-23 21:00:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) { return string.toUpperCase()}
+var uppercase="HELLO!"
+uppercase.toUpperCase()===uppercase//true
+var lowercase='hello!'
+lowercase,toLowerCase()
+===lowercase// true
+
+var mixedCase='Hi there!'
+var mixedCase='Hi there!
+mixedCase.toLowercase() ===mixedCase//false
+mixedCase.toUpperCase() ===mixedCase//false
