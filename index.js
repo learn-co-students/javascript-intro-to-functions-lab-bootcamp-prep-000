c81ac3a56c83844760dd55734bb4845c7c255a62
Rachel Lum <rlum894@gmail.com>
2018-09-24 17:17:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-function shout(string) {
- return string.toUpperCase()
-}
 
-function whisper(string) {
-  return string.toLowerCase()
-}
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
 
-function logShout(string) {
-  console.log(string)
-  if (string === uppercase) {
-    return string
-  }
-  else
-  return string.toUpperCase()
-}
-
-function logWhisper(string) {
-  console.log(string)
-  return string.toLowerCase()
-}
-
-var uppercase = "HELLO!"
-uppercase.toUpperCase === uppercase
-
-var lowercase = 'hello!'
+var lowercase = 'hello'
 lowercase.toLowerCase() === lowercase
 
 var mixedCase = 'Hi there!'
@@ -33,15 +12,31 @@ mixedCase.toLowerCase() === mixedCase
 mixedCase.toUpperCase()=== mixedCase
 
 
+function shout(string) {
+ return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(`string`.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(`string`.toLowerCase)
+}
+
+
 function sayHiToGrandma(string) {
-  console.log(string)
   if (string === lowercase) {
     console.log("I can't hear you!")
   return "I can't hear you!"
   }
   else if (string === uppercase) {
-    console.log("YES, INDEED!")
-    return "YES, INDEED!"
+    console.log("YES INDEED!")
+    return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
     console.log("I love you, too")
