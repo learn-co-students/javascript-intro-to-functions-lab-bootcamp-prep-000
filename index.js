671b0541416ec2f6d6f20aba4d84279e7819b5f9
Rachel Lum <rlum894@gmail.com>
2018-09-25 01:09:01 +0000

    Done.

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(`string`.toUpperCase)
+  console.log(`${string.toUpperCase()}`)
 }
 
 function logWhisper(string) {
-  console.log(`string`.toLowerCase)
+  console.log(`${string.toLowerCase()}`)
 }
 
 
