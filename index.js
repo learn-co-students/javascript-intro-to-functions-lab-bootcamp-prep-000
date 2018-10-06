db90effcb454057bd5372b15001cc7e544b0633a
florah001 <florah001@yahoo.co.uk>
2018-10-06 15:20:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,56 @@
+function shout(string) {
+  return string.toUpperCase("YES INDEED");
+}
+function whisper(string) {
+  return string.toLowerCase("hello");
+} 
+function sayHiToGrandma(string) {
+  return string.toUpperCase("YES INDEED");
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!");
+}
+function sayHiToGrandma(string) {
+  console.log("I can't hear you!");
+}
+function sayHiToGrandma() {
+ console.log("hello");
+}
+function sayHiToGrandma(string) {
+  console.log("YES INDEED");
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I love you, too.");
+}
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma(string)=== hello);
+}
+function logShout(string) {
+  console.log("HELLO");
+}
+function logWhisper(string) {
+  console.log('hello');
+}
+
+function sayHiToGrandma(string) {
+if (string === "hello".toLowerCase()) {
+  console.log("I can't hear you!");
+  
+} else if (string === string.toUpperCase()){
+  console.log("YES INDEED")
+  
+} 
+}
+if (sayHiToGrandma === ("i love you, Grandma".toLowerCase)) {
+console.log("i can't hear you")
+}
+
+/*else if ("I love you, Grandma" === string.toLowerCase()) {
+  console.log("I love you, too.")  
+}*/
+
+
+
+
+
+
