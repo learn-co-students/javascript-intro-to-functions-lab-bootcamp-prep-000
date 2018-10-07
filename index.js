1f60716e56581b00b07304bd3f59d42167ecece5
elisa stater <elisa_60@msn.com>
2018-10-07 21:07:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string){ 
+return string.toUpperCase('HELLO')
+}
+
+function whisper(string){
+return string.toLowerCase('hello')
+}
+
+function logShout(string) {
+ console.log(`HELLO`)
+ 
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+
+
+
+function sayHiToGrandma(string) {
+if(string===toLowerCase()) 
+return 'I can\'t hear you!';
+}
+if (string.toUpperCase===('HELLO')); {  
+return string.toUpperCase ('YES INDEED!');
+}
+if (string===('I love you, Grandma.')); {  
+return 'I love you, too.';
+}
+ 
+ 
+  
+  
