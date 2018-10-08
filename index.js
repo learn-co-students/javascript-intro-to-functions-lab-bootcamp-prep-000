0408ce6d56c6ec94602329c2e32b2c5321075f7c
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-08 23:47:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(text){
+  return text.toUpperCase()
+}
+
+function whisper(text){
+  return text.toLowerCase()
+}
+
+function logShout(text){
+  console.log(text.toUpperCase())
+}
+
+function logWhisper(text){
+  console.log(text.toLowerCase())
+}
+
+function sayHiToGrandma(text){
+  if (text.toLowerCase === text){
+    return "I can\'t hear you!"
+  } else if (text.toUpperCase === text){
+    return "YES INDEED"
+  } else if ("I love you, Grandma."){
+    return "I love you, too."
+  }
+}
