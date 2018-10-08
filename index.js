deee3a4db2d09979295ee3937e33d57cc0c0a0ee
Daniel Glover <iamdanielglover@gmail.com>
2018-10-08 11:03:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logshout(string) {
+  string.toUpperCase();
+  console.log(string);
+}
+
+function logwhisper(string) {
+  string.toLowerCase();
+  console.log(string);
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase());
+  return("I can't hear you!");
+  } else {
+    if (string === string.toUpperCase());
+  return("YES INDEED!");
+}
+
+
