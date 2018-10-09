d0b1bd866942efdb9044bc6a5d6f027db39f1531
Leah Guecia <lguecia@gmail.com>
2018-10-09 17:24:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(toUpperCase);
+  return string.toUpperCase()
+}
+lowercase.toLowerCase()===lowercase;
+function logWhisper(string) {
+  console.log(toLowerCase);
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  lowercase.toLowerCase()===lowercase
+  return ("I can't hear you!")
+}
+function sayHiToGrandma(string){
+  uppercase.toUpperCase()===uppercase
+  return ("YES INDEED!")
+}
+function sayHiToGrandma(string){
+  var word = "I love you, Grandma.;"
+  return ("I love you, too.;")
+}
