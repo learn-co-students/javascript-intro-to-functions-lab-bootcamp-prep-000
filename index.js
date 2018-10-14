1f91b9cbc0779c061381d729b264c4014c50b953
Jody Neckles <jody@technative.tv>
2018-10-14 14:48:05 +0000

    Done.

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase('spy'))
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase('spy'))
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  if(string == string.toUpperCase()) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma.") {
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
