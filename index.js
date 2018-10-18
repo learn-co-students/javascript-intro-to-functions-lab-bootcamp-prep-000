57b3ae994d4f556dc904b698131730c5ad62c245
Brooke Gill <brooke.gill25@gmail.com>
2018-10-18 02:31:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if (whisper(string) === string) {
+    return "I can\'t hear you!";
+  } else if (shout(string) === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+
+// Test
+console.log(sayHiToGrandma("hi"))
+console.log(sayHiToGrandma("HI"))
+console.log(sayHiToGrandma("I love you, Grandma."))
