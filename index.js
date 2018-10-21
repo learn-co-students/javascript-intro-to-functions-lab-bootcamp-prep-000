893e1b6123099364363f27855f558399ededf225
David Sollesse <davidsollesse@gmail.com>
2018-10-21 22:29:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+if(string.toLowerCase() === string)
+  return('I can\'t hear you!')}
+else {(string.toUpperCase() === string)
+  return "YES INDEED!"}
+else if (true) {(string = 'I love you, Grandma.')
+  return "I love you too"}
+}
+//The first two work fine! For some reason the third one just won't work in whichever form.
+//Will resubmit later
+
+//else if (true) {
+  //(string.toUpperCase() === string)
+//return 'YES INDEED!'
+//}
+//else if (true) {
+//  (string === 'I love you, Grandma.')
+//return "I love you too"
+//}
