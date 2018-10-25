94ae6651349151a1315a695631a013c56ecaa4a5
Angie Quin <aquinones100@gmail.com>
2018-10-25 16:14:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase
+}
+
+function logShout(string) {
+  console.log() string.toUpperCase
+}
+
+function logWhisper(string) {
+  console.log() string.toLowerCase
+}
+
+function sayHiToGrandma(string) {
+  if ((string)===uppercase)
+  return "YES INDEED!"
+}
+else if ((string)===lowercase){
+  return "I can't hear you!"
+}
+else if ((string)==="I love you, Grandma.") {
+  return "I love you, too."
+}
+}
+}
