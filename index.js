4f78ca888dece772c46d1e2ff3cac2d40671b3d9
Brooke Gill <brooke.gill25@gmail.com>
2018-11-09 00:48:52 +0000

    Done.

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string) {
   if (whisper(string) === string) {
     return "I can't hear you!";
 
-  } else (shout(string) === string) {
+  } else if (shout(string) === string) {
     return "YES INDEED!";
 
-  } else (string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
