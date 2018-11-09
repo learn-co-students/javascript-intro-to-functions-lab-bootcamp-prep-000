7c6f00052c75cff22bf8d7711fa3864d98fe23fe
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-09 01:23:08 +0000

    Done.

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-/*
+
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
-  } elseif (string === lowercase) {
+  }
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else {
+  }
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-*/
+}
