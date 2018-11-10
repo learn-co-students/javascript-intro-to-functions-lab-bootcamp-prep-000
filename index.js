f0245a8c34c6ac52a9d4d453a5b43cee699aa51b
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-10 22:32:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+const string;
+
+function sayHiToGrandma(string){
+  return string.toUpperCase()
+}
+"hello!".toUpperCase();
+
+var string = "HELLO!";
+
+string.toUpperCase()=== string;
+
+var lowercase = 'hello!';
+lowercase.toLowerCase() === lowercase;
