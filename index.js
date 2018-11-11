00e866abf21da3aad648a88fcac896772f368230
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-11 22:18:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+ function shout(string) {
+  return string.toUpperCase()
+}
+
+function  whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+ if else ("I can't hear you!") console.log(string.toUpperCase) 
+} 
--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,8 @@
-function shout(string) {
-  return string.toUpperCase()
-}
+
 describe('shout(string)', function() {
  it('receives one argument and returns it in all caps'), function() {
-    expect(shout('hello')).toEqual('HELLO') }
+     expect(shout('hello')).toEqual('HELLO') }
 })
-
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
