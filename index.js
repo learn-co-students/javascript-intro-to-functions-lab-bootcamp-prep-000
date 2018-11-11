fd0f15f52a13c7cdec8093ec421e12545b12e252
Stuart Sewell <sewelly@gmail.com>
2018-11-11 21:44:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+// function logShout(string){
+//   spy(console.log(string))
+// }
+
+function logShout(string){
+  var uppercase = string.toUpperCase()
+  string = uppercase
+  console.log(uppercase)
+}
+
+function logWhisper(string){
+  var lowercase = string.toLowerCase()
+  string = lowercase
+  console.log(lowercase)
+}
+
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){
+    return 'I can\'t hear you!'
+  } else if (string.toUpperCase() === string){
+    return 'YES INDEED!'
+  } else if (string === 'I love you, Grandma.'){
+    return 'I love you, too.'
+  }
+}
