3c41e95cdcf09354085d7cfc9793753dec46526b
Carell Hamil <ayannahamil@gmail.com>
2018-11-15 17:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!"
   }
-  else if (string === string.toUpperCase()){
+  if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
