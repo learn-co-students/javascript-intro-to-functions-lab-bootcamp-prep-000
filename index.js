ddec4397a6306f006322fdf30aa87cefb89c3d30
Ted Sazon <tedsazon@gmail.com>
2018-11-15 02:22:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(`${string.toUpperCase()}`)
+}
+function logWhisper(string) {
+  console.log(`${string.toLowerCase()}`)
+}
+function sayHelloToGrandma('hello') {
+  return hello
+}
+function sayHelloToGrandma('HELLO') {
+  return HELLO
+}
+function sayHelloToGrandma ("I love you, Grandma") {
+  return I love you, Grandma
+}
