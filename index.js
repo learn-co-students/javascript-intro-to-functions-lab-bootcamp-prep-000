254bb6c9b6ec1a1e0ae97ffb75a728d9f33037bc
zenryoku <zenryoku@hotmail.com>
2018-11-19 02:03:27 +0000

    Done.

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
