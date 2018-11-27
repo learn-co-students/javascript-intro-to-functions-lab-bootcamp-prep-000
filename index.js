d42667488aba8422bbb6f7d2103ae153c102ef88
Paris Dolfman <parisdolfman@gmail.com>
2018-11-27 19:52:08 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string)
- if (condition === condition) {
-   block
+function sayHiToGrandma(string) {
+ if (string === string.toLowerCase()) {
+   return "I can't hear you!"
+ }
+
+if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+}
+
+if (string === "I love you, Grandma.")
+  return "I love you, too."
  }
