a53a875bd6d873f4b7a888c8baf51e72641b6dae
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-11-30 22:40:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  
+  if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+   if (string.toUpperCase() === string){
+    return "YES INDEED!";
+  }
+   if (string.toLowerCase() === string){
+    return "I can't hear you!";
+  }
+}
+
+sayHiToGrandma("I love you, Grandma.");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("hi");
