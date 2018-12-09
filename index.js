33bb8c3229a69fdafc21eae1970af3d4d778a65f
David Krieger <davidjkrieger@gmail.com>
2018-12-09 01:57:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string){
+  return "hello".toUpperCase();
+}
+
+function whisper(string){
+  return "HELLO".toLowerCase();
+}
+
+function logShout(string){
+  console.log(shout("hello"));
+}
+
+function logWhisper(string){
+  console.log(whisper("HELLO"));
+}
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO!";
+  uppercase.toUpperCase() === uppercase;
+  return "YES INDEED!";
+}
+
+sayHiToGrandma("HELLO!");
+
+function sayHiToGrandma(string){
+  var lowercase = "hello!";
+  lowercase.toLowerCase() === lowercase; // true
+  return "I can\'t hear you!";}
+
+sayHiToGrandma("hello!");
+
+function sayHiToGrandma(string){
+
+  
+  return "I love you, too."}
+
+sayHiToGrandma("I Love you, Grandma.");
