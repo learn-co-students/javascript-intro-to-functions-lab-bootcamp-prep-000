308455fa561ae0cf9e2714d65de0be1422b8fce1
Andrew Turner <andrewturner227@gmail.com>
2018-12-11 15:19:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+global.expect = require('expect');
+
+const babel = require('babel-core');
+const jsdom = require('jsdom');
+const path = require('path');
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log(shout(string));
+}
+
+function logWhisper(string) {
+    console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return 'YES INDEED!';
+    } else {
+        return 'I love you, too.';
+    }
+}
