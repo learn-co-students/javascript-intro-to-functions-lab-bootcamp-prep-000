ae4b4777c7a7f080561ca18755574eb838f26294
Begum S. Tuyen <begumtuyen@gmail.com>
2018-12-17 21:44:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+var string = "HELLO"
+var lowercase = string.toLowerCase()
+var uppercase = string
+function sayHiToGrandma(string){
+  if (string === lowercase ) {
+  return "I can\'t hear you!"
+} else if (string === uppercase ) {
+  return ("YES INDEED!")
+} else {
+  return "I love you, too."
+}
+}
