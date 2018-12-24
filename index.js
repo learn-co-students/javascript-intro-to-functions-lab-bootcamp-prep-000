fe1251960ab7c6c36f7dbbbda0bd61c444fff452
rnavedojr <rnavedojr@outlook.com>
2018-12-24 04:25:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout() {
+  console.log('HELLO')
+}
+
+function logWhisper() {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (string = string.toLowerCase()) {
+    return "I can\'t hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string = string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string = "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
