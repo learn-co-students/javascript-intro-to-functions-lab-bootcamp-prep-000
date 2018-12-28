aece78698fdf0363f5371e9cae93e75b75ea758c
Eyal Toledano <eyaltoledano@me.com>
2018-12-28 21:48:20 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  var lowercase = "hello"
-  var uppercase = "HELLO"
-  if (string === lowercase) {
+  if (string === string.toLowerCase()) {
     return("I can't hear you!")
   }
-  else if (string === uppercase) {
+  else if (string === string.toUpperCase()) {
     return("YES INDEED!")
   }
   else if (string === "I love you, Grandma.") {
     return("I love you, too.")
   }
+  return ("Are you eating enough my dear?")
 }
