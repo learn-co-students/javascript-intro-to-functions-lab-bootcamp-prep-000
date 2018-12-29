bcf181787acaef3bca46c382eae491ee087ea69e
ADE AHMID <adewale50@yahoo.co.uk>
2018-12-29 10:21:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function shout("hello") {
+  return("hello").toUpperCase(HELLO)
+}
+function whisper("HELLO") {
+  return("HELLO").tolowercase("hello")
+}
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase // true
