d8ec5951759d93a8ec709192668d2acb02ee3a09
Michael Maggs <michaelmaggs@gmail.com>
2019-01-03 20:40:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if string = string.toLowerCase() console.log("I can't hear you")
+  if string = string.toUpperCase() console.log("YES INDEED!")
+  if string = "I love you, Grandma." console.log("I love you, too.")
+}
