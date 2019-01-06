5bab679bdc38c13ab0d3b917f2e7fc3150bae566
Phillip Headen <pheaden15@gmail.com>
2019-01-06 19:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ function whisper(string)
   return string.toLowerCase()
 }
 
-function shout(string)
+function logShout(string)
 {
   console.log('HELLO')
+}
+
+function logWhisper(string)
+{
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
