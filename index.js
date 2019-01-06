3dc8321d7d5cb5d10d983fefb9f33cbf050f3c3f
Ben Manekin <ben.manekin@gmail.com>
2019-01-06 19:18:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+  return(string.toUpperCase())
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function whisper(string) {
+  return(string.toLowerCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+  if (string === uppercase) {
+    return ("YES INDEED!")
+  }
+  else {
+    return ("I can\'t hear you!")
+  }
+}
+
+var uppercase = "HELLO"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello!"
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = "Hi there!"
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
