f7c89b5af6436ea36fa690b67ffaeba01b9bdf26
John Hill <john.m.hill91@gmail.com>
2019-01-09 05:04:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(STRING) {
+  return STRING.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  return "I can\'t hear you!"
+}
+function sayHiToGrandma(string){
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string){
+  return "I love you, too."
+}
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase()){
+ return "I can\'t hear you!"
+  }
+ else if (string===string.toUpperCase()){
+  return "YES INDEED!"
+ }
+  else if(string==="I love you, Grandma."){
+  return"I love you, too."
+  }
+}
