accf32e38ef0cf5ddb00cd1779a075bfc389503e
Rita Musello-Kelliher <ritamkell@gmail.com>
2019-01-16 02:40:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase
+}
+
+function sayHiToGrandma(arg) {
+  if (arg.toLowerCase() === arg) {
+  console.log("I can't hear you!") 
+  return "I can't hear you!"
+  } else if (arg.toUpperCase() === arg) {
+    return "YES INDEED!"
+    console.log("YES INDEED")
+  } else if (arg === "I love you, Grandma.")
+  return "I love you, too."
+  console.log("I love you, too.")
+  } 
+  
+
