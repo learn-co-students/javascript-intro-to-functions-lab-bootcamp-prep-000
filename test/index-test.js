34819aeebb00127c0a60ee256b88f15ec8888246
cesar mota <cesarmota@ymail.com>
2019-01-16 21:12:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -11,12 +11,12 @@ describe('whisper(string)', function() {
   })
 })
 
+
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
-
     expect(spy).toHaveBeenCalledWith('HELLO')
 
     console.log.restore()
