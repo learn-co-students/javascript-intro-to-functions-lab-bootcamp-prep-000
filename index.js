42a0241229278d435e298623a82b88393c898e1b
Renée Cruz <reneebasscruz@gmail.com>
2019-01-16 07:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ function whisper(string)
     return string.toLowerCase()
 }
   
-  function logShout(string)
+function logShout(string)
     {
       console.log(string.toUpperCase())
 }
 
-function logWhisper(string)
-  {
+function logWhisper(string) {
     console.log(string.toLowerCase())
 }
 
-
-function sayHiToGrandma(string)  
-  {
-    
-     "YES INDEED!" return "I can\'t hear you!"
-    
-     lowercase.toLowerCase === lowercase
-     return "I can't hear you!"
-    
-     sayHiToGrandma(string) === sayHiToGrandma("I love you, Grandma")
-     return "I love you, too."
+function sayHiToGrandma(string) {
+      if (string.toLowerCase() === string) {
+      return "I can\'t hear you!"
+      }
+      
+      if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+      }
+      
+      if (string === "I love you, Grandma.") {
+      return "I love you, too."
+      }
 }
+
