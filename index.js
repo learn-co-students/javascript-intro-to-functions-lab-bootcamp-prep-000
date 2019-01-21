1079dbef7fe68961a1f8890f5a81f1264d01ab70
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-21 16:38:30 +0000

    Done.

--- index.js
+++ index.js
@@ -7,48 +7,22 @@ function whisper(string) {
 }
 
 function logShout(string) {
-
   console.log(string.toUpperCase())
 }
 
-function logWhisper (string) {
-  console.log(string.toLowerCase())
+function logWhisper(string){
+  console.log (string.toLowerCase())
 }
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return ("I can't hear you!")
+  }
 
-function sayHiToGrandma (string) {
- 
-if (string === string.toLowerCase()) {
-  return ("I can't hear you!")
-} else if  (string === string.toUpperCase()) 
-  {return ("YES INDEED!")
+else if (string === string.toUpperCase()) 
+{return ("YES INDEED!")
 }
-else if (string = "I love you, Grandma.") 
-  {return ('I love you, too.')
-
-
+else if (string === "I love you,  Grandma!"){
+  return "I love you,  too."
 }
-
 }
-
-
-
-
-
-
-
-
-
