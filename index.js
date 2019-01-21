8deb49aef5b69af45c64a5d02b40e46a75cfd424
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-21 16:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
 else if (string === string.toUpperCase()) 
 {return ("YES INDEED!")
 }
-else if (string === "I love you,  Grandma!"){
-  return "I love you,  too."
+else if (string = "I love you, Grandma!"){
+  return "I love you, too."
 }
 }
