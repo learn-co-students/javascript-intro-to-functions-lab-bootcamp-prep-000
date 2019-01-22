f32791358974493d2e682519a821e5a2ba88e020
Grace Tan <gracetan37@gmail.com>
2019-01-22 10:03:15 +0000

    Done.

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {return 'I can\'t hear you!'}
   if(string.toUpperCase() === string) {return 'YES INDEED!'}
-  if(string === "I love you, Grandma") {return 'I love you, too.'}
+  if(string === "I love you, Grandma.") {return 'I love you, too.'}
 }
