db15efc55e81166f9608cd25d0b9ffe7fd4acbc0
Grace Tan <gracetan37@gmail.com>
2019-01-22 10:02:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) { }
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase() 
+}
+function sayHiToGrandma(string) {
+  console.log(string.toUpperCase())
+}
+function sayHiToGrandma(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+}
+var lowercase = "hello"
+lowercase.toLowerCase() === lowercase
+function sayHiToGrandma(string) {
+  console.log(string.toLowerCase())
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {return 'I can\'t hear you!'}
+  if(string.toUpperCase() === string) {return 'YES INDEED!'}
+  if(string === "I love you, Grandma") {return 'I love you, too.'}
+}
