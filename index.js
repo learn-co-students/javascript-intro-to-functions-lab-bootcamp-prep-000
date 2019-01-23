7eec0bc8e82ce4c96e6dee65363a13fcee2a6fcd
Diana Utan <dia_utzi@yahoo.com>
2019-01-23 18:09:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+  
+  
+ function sayHiToGrandma(string) {
+  // var uppercase = string.toUpperCase();
+  // var lowercase = string.toLowerCase();
+    
+  if ( string === string.toLowerCase()) {
+    return  "I can\'t hear you!"
+    
+  }
+ 
+   if (string === string.toUpperCase()) {
+      return  'YES INDEED!' ;
+   }
+  
+   if ( string === "I love you, Grandma.")  {
+     return "I love you, too.";
+   }
+      
+
+}
