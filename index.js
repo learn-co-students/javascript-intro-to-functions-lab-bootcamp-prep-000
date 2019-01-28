a5dcfcb4bbeb3d687cc5c0f22af58fcb90e2cf80
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 20:09:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+expect(shout('hello')).toEqual('HELLO')
+'Hello!'.toUpperCase()
+function shout(string){return string.toUpperCase()}
+function logshout(string){console.log()}
+
+var uppercase = "HELLO!";
+
+var lowecase = 'hello!';
+
+var mixedCase = 'Hi there!';
+
+'Hello!'.toUpperCase();
+
+function shout('Whisper'){return 'Whisper'.toLowerCase()};
+
+
