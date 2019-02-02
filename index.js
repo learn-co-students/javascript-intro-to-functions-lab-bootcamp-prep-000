3f5a0bca1158b72b393c95e2802a0425448ccbc7
Angela Stead <angie_stead55@yahoo.com>
2019-02-02 16:34:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string)
+{
+return string.toUpperCase();
+
+}
+
+function whisper(string)
+{
+return string.toLowerCase();
+
+}
+
+function logShout(string)
+{
+console.log (string.toUpperCase());
+}
+logShout('Hello');
+
+function logWhisper(string)
+{
+console.log (string.toLowerCase());
+}
+logWhisper('Hello');
+
+function sayHiToGrandma(string)
+{ if (string === 'hello') 
+{
+ return ('I can\'t hear you!');
+} else if (string === 'HELLO') {
+  return ('YES INDEED!');
+} else if (string === "I love you, Grandma.") {
+  return ('I love you, too.');
+}
+}
+sayHiToGrandma('hello');
+sayHiToGrandma('HELLO');
