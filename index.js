cfcf5645f5f3347a08a544828a715ce2a9260cc7
Crystal Wang <crystalmaro@gmail.com>
2019-02-07 10:48:51 +0000

    Done.

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function logWhisper(string) {
   console.log('HELLO'.toLowerCase())
 }
 
-function sayHiToGrandma(string)
+function sayHiToGrandma(string) 
 {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
     return ("I can't hear you!")
-} else if (string === "I love you, Grandma.") {
-    return ("I love you, too.")
-} else {
-    return ("YES INDEED!")
-}
+  } else if (string === string.toUpperCase()) {
+    return ("YES INDEED!") 
+    } else if (string === "I love you, Grandma.") {
+      return ("I love you, too.")
+    }
 }
