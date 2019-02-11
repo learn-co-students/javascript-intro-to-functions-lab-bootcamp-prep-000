ed550df65fd76273edfaa7e9e754f15e1b46d9bf
Richard Dowland <rdowland@hotmail.co.uk>
2019-02-11 16:38:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+var uppercase, lowercase 
+
 function shout(string) {
   
-  return string.toUpperCase()
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
   
 }
 
 function whisper(string) {
   
-  return string.toLowerCase(
-    )
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
+  
 }
 
 function logShout(string) {
   
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase() === uppercase
-  console.log(string)
-  return
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+  
+}
+
+function logWhisper(string) {
+  
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+  
+}
+
+function sayHiToGrandma(string) {
+  
+    if (string.toLowerCase() === string) {
+      return 'I can\'t hear you!';
+      
+    }
+    
+    if (string.toUpperCase() === string) {
+      return 'YES INDEED!';
+      
+    }
+    if (string === 'I love you, Grandma.') {
+      return 'I love you, too.';
+    }
 }
