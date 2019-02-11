6cb06a6eebe6b6645d2e2a0db916c63ae1ac37d0
Richard Dowland <rdowland@hotmail.co.uk>
2019-02-11 16:23:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  
+  return string.toUpperCase()
+  
+}
+
+function whisper(string) {
+  
+  return string.toLowerCase(
+    )
+}
+
+function logShout(string) {
+  
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+  console.log(string)
+  return
+}
