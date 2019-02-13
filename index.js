bbc240b4f6aedc4dabce38346477c70bdae4d406
Hazel Donaldson <hazel90.hd@gmail.com>
2019-02-13 16:06:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  const spy = ['HELLO']
+  console.log(`${[spy]}`)
+}
+
+function logWhisper(string) {
+  const spy = ['hello']
+  console.log(`${[spy]}`)
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase) { 
+    return "I can\'t hear you!"
+  }
+    else if (string.toUpperCase) {
+      return "YES INDEED!"
+    }
+    else if (string == "I love you, Grandma.") {
+      return "I love you, too."
+    }
+    
+
+}
+    
+
+
