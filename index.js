5660e508f854eebf445a63b55c7cf02a16181584
aj.artwil <aj.artwil@gmail.com>
2019-02-14 15:20:02 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function logWhisper (){
   console.log ("hello")
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string.toLowerCase() === string) 
     return "I can't hear you!"
   }
+function sayHiToGrandma(string) {
+  if (string.toUpperCase()=== string)
+    return "YES INDEED!"
+}
