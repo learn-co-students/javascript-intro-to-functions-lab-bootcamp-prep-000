2cfa196fa3aef5db08ca7ed61d58dc5a03141d93
aj.artwil <aj.artwil@gmail.com>
2019-02-14 15:56:26 +0000

    Done.

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ function logWhisper (){
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) 
     return "I can't hear you!"
-  }
-
-  if (string.toUpperCase() === string) {
+}
+  if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-
-  return "Are you eating enough?"
-}
