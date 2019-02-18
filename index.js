4ed8050b1b06b35b517e89240026d686afd24abb
sheila barron <sheilabarron@yahoo.com>
2019-02-18 05:15:57 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,21 @@ function logShout() {
 console.log ('HELLO')
 }
 
+function sayHiToGrandma(string) {
+  console.log (whisper)
+  return ("I can\'t hear you!")
+}
 
-
-function sayHiToGrandma() {
-  console.log ("YES INDEED!")
+function sayHiToGrandma(string) {
+  console.log (shout)
   return ("I love you, too.")
+}
+function sayHiToGrandma(string) {
+  console.log ("I love you, too.")
+  return ("YES INDEED")
   }
   
-  
+function sayHiToGrandma(shout) {
+  console.log ("I love you, Grandma.")
+  return ("I love you, too.")
+}  
