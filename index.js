0607e99f3170f8fabe0f414c02fd6e6bb3179ef7
Kyle Conlon <kyleconlon13@gmail.com>
2019-02-24 19:26:02 +0000

    Done.

--- index.js
+++ index.js
@@ -14,40 +14,17 @@ function logWhisper(spy) {
   console.log('hello')
 }
 
-function sayHiToGrandma(string, lowercase, uppercase) {
-  var lowercase = 'hello'
-  var uppercase = 'HELLO'
-  var mixedcase = 'I love you, Grandma.'
+function sayHiToGrandma(string) {
+  var lowercase = 'hello';
+  var uppercase = 'HELLO';
   
-  if (string.toLowerCase()===lowercase){
-    return "I can\'t hear you!"
+  if (string.toLowerCase()===string){
+    return 'I can\'t hear you!';
   }
-  
-  else if (string.toUpperCase()===uppercase){
-    return "YES INDEED!"
+  if (string.toUpperCase()===string){
+    return 'YES INDEED!';
   }
-  
-  //else if (string===mixedcase){
-    //return "I love you, too."
-  //}
-  
-  
-  
-  
-  //else if (string.toUpperCase()===mixedcase)
-  //else if (string.toLowerCase()===mixedcase){
-    //return "I love you, too."}
+  if (string === 'I love you, Grandma.'){
+    return 'I love you, too.';
+  }
 }
-
-
-//describe('sayHiToGrandma(string)', function() {
-  //it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    //expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  //})
-
-  //it('returns "YES INDEED!" if `string` is uppercase', function() {
-    //expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  //})
-
-  //it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    //expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
