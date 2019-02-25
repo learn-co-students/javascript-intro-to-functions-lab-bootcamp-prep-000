db390d223954efe997c148db9478c04801dcc630
rob cabrera <robert1cabrera@gmail.com>
2019-02-25 01:03:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-Function shout(String)
-{
-return
- String.toUpperCase()
+function shout(hello) {
+  return hello.toUpperCase()
 }
-'Hello!' .toUpperCase()
- // 'HELLO!'
- 
+
+function whisper(hello) {
+  return hello.toLowerCase()
+}
+function logShout(hello) {
+  console.log(hello.toUpperCase());
+}
+function logWhisper(hello) {
+  console.log(hello.toLowerCase());
+}
+
+function sayHiToGrandma(hello) {
+  if (hello.toLowerCase() === hello) {
+    return "I can't hear you!"
+  }
+  if (hello.toUpperCase() ===hello) {
+    return "YES INDEED!"
+    }
+  if (sayHiToGrandma ==="I Love you,Grandma.") {
+   return "I love you, too."
+ }
+
+ return "I love you, too."
+ }
