b9fc21dc4dcda1caea115fbebeb644e6e15f9b6c
Bright Olayinka <explorebright@gmail.com>
2019-02-28 20:12:16 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string){
-  calls console.log(string)
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
+  function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
