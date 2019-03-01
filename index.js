a13eb3b152fe3d8add8b2839b1d22b1e769f5690
esteban zambrano <ezam0000@gmail.com>
2019-03-01 20:01:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
 }
-function whisper(string) {
-  return string.toLowerCase()
+
+ shout("hello");
+
+ function logShout(string){
+  return console.log(string.toUpperCase())
 }
-function logShout(string) {
-  console.log; logShout(string)
+
+ logShout("hello")
+
+ function whisper(string){
+  return string.toLowerCase();
 }
+
+ whisper("HELLO")
+
+ function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+ logWhisper("HELLO")
+
+ function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
