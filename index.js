3e56ecffeb681fef0c164255e00dd9258992b862
Nicholas Stano <nicholasstano@gmail.com>
2019-03-02 15:07:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase //true
-
-var lowercase = 'hello!'
-lowercase.toLowerCase() === lowercase //true
-
-var mixedCase ='Hi there!'
-mixedCase.toLowerCase() === mixedCase //false
-mixedCase.toUpperCase() === mixedCase //false
-
 function shout(string) {
   return string.toUpperCase()
-  
 }
+
 function whisper(string) {
-  return string.toLowerCase()
-  
+  return string.toLowerCase() 
 }
 
 function logShout(string) {
-console.log(string.toUpperCase())
-
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
-  
 }
 
 function sayHiToGrandma(string) {
-if (string === string.toLowerCase()) {
-return "I can't hear you!"
-}
-if (string === string.toUpperCase()) {
-return "YES INDEED!"
-}
-if (string === "I love you, Grandma.") {
-  return "I love you, too." 
-}
-
+  if (whisper(string) === string) {
+    return "I can't hear you!"
+  }
+  else if (shout(string) === string) {
+    return "YES INDEED!"
+  }
+  
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
 
