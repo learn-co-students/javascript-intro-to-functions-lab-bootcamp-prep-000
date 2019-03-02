891445895c8acda13da6ac6da9a85c103ee781e8
Annia Forney <anniaforney@gmail.com>
2019-03-02 20:35:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function whisper(string) {
+ return string.tolowercase()
+}
+console.log(shout) 
+ return string.toUpperCase()
+}
+console.log(whisper) {
+  return string.tolowercase()
+}
+sayHiToGrandma(string) {
+  console.log('I can"t hear you!')
+return string.tolowercase()
+}
+sayHiToGrandma(string) {
+  console.log ('YES INDEED')
+  return string.toUpperCase()
+}
+sayHiToGrandma(string) {
+  console.log ('I love you, Grandma')
+  return string.to ('I love you, too.')
+}
