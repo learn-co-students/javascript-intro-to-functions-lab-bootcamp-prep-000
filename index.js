0f86469f03062730eda7505fee9a6560233796b8
Christopher Cabell <cccabell@icloud.com>
2019-03-02 00:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return ("I can't hear you!")
+    return ("I can't hear you!");
   } else if (string === string.toUpperCase()) {
-    return ("YES INDEED!")
+    return ("YES INDEED!");
   } else if (string === "I love you, Grandma.") {
-    return ("I love you, too.")
+    return ("I love you, too.");
   }
 }
