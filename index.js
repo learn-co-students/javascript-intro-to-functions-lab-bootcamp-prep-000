ca3b6f1581ccdd614a2664f78bbd3f1816ab9352
versatile-buffer-3628 <fattytolani@gmail.com>
2019-03-05 00:36:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return whisper.toLowerCase ()
+  
+} function sayHiToGrandma(string)
+return "I love yu, too."
