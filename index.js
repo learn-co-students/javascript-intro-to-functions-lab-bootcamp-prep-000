b4c59f480c3338242a19d3350d3f406200a95da3
Mariam <mariamjhabib@gmail.com>
2019-03-10 17:54:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logshout(string) {
+  console.log (string).toUpperCase
+}
