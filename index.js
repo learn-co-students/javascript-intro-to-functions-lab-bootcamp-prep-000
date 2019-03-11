727257d04ca3196156fc624157fef9f754ee7629
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-03-11 03:48:12 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string){
   return string.toUpperCase()
 }
 
-function whisper(string){
+function whisper (string){
   return string.toLowerCase()
 }
 
@@ -15,13 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string) {
-    return "I can\'t hear you!";
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
   }
-  else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
+  else if (string === string.toUpperCase()){
+    return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma.") {
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
