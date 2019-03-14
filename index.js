4acd6abd8d77962953574ad022dbc7bb2dd8277b
Franklin <makyfj@hotmail.com>
2019-03-14 23:51:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase())
+    return "I can't hear you"
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase())
+    return `YES INDEED!`
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+    return `I can\'t hear you!`
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase())
+    return `YES INDEED!`;
+else
+    return `I can\'t hear you!`;
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+    return `I love you, too`;
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+   return `I can\'t hear you!`;
+}
+function sayHiToGrandma(string){
+if (string === string.toUpperCase()) 
+    return `YES INDEED!`;
+}
+function sayHiToGrandma(string){
+if (string === "I love you, Grandma.")
+    return `I love you, too.`;
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+    return `I can\'t hear you!`;
+  if (string === string.toUpperCase())
+    return `YES INDEED!`;
+  if (string === "I love you, Grandma.")
+    return `I love you, too.`;
+}
+
+
+
+
+
