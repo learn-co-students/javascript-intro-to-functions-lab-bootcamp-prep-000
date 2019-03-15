cf93249aae9ea65c8db23be65085ad08210e92b5
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-15 02:15:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+ 
+function logShout(string) {
+  console.log('HELLO');
+  return string.toUpperCase();
+}
+
+function logWhisper(string) {
+  console.log('hello');
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  var hello = "I can\'t hear you!"
+  console.log(${hello})
+  return string.toLowerCase()
+}
