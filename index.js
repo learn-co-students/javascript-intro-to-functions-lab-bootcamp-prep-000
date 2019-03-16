13d302a1343defe11de42d0a4893d0355d72525d
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-16 23:53:31 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var hello = "I can\'t hear you!"
-  console.log(${hello})
-  return string.toLowerCase()
+if(string.toLowerCase() === string)
+  return "I can't hear you!";
+  else if(string.toUpperCase() === string)
+  return "YES INDEED!";
+  else if(string === "I love you, Grandma.");
+  return "I love you, too.";
 }
