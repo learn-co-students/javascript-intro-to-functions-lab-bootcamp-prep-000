271ea82ed5efd11af498ba8fc01d5b278150d81d
Awoh Negheie <awohnegheie@gmail.com>
2019-03-20 17:57:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma("I can't hear you!") {
+  return string.toLowerCase()
+}
