6174a1ecbafd4f624aa696e1f3c12290d2a4850f
Aodhan Vanderibilt <tamegorilla@gmail.com>
2019-03-21 18:41:22 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string.toLowerCase()===string)  {
+if (string===string.toLowerCase())  {
   return('I can\'t hear you!')
 }
-if (string.toUpperCase()===string) {
+if (string===string.toUpperCase()) {
   return('YES INDEED!')
 }
 if (string==='I love you, Grandma.'){
