c79f97963e01083d89febf340b678bb5cb760f0c
Christopher Yuan <chrisyuan7@gmail.com>
2019-03-22 16:18:28 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else if (string === string.toUpperCase()) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
