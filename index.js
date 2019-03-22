d6eb5aa46ef3c717a918b3f24da36c3465b7c147
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-22 19:51:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+ if (string==="I love you, Grandma.") {
+  return ("I love you, too.");
+}
+ else 
+ if (string===string.toLowerCase()) {
+  return ("I can\'t hear you!");
+}
+  else
+   (string===string.toUpperCase()) 
+    return ("YES INDEED!");
+}
