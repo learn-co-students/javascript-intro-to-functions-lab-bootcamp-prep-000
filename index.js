289a3bf40ec7e0bd9b891f46fc9a91043ead0e4b
szhu5759 <szhu5759@gmail.com>
2019-03-26 04:39:51 +0000

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase
+  if (string == string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  if (string == string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
