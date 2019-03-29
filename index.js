d5e52add6376527cab2ddd0b659c128a4191c47a
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-29 23:47:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log.string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  if string===string.toLowerCase()
+    console.log("I can't hear you!")
+  else if string===string.toUpperCase()
+    console.log("YES INDEED!")
+    else
+      console.log("I live you, too.")
+    }
+
+
+
+
+}
