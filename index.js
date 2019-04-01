2a26ed1c0265ce266e3df32b92806ea6301d6e6c
Ezra Brand <ezrabrand@gmail.com>
2019-04-01 18:37:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+shout('hello')
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase)
+  return 'I can\'t hear you!'
+}
