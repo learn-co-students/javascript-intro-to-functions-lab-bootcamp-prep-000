dc62f6f6ba6c705caf6a704ab2c0281a239d09c9
Joseph Reilly <reilly4181@gmail.com>
2019-04-01 20:54:14 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase){
+  if(string === string.toLowerCase()){
     return "I can't hear you!"
-  } 
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
 }
