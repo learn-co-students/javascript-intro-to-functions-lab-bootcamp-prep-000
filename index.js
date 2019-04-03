8b38ecbc367a7806229a302279fa40dbacd7f510
Ramon Chavez <rachavez1974@gmail.com>
2019-04-03 18:40:03 -0400

    Finish avascript-intro-to-functions-lab-bootcamp-prep-000

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+function shout(phrase){
+  return phrase.toUpperCase()
+}
+
+function whisper(phrase){
+  return phrase.toLowerCase()
+}
+
+function logShout(phrase){
+  console.log(phrase.toUpperCase())
+}
+
+function logWhisper(phrase){
+  console.log(phrase.toUpperCase())
+}
+
+function logWhisper(phrase){
+  console.log(phrase.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+  
+}
+
