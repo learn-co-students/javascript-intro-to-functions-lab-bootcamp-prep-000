1d4fbba0a8974df9d2e0b6167106bbab41bdc44c
Ititi Obidah <iobidah9@gmail.com>
2019-04-15 23:16:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function shout(string){
+function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(string){
+function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string){
+
+function logShout(string) {
   console.log(string.toUpperCase())
-}
-function logWhisper(string){
+} 
+
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
-  if (string.toLowerCase() === string) {
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())  
   return "I can't hear you!"
-  }
-  if (string.toUpperCase()=== string) {
+  else if (string === string.toUpperCase())
   return "YES INDEED!"
-  }
-  if (string === "I love you, Grandma.") {
+  else (string === "I love you, Grandma.")
   return "I love you, too."
-}
 }
