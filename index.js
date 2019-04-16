b269f612cfd8c585271f76018e72018053306a5f
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-16 23:55:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log('HELLO');
+}
+function logWhisper(string) {
+  console.log('hello');
+}
+var message = `("${string}, grandma")`;
+var sayHiToGrandma = "string";
+var sayILoveYouToGrandma = "I Love You, Grandma";
+console.log("hello");
+function sayHiToGrandmalogWhisper(){
+console.log(`"${whisper}, grandma!"`);
+return "I can't hear you!";
+}
+function sayHiToGrandmalogShout(){
+  console.log(`"${shout}, grandma!"`);
+  return "YES INDEED!";
+}
+function sayILoveYouToGrandma(){
+  console.log(`"${"I love you"}, grandma"`);
+}
+function sayILoveYouToGrandma(){
+  console.log("I love you, grandma!");
+  return "I love you, too.";
+}
+
