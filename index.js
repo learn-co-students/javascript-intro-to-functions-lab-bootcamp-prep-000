b8455a9a263eb83e58e03cee243bee3201f9eb8a
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-22 19:53:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string)
+{
+  return string
+}
+"Hello!".toUpperCase()
+function shout(string)
+{
+  return string.toUpperCase()
+}
+function whisper(string) 
+{
+  return string.toLowerCase()
+}
+function logShout(string) 
+ 
+{ it ('calls console.log() its one argument in all caps'} 
+ 
+ 
+function logWhisper(string) calls {console.log ("logWhisper").toUpperCase() } 
+
+ function sayHiToGrandma(string) {console.log ("I can't hear you").toLowerCase() }
+ 
+ function sayHiToGrandma(string) {console.log ("YES INDEED").toUpperCase() }
