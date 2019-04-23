b6bfdbd73c47c0b65a042baf2c2b99de2fee1a92
Sam Markham <sam.e.markham@gmail.com>
2019-04-23 00:06:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+  return string.toUpperCase()
+};
+
+function whisper(string) { 
+  return string.toLowerCase()
+}; 
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+};
+logShout('hello');
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+};
+logWhisper('HELLO');
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else {
+    return
+  }
+}
+
+//describe('sayHiToGrandma(string)', function() {
+ // it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+   // expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  //})
+
+  //it('returns "YES INDEED!" if `string` is uppercase', function() {
+   // expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  //})
+
+ // it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+  //  expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+ // })
+//})
