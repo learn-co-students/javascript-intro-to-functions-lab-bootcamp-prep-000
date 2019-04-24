1dc7e96675897e827b47eb5c5a982b4e2d0dd4a4
Dominic Emond <emond.dominic@gmail.com>
2019-04-24 23:43:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(str)
+{
+  return str.toUpperCase()
+}
+
+function whisper(str)
+{
+  return str.toLowerCase()
+}
+
+function logShout(str)
+{
+  console.log(str.toUpperCase());
+}
+
+function logWhisper(str)
+{
+  console.log(str.toLowerCase());
+}
+
+function sayHiToGrandma(str)
+{
+  if (str == "I love you, Grandma.")
+  {
+    return "I love you, too."
+  }
+  else if (str.toLowerCase() == str)
+  {
+    return "I can\'t hear you!"
+  }
+  else if (str.toUpperCase() == str)
+  {
+    return "YES INDEED!"
+  }
+}
