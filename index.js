7dfbf88de2cf403f2cc3f76cc94bcd7252bbfdec
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-04-30 12:06:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function shout('Hello!'){
-return
-'Hello!'.toUpperCase
+function shout(string) {
+  return (string.toUpperCase());
+}
+
+function whisper(string) {
+  return (string.toLowerCase());
+}
+
+function logShout (string) {
+  console.log(string.toUpperCase( ));
+}
+
+function logWhisper (string) {
+  console.log(string.toLowerCase( ));
   
+
+}
+function sayHiToGrandma (aWord) {
+  var cantUnswer = "I can't hear you!";
+  var yesUnswer = "YES INDEED!";
+  var lovUnswer = "I love you, too.";
+  if (aWord.toLowerCase(aWord) === aWord) {
+    return cantUnswer;
+  }
+  else if (aWord.toUpperCase(aWord) === aWord) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === aWord) {
+    return lovUnswer
+  }
 }
