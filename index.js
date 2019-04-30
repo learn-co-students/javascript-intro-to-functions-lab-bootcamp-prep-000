236a555c0fa34c8b778982b71381d9e5becd8b8f
Justin Perez <jperez.devkaizen@gmail.com>
2019-04-30 15:33:44 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) { 
+    return("I can't hear you!");
+  } else if (string === string.toUpperCase()) { 
+    return("YES INDEED!");
+  } else { 
+    return("I love you, too.");
+  }
+}
