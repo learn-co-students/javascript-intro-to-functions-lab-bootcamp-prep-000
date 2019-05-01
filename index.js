add0112c0c2d5717567804f7f219e23a52008827
dennis wang <dwang0816@gmail.com>
2019-05-01 02:12:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ whisper("HELLO")
 function logShout(string) {
   console.log(string.toUpperCase())
 }
-logShout("hello")
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-logWhisper("HELLO")
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()) return "I can't hear you!";
