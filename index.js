832fbe45fc30c9d734f2bbd64a0d7ad1fab9e487
Rick Battipede <rbattipede@gmail.com>
2019-05-01 03:00:55 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO";
+  var lowercase = "hello";
+  var grandma = "I love you, Grandma.";
+  if(string===lowercase){
+    return "I can\'t hear you!"
+    } else if(string===uppercase){
+      return "YES INDEED!"
+    } else if(string===grandma){
+      return "I love you, too."
+    }
+}
