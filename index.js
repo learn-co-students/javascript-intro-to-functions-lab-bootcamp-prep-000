6916204e634b17e138c9cd1dfa410aa591308a43
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-05-01 05:55:11 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase());
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
