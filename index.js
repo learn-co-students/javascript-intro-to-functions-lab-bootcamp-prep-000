3fc0f19d10d99d419facedcfb0a5e2684bff79e1
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-06 00:08:56 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
+var string = lowercase
 function sayHiToGrandma(string) {
-  return string
+  return ("I can't hear you!") 
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  return ("YES INDEED!")
+  console.log ('HELLO')
+} 
+var string = "I love you, Grandma."
+function sayHiToGrandma(string) {
+  return ("I love you, too.") 
 }
