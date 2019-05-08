fb8283894d6066069e909851b1b9858c68d34730
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-08 09:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
 }
-function whisper(string) {
+
+function whisper(string){
   return string.toLowerCase()
 }
+
 function logShout(string){
-  console.log('HELLO')
-}
-function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma(string) {
- if (string.toLowerCase() === string) {
-   return("I can\'t hear you!")
- } else if (string.toUpperCase() === string) {
-    return("YES INDEED!")
-  } else (string === 'I love you, Grandma.'); {
-    return("I love you, too.") }
+function logWhisper(string){
+  console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === `I love you, Grandma.`) {
+    return "I love you, too."
+  }}
