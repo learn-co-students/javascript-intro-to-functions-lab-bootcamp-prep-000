43162df088d83b86737ec6974247d5f061b989eb
aj.artwil <aj.artwil@gmail.com>
2019-05-09 20:09:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-function shout (string) {
+function shout(string) {
   return string.toUpperCase()
-}
-function whisper(string) {
-  return string.toLowerCase()
-}
-function logShout (){
- console.log ("HELLO") 
-}
-function logWhisper (){
-  console.log ("hello")
-}
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) 
-    return "I can't hear you!"
-}
-  if (string.toUpperCase() === string){
-    return "YES INDEED!"
+} 
+  
+function whisper(string){
+  return string.toLowerCase();
   }
+  
+function logShout(string){
+  console.log('HELLO')
+} 
+
+function logWhisper(string){
+  console.log('hello') 
+}
+
+function sayHiToGrandma(string){
+     if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
