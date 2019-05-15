1e8c963d46edb4c11f58a4c6158224a09775b0b8
keemo <keemowatson1@gmail.com>
2019-05-15 16:08:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout (string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase())
+return ("I can't hear you!")
+ if ( string===string.toUpperCase())
+  return("YES INDEED!")
+  if(string==='I love you, Grandma.')
+  return ('I love you, too.')
+  }
+
