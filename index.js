bf4458d8bfa26f37c9b6111b66c125591697baac
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-23 21:41:38 +0000

    Done.

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-
+function sayHiToGrandma(string) {
+	if (string === string.toLowerCase()) {
+   return "I can't hear you!"
+ } else if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+ } else if (string === 'I love you, Grandma.'){
+	return "I love you, too."
+ }
+}
