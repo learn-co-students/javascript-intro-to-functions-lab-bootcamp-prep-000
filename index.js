76b582033dc019ad5b1cdcfb742aecbe93d37864
Tenika Powell <reachformestar24@gmail.com>
2019-05-27 03:25:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string){
+return string.toUpperCase()
+}
+
+var uppercase = "HELLO!"
+return uppercase.toUpperCase()=== uppercase
+
+var lowercase = 'hello!'
+return lowercase.toLowerCase() === lowercase
+
+  
