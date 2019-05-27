e1cdba9c2c2e39f34112b410629f294b45eccf68
Roberto Samia <bojosteph@gmail.com>
2019-05-27 16:06:38 +0000

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
- console.log(string.toLowerCase())
+  console.log(string.toLowerCase())
 } 
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-   return "I can't hear you!";
-}  else if (string.toUpperCase() === string) {
-  return"YES INDEED!";
+    return "I can't hear you!";
+} else if (string.toUpperCase() === string) {
+    return"YES INDEED!";
 } else if (string === "I love you, Grandma.") {
   return "I love you, too.";
 }
 }
+
+  
+function logWhisper(string) {
+ console.log(string.toLowerCase())
+} 
+  
