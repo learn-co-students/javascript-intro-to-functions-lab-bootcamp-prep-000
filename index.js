ec7577c7b69e082679bb6a7ce4bdf00ae3f36a4b
Nicole Villa <nicolevilla.109@gmx.com>
2019-05-29 07:20:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string){
+  return string.toUpperCase('hello')
+}
+
+function whisper(string){
+  return string.toLowerCase('HELLO')
+}
+
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+}
