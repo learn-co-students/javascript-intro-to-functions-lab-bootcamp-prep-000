d05404bc9a2a7a6e8b6ed609c5e32709ff800ec7
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 13:29:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(shout('HELLO'))
+}
+function logWhisper(string) {
+  console.log(whisper('hello'))
+}
+function sayHiToGrandma(string) {
+ return ("I can\'t hear you!" if string.toLowerCase)
+}
+function sayHiToGrandma(string) {
+  return ("YES INDEED!" if string.toLowerCase)
+}
+function sayHiToGrandma(string) {
+  return("I love you, too." if `string` is "I love you, Grandma."`')
+}
