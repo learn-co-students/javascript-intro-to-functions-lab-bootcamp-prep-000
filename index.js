9e3c07b5ee216d29de90b62b0d936b46f00c78f1
Nicole Villa <nicolevilla.109@gmx.com>
2019-06-05 07:18:19 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ function whisper(string){
 }
 
 function logShout(string){
-  uppercase = string.toUpperCase()
-  string = uppercase;
-  console.log(uppercase);
+  string = "hello";
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  logWhisper = "HELLO"
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase("hello") === string){
+    return 'I can\'t hear you!';
+  }
+  if (string.toUpperCase("HELLO") === string){
+    return 'YES INDEED!';
+  }
+  if (string === 'I love you, Grandma.'){
+    return 'I love you, too.';
+  }
 }
