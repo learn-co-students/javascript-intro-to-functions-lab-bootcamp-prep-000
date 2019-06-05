8fb296969ccd3aed7626b2d37f3656b45b58f0b5
diligent-innovator-8030 <darvoz@hotmail.co.uk>
2019-06-05 09:04:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
-
-
 function shout(string) {
   return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(logShout(string.toUpperCase))
 }
