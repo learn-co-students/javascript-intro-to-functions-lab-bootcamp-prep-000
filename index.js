ef77f37e4b8734b670a36ece22edd3dc587d2cbd
Emily <aspentree79@gmail.com>
2019-06-06 21:02:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(hello) {
+return hello.toUpperCase()
+}
+
+function whisper(hello) {
+return hello.toLowerCase()
+}
+
+var hello = 'hello'
+function logShout(string) {
+  console.log(hello.toUpperCase())
+  return (hello).toUpperCase()}
+
+
+var hello = 'hello'
+function logWhisper(string) {
+  console.log(hello)
+  return (hello).toLowerCase()}
+
+function sayHiToGrandma(string) {
+if (string === string.toLowerCase()) {return 'I can\'t hear you!';}
+if  (string === string.toUpperCase()) {return ('Yes Indeed!').toUpperCase();}
+if (string === 'I love you, Grandma.') {return 'I love you, too.';}
+}
