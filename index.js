7d4768d127bf90ba480f6d75b90d4c114d28e24a
jose luis <cortezluisjose@gmail.com>
2019-06-12 00:52:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase 
+ 
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase 
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // 
+ 
+mixedCase.toUpperCase() === mixedCase
