890a876640aaa819c161c36947456077b27a3d89
Aaron M <amitchtech@gmail.com>
2019-06-18 19:30:09 +0000

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,41 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-var uppercase = "HELLO!"
+function whisper(string) {
+  return string.toLowerCase()
+}
 
-uppercase.toUpperCase() === uppercase
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
-var lowercase = 'hello!'
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
 
-lowercase.toLowerCase() === lowercase
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else (string === "I love you, Grandma.")
+    return "I love you, too."
+}
 
-var mixedCase = 'Hi there!'
 
-mixedcase.toUpperCase() === mixedCase
+logShout("hello")
 
-mixedcase.toLowerCase() === mixedCase
+//
+// var uppercase = "HELLO!"
+//
+// uppercase.toUpperCase() === uppercase
+//
+// var lowercase = 'hello!'
+//
+// lowercase.toLowerCase() === lowercase
+//
+// var mixedCase = 'Hi there!'
+//
+// mixedcase.toUpperCase() === mixedCase
+//
+// mixedcase.toLowerCase() === mixedCase
