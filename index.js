5dd3bfc7ac11d6395cfdb6464fa618bc2d11a4a6
Alejandro Cuellar <alejandrocuellar18@gmail.com>
2019-06-18 12:21:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(str){
+  return str.toUpperCase()
+}
+
+function whisper(str){
+  return str.toLowerCase()
+}
+
+function logShout(str){
+  console.log(str.toUpperCase())
+}
+
+function logWhisper(str) {
+  console.log(str.toLowerCase())
+}
+
+function sayHiToGrandma(str){
+  if(str.toLowerCase() === str){
+    return "I can\'t hear you!"
+  }
+}
+
+function sayHiToGrandma(str){
+  if(str.toUpperCase() === str){
+    return "YES INDEED!"
+  }}
+
+function sayHiToGrandma(str){
+  if(str ==="I love you, Grandma."){
+    return "I love you, too."
+  }}
