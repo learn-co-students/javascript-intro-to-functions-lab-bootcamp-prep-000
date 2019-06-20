e71bfc74f06c52f59bde6efde5163843e8dfa29b
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-20 17:05:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+
+function sayHiToGrandma(string){
+  } if (string === lowercase) {
+     return("I can't hear you!")
+}
