8a9a3e637a10d9f3d7a925e56d8172590138eae8
Andrew Berger <aberg0606@gmail.com>
2019-06-21 03:13:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase
-
-mixedCase.toUpperCase() === mixedCase
 
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma('hello!') {
-  return "I can't hear you!"
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  
+  return "Are you eating enough?"
 }
