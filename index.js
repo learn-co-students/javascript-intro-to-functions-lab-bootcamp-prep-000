c155a56945a7389d9507000e63297b4cb81cd64d
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 06:47:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var a='hello', b='HELLO', c= 'I love you, Grandma.'
+
+function shout(string) {
+return 'hello'.toUpperCase()
+}
+
+  function whisper(string) {
+return 'HELLO'.toLowerCase()
+  }
+  
+  function logShout(string) {
+console.log ('HELLO')
+}
+
+  function logWhisper(string) {
+console.log ('hello')
+}
+
+function sayHiToGrandma(a,b,c) {
+
+if (a){
+return ("I can\'t hear you!")}
+
+if (b){
+return ("YES INDEED!")}
+
+if (c)
+return ("I love you too")
+}
