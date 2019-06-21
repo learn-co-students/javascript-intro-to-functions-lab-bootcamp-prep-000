9010e72af1cc5ddfd542f7cf936277fdaf9d4046
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-21 12:46:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 var lowercase = 'hello!'
 lowercase.toLowerCase() === lowercase
 
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+
+
 function sayHiToGrandma(string){
-  } if (string === lowercase) {
-     return("I can't hear you!")
+    if (string === "I love you, Grandma.")
+      return "I love you, too.";
+    else if (string === string.toUpperCase())
+      return 'YES INDEED!';
+    else (string.toLowerCase() === string);
+      return "I can't hear you!";
+}
+
+
+function whisper(string){
+  return string.toLowerCase()
+  }
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
 }
