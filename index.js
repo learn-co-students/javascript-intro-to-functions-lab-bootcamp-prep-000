b1a4f65fb200fffaa940f7ce19313b44f0bf8413
Deborah Savage <savaged725@yahoo.com>
2019-06-25 15:24:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello') 
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello') 
+}
+
+function sayHiToGrandma(string) { 
+   if (string === string.toLowerCase())
+   return "I can't hear you!"
+   if (string ===string.toUpperCase())
+   return "YES INDEED!"
+   else if (string === "I love you, Grandma.")
+    return "I love you, too."
+}
+
+
+
+
+ 
+ 
