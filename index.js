0391f6ff89ddbbdb1473315f701be0c6bb6f7f0e
Adam Brown <adam.brown.betty@gmail.com>
2019-06-25 21:50:38 +0000

    Done.

--- index.js
+++ index.js
@@ -15,41 +15,19 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-var ifLow = "I can\'t hear you!";
-var ifUp = "YES INDEED!";
-var ifLove = "I love you, too.";
-
-if (string.toLowerCase(string) === string) {
-
-return ifLow;
-
-} else if (string.toUpperCase(string) === string) {
+  if (string.toLowerCase() === string) {
+    
+    return "I can\'t hear you!";
+    
+  } else if (string.toUpperCase() === string) {
+    
+    return "YES INDEED!";
+    
+  } else if (string === "I love you, Grandma.") {
+    
+    return "I love you, too.";
   
-  return ifUp;
+    
+  }
+} 
 
-  
-} else if (string === "I love you, Grandma.") {
-  
-  return ifLove;
-}
-
-  
-}
-  
-
-  
-
-/*
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-}) */
