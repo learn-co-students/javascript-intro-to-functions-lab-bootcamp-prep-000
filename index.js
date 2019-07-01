405d177440e76e500c22189fe0635b37502b0e86
Seeta Rajpara <seeta_rajpara@alumni.brown.edu>
2019-07-01 20:10:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toUpperCase()
+  console.log(logShout())
+  spy('HELLO')
+}
+function logWhisper(string) {
+  return string.toLowerCase()
+  console.log(spy('hello'))
+}
+function sayHiToGrandma(string) {
+  if string.toLowerCase() === lowercase
+    console.log('I can\'t hear you!')
+}
