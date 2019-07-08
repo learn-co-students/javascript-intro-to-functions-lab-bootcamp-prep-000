511e9cbffd10dad2949cdadae77ffc8c6ef08229
Keli Anaya <keli.anaya@gmail.com>
2019-07-08 17:44:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO")
+}
+function logWhisper(string) {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string){
+	if (string.toLowerCase() === string){
+	  return 'I can\'t hear you!';
+	}
+	if (string.toUpperCase() === string){
+	  return 'YES INDEED!';
+	}
+	if (string === 'I love you, Grandma.'){
+	  return 'I love you, too.';
+	}
+}
