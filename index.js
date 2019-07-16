49541a5ddc57df632a40ca79055733d2e78b9b4b
Carolina Ferreira <carolina.ferreira.16@ucl.ac.uk>
2019-07-16 03:14:42 +0000

    Done.

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ function logWhisper(string) {
 }
 
 
-
-function sayHiToGranda(string) {
-  var lowercase = string.toLowerCase()
-  if (string === "lowercase")
-  return "I can't hear you"
-  else 
-  var uppercase = string.toUpperCase
-  if (string === "uppercase")
-  return "YES INDEED!"
-  else
-  if (string = "I love you, Grandma.")
-  return "I love you too."
-
+ 
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase() 
+  var uppercase = string.toUpperCase() 
+  var hiGrandma = "I love you, Grandma."
+  
+  if (string === lowercase) {
+    return "I can\'t hear you!"
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else if (string === hiGrandma) {
+    return "I love you, too."
+  }
 }
