64b104cd373fda65f76ddafc470f2232ab084c7f
Priscilla Firpo <priscillayf@gmail.com>
2019-07-22 01:42:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,112 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return whisper.toLowerCase()
+}
+function whisper(string) {
+  return whisper.toLowerCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+var uppercase = "STRING"
+
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+var lowercase = "string"
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+var uppercase = "STRING"
+
+uppercase.toUpperCase() === uppercase
+
+function sayHiToGrandma(string) {
+  return "YES INDEED!"
+}
+var lowercase = "string"
+
+lowercase.toLowerCase() === lowercase
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+var uppercase = "STRING"
+
+uppercase.toUpperCase() === uppercase
+
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+var uppercase = "STRING"
+
+uppercase.toUpperCase() === uppercase
+
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+function sayHiToGrandma(STRING) {
+  console.log("YES INDEED!")
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+function sayHiToGrandma(uppercase) {
+  console.log("YES INDEED!")
+}
+function sayHiToGrandma(uppercase) {
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(STRING) {
+  return "YES INDEED!"
+}
+
+var uppercase = "STRING"
+ 
+uppercase.toUpperCase() === uppercase
+
