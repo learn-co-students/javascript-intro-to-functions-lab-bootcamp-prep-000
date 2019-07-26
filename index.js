26c7b892395e8537a0870fb350283426024b72d5
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-26 16:16:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase()) 
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) { // "I can't hear you"
+  }
+    
+  if (string.toUpperCase()) {
+    // "HELLO"
+  }
+    
+  if (string."I love you, grandma"()) {
+    // "I love you too"
+    sayHiToGrandma === lowerCase
+  }
+}
