16d01ba504a7e97f74c3baddf6c56f76adff72e2
Buford Eeds <bufordeeds8@gmail.com>
2019-08-01 18:19:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+'Hello!'.toUpperCase()
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+'HELLO'.toLowerCase()
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  console.log ('hello');
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
