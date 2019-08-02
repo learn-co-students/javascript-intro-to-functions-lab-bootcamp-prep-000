3db164deed9bbdd5c31369e42f9502ac997ed3b9
Jason Santana <jsantana006@gmail.com>
2019-08-02 00:26:27 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -39,12 +39,11 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
-
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
   })
-
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
+
