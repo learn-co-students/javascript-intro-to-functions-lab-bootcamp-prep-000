82342e055dbd5fe6a994a32f35b41d193b841518
kevin Abreu <kabreu11385@gmail.com>
2019-08-07 21:20:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+}
+function SayHelloToGrandma(hello) {
+  return "i can't hear you!".toLowerCase()
+  === lowercase
+}
