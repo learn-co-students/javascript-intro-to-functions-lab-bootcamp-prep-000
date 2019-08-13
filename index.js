95eaa640f86366cab58eb86a20c4b00619c91fb9
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-13 16:40:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+return string.toLowerCase()
+}
+function logShout(string) {
+return string.toUpperCase()
+}
+function logWhisper(string) {
+return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+console.js("I love you, too.")
+}
