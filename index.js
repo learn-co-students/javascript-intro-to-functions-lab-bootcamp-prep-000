5f46b8571977c9e28e8e3bfe9a74624a1aa1c2f5
Erica Lindholm <elindholm94@gmail.com>
2019-08-13 02:09:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+
+  return string.toUpperCase()
+
+}
+
+function whisper(string) {
+
+  return string.toLowerCase()
+
+}
+
+function logShout(string) {
+
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  return ("I can't hear you!")
+ if (string)toLowerCase
+
+   return ("YES INDEED!")
+  if (string)toUpperCase
+
+  return ("I love you, too")
+ if (string)"I love you, Gandma"
+}
