3829400ce1b4d843e822ec8cfa0ace22b68e354c
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-17 04:24:53 +0000

    Done.

--- index.js
+++ index.js
@@ -18,11 +18,3 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return "I can't hear you!"
 }
-
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) return "YES INDEED!"
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") return "I love you, too."
-} 
