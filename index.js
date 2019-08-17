dce2a0e7ef98f3abd0a4cd2e18f6110a1eae0a82
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-17 04:33:19 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase())
+  if (string === string.toLowerCase()) {
   return "I can't hear you!"
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+}
 }
