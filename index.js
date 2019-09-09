1fb782ea8f428bcf2b2191c79d393af5aa887b4b
Adham  Hamed <hameda6256@gmail.com>
2019-09-09 14:43:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma (string){
+  var cantAnswer = "I can't hear you!";
+  var yesAnswer = "YES INDEED!";
+  var loveAnswer = "I love you, too.";
+    if (string.toLowerCase(string) === string){
+      return cantAnswer;
+    }
+    else if (string.toUpperCase(string) === string){
+      return yesAnswer;
+    }
+    else if ("I love you, Grandma." === string){
+      return loveAnswer;
+    }
+}
