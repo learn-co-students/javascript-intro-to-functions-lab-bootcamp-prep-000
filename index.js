39841a1398abfa722618d525c7decc53b1340ef7
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-09-22 17:33:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(hello){
+  console.log(hello.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+ function sayHiToGrandma(string){
+   return string.toLowerCase()
+   
+ }
