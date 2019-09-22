da1a9a6a06115f60e430c9ab20b5f8ed1f2f056d
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-09-22 18:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,26 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
+
+
  function sayHiToGrandma(string){
-   return string.toLowerCase()
-   
- }
+if(string===string.toLowerCase()){
+  return "I can't hear you!"
+  
+}
+  else if(string===string.toUpperCase() ){
+    return 'YES INDEED!'
+  }
+  
+  else if(string=== "I love you, Grandma."){
+return "I love you, too."    
+  }
+  
+  
+}
+
+
+
+
+
+ 
