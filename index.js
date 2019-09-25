91f6d13627a9ce994572980f45877145f31b2610
Firas Chaabani <firaschaabani1@gmail.com>
2019-09-25 14:53:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+function whisper{
+  return string.toLowerCase()
+}
+var uppercase = "HELLO'"
+var lowercase= "hello'"
+if (uppercase = "HELLO") {
+  console.log ('HI THERE HELLO')
+} else {console.log ('hello')
+}
