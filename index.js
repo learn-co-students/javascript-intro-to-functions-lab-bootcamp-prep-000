f93068abff8085e6e78df15312e1795ce008625e
April Escobar <aprilfaith.e@gmail.com>
2019-10-26 21:00:52 +0000

    Done.

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else if(string === string.toUpperCase()) {
+  } if(string === string.toUpperCase()) {
     return "YES INDEED!"
-  } else if(string === "I love you, Grandma.") {
+  } if(string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
