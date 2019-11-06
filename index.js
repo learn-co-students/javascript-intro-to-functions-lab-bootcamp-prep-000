0645d95ed21b263ba2a9be4a164e5a4f99b19f05
Worod Idrees <rose.wassouf93@gmail.com>
2019-11-06 00:20:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase
+  return "I can\'t hear you!"
+}
+function sayHiToGrandma(string){
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string){
+  var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
+return "I love you, too."
+}
