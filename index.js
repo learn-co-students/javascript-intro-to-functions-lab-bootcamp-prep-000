65df4f03d823b0182081d87ac1bb6393422b13f7
Dave Patel <ddp2134@gmail.com>
2020-01-28 20:16:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+}
+
+
+function sayHiToGrandma(string) {
+  if .toUpperCase()
+  return "YES INDEED" ;
+  } else {
+  return "I can't hear you!";
+  }
+
+
+  
