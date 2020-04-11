7a91d76d11ca72ac9a44dc16fc2e34e5f06a659d
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-11 01:33:04 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toUpperCase()){
     return "YES INDEED!";
-    if(string.toLowerCase()){
-      return "I can't hear you!";
-    }
+  }
+  if(string.toLowerCase()){
+    return "I can't hear you!";
+  }
+  if(string === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
