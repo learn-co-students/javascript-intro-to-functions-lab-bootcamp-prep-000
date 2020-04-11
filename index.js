4dd56ce25675c7147bd4539972280f26205fb97a
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-11 01:48:10 +0000

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
     if(string.toUpperCase()){
       return "YES INDEED!";
     }
-    if(string.toLowerCase()){
+    else{
       return "I can't hear you!";
     }
   }
