dbaf8781ac39fae437fefe1b88dade41af311b92
James Mannarino <jamesmannarino@gmail.com>
2020-04-26 21:08:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+
+var cantHear = "I can't hear you!"; 
+var yes = "YES INDEED!";
+var loveYou = "I love you, Grandma.";
+var youToo = "I love you, too."
+
+if (string.toLowerCase(string) === string) {
+  return cantHear
+}
+
+if (string.toUpperCase(string) === string) {
+  return yes
+}
+
+if (string === loveYou) {
+  return youToo
+}
+}
