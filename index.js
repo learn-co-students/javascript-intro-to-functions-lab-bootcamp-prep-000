0d5ffac3cb40673ba8df9a32f01fb80104a89ecf
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-25 18:40:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("HELLO"); string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log("hello"); string.toLowerCase()
+}
+  var lowercase ="hello!"
+  lowercase.toLowerCase() === lowercase
+  var uppercase= "HELLO!"
+  uppercase.toUpperCase() === uppercase
+  var mixedCase= "I love you, Grandma."
+  mixedCase.toLowerCase()=== mixedCase
+  mixedCase.toUpperCase()=== mixedCase
+ 
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) 
+   return `I can't hear you!`
+  else if (string === string.toUpperCase())
+  return `YES INDEED!`
+  else (string === "I love you, Grandma.")
+  return `I love you, too.`
+  
+  }
+  
+/*function sayHiToGrandma(string){
+  var lowercase.toLowerCase()=== lowercase
+  var uppercase.toUpperCase()=== uppercase
+  var mixedCase==="I love you, Grandma."
+  if (lowercase===lowercase.toLowerCase)
+  return `I can't hear you!`
+  else if (uppercase===uppercase.toUpperCase)
+  return `YES INDEED!`
+  else (string="I love you, Grandma.")
+  return `I love you, too.`
+} */
+  
