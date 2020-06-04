6dbc6d0e54551d7a54c5ac852cf6844afb986f56
Cedric Patton <ceddyp117@gmail.com>
2020-06-04 20:14:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) 
+{  return string.toUpperCase()
+}
+
+ whisper(string) 
+ { return string.toLowerCase()
+}
+
+ 
+  logShout.toUpperCase()
+  
+
+
+function logWhisper(string) { 
+console.log().toLowerCase()
+}
