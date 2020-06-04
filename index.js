c7a82f54dc6df7e51e6de437f1bbf6942b0edea5
Cedric Patton <ceddyp117@gmail.com>
2020-06-04 20:17:27 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function shout(string)
 function logWhisper(string) { 
 console.log().toLowerCase()
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
