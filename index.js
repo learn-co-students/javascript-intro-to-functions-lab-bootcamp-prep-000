ed8843031ad9c16658d5a0b9988a4e6806db0486
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 15:27:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  if (lowercase.toLowerCase() === lowercase)
+  return "I can't hear you!"
+  var uppercase = "HELLO!"
+  else if (uppercase.toUpperCase() === uppercase)
+  return "YES INDEED!"
+  var mixedCase = 'Hi there!'
+  else if (string = "I love you, Grandma.")
+  return "I love you, too."
+}
