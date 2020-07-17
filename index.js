f6eb057c32a5f94bf322d3557d40f62584c7802b
Terrell Murrell <littlerell12@gmail.com>
2020-07-17 13:55:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase( ))
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase( ))
+}
+
+function sayHiToGrandma(string) {
+  var cantUnswer = "I can't hear you!";
+  var yesUnswer = "YES INDEED!";
+  var lovUnswer = "I love you, too.";
+  if (string.toLowerCase(string) === string) {
+    return cantUnswer;
+  }
+  else if (string.toUpperCase(string) === string) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === string) {
+    return lovUnswer;
+  }
+
+}
