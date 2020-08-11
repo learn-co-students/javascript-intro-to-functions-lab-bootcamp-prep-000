f97155bf4e5c1fc7fa0ce83fafa3b32ece43439a
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-11 02:55:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(string){
+function whisper(string) {
   return string.toLowerCase()
+}
+function logShout(string) {
+   console.log('HELLO'. toUpperCase() )
+}
+function logWhisper(string){
+  console.log('hello'. toLowerCase())
+}
+function sayHiToGrandma(string) {
+  console.log(("I can't hear you!") `string` is lowercase)
+}
+function sayHiToGrandma(string) {
+  return string(("YES INDEED!") is `string` is uppercase)
+}
+function sayHiToGrandma(string) {
+  return string.("I love you too", "Grandma.":)
 }
