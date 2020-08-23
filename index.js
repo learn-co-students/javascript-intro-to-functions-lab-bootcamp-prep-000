007f6c99c2888ba8e5ff570cf8236ef317ea0c58
Pavel Dziamentsyeu <paveldz@mail.com>
2020-08-23 20:44:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function shout(string) {
- return string.toUpperCase();
-  }
+  return string.toUpperCase()
+}
 
 function whisper(string) {
-  return string.toLowerCase();
-  }
+  return string.toLowerCase()
+}
 
-function logShout(string) {
-  console.log(string.toUpperCase());
-  }
+function logShout(string){
+  console.log(string.toUpperCase())
+}
 
-function logWhisper(string) {
-  console.log(string.toLowerCase());
-  }
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
-    return "I can't hear you!";
+    return "I can't hear you!"
   }
 
   if (string.toUpperCase() === string) {
-    return "YES INDEED!";
+    return "YES INDEED!"
   }
 
-  if (string==="I love you, Grandma.") {
-    return "I love you, too.";
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
