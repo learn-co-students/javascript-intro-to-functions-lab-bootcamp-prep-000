4e64c6e7da1f8a850d4e4d32f263f5ba62e0561e
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-16 21:18:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+return string.toLowerCase()
+}
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+function sayHiToGrandma (string) {
+var lowercase = "I can't hear you!"
+var uppercase = "YES INDEED!"
+var love = "I love you, too."
+if (string.toLowerCase(string) === string) {
+return lowercase;
+}
+else if (string.toUpperCase(string) === string) {
+  return uppercase;
+}
+else if ("I love you, Grandma." === string) {
+  return love;
+}
+}
