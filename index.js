a5260f422f4016be2c1555cfd1a55db21ad8488c
Amaury Burgos <amauryb.1204@gmail.com>
2020-11-02 04:11:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return 'HELLO'.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return 'HELLO'.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
