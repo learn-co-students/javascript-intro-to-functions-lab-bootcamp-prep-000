ad585dde5d6bb54f3606a239799440ab24dcbca3
Matthew Centeri <mattcenteri@gmail.com>
2021-03-16 21:26:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+var uppercase, lowercase;
+
+function logShout(string) {
+  string.toUpperCase();
+  string=uppercase;
+  console.log(uppercase);
+}
+function logShout(string){
+uppercase=string.toUpperCase();
+string=uppercase;
+console.log (uppercase);
+}
+function logWhisper(string){
+lowercase=string.toLowerCase();
+string=lowercase;
+console.log (lowercase);
+}
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()===string){
+    return "I can't hear you!";
+    }if(string.toUpperCase()===string){
+      return "YES INDEED!";
+    }if(string === "I love you, Grandma."){
+      return "I love you, too.";
+    }
+}
